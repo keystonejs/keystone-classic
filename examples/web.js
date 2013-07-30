f4858972be4d98eec009aed30a410afc4db6bbbd
Jed Watson <jed.watson@me.com>
2013-07-31 03:20:21 +1000

    Example web.js update

--- examples/web.js
+++ examples/web.js
@@ -1,73 +1,34 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var keystone = require('keystone');
 
-var express = require('express'),
-	http = require('http'),
-	path = require('path'),
-	flash = require('connect-flash'),
-	mongoose = require('mongoose'),
-	keystone = require('keystone').connect(mongoose);
+keystone.init({
+	
+	'name': 'My Project',
+	'brand': 'Project Admin',
+	
+	'favicon': 'public/favicon.ico',
+	'less': 'public',
+	'static': 'public',
+	
+	'views': 'views',
+	'view engine': 'jade',
+	
+	'auto update': true,
+	'mongo': process.env.MONGOLAB_URI || ['localhost', 'my-project'],
+	
+	'auth': true,
+	'user model': 'User',
+	'cookie secret': '--- your secret ---',
+	
+	'emails': 'emails',
+	'mandrill api key': '--- your api key ---',
+	'email rules': { find: '/images/', replace: (keystone.get('env') != 'production') ? 'http://localhost:3000/images/' : 'http://www.team9.com.au/images/' },
+	
+	'cloudinary config': { cloud_name: '--- your cloud name ---', api_key: '--- your api key ---', api_secret: '--- your api secret ---' }
+	
+});
 
 require('./models');
 
-var session = require('./lib/session');
-
-keystone.set('auth', session.keystoneAuth);
-keystone.set('brand', 'Team 9');
-
-// Initialise Express Application
-var app = express();
-
-app.configure(function() {
+keystone.set('routes', require('./routes'));
 	
-	// Setup
-	app.set('port', process.env.PORT || 3000);
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	
-	// Serve static assets
-	app.use(express.compress());
-	app.use(express.favicon(__dirname + '/public/favicon.ico'));
-	app.use(require('less-middleware')({ src: __dirname + '/public' }));
-	app.use(express.static(path.join(__dirname, 'public')));
-	keystone.static(app);
-	
-	// Handle dynamic requests
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser('-- your secret here --'));
-	app.use(express.session());
-	app.use(flash());
-	app.use(session.persist);
-	
-	// Route requests
-	app.use(app.router);
-
-	// Handle 404s
-	app.use(function(req, res, next) {
-		res.status(404).send("Sorry, no page could be found at this address.");
-	});
-
-});
-
-// Use Express error handler in the development environment
-app.configure('development', function() {
-	app.use(express.errorHandler());
-});
-
-// Configure keystone routes
-keystone.routes(app);
-
-// Configure application routes
-require('./routes')(app);
-
-// Connect to the database and start the webserver
-mongoose.connect('localhost', '-- your database --');
-
-mongoose.connection.on('error', function() {
-	console.error('Website failed to launch: mongo connection error', arguments);
-}).on('open', function() {
-	http.createServer(app).listen(app.get('port'), function() {
-		console.log("Website is ready on port " + app.get('port'));
-	});
-});
+keystone.start();
