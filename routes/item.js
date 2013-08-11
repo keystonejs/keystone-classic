1e77df8e6b0eb8dff11652764c685fb974711a7b
Jed Watson <jed.watson@me.com>
2013-08-11 21:58:58 +1000

    Now using UpdateHandler internally to save item changes

--- routes/item.js
+++ routes/item.js
@@ -147,120 +147,14 @@ exports = module.exports = function(req, res) {
 		
 		if (req.method == 'POST' && req.body.action == 'updateItem' && !req.list.get('noedit')) {
 			
-			var actionQueue = [],
-				validationErrors = [];
-			
-			var progress = function() {
-				if (validationErrors.length) {
-					_.each(validationErrors, function(i) { req.flash('error', i); });
-					renderView();
-				} else if (actionQueue.length) {
-					// TODO: async upload to cloudinary
-					actionQueue.pop()();
-				} else {
-					saveItem();
+			item.getUpdateHandler(req).process(req.body, { flashErrors: true }, function(err) {
+				if (err) {
+					return renderView();
 				}
-			}
-			
-			var saveItem = function() {
-				item.save(function(err) {
-					if (err) {
-						if (err.name == 'ValidationError') {
-							viewLocals.validationErrors = err.errors;
-							_.each(err.errors, function(e, path) {
-								if (e.type == 'required') {
-									req.flash('error', 'Field ' + path + ' is required.');
-								}
-							});
-						} else {
-							console.error('Error saving changes to ' + req.list.singular + ' ' + item.id + ':');
-							console.error(err);
-							req.flash('error', 'There was an error saving your changes: ' + err.message + ' (' + err.name + ')');
-						}
-						return renderView();
-					} else {
-						req.flash('success', 'Your changes have been saved.');
-						return res.redirect('/keystone/' + req.list.path + '/' + item.id);
-					}
-				});
-			}
-			
-			_.each(req.list.fields, function(field) {
-				
-				// skip uneditable fields
-				if (field.noedit)
-					return;
-				
-				// Some field types have custom behaviours
-				switch (field.type) {
-					
-					case 'cloudinaryimage':
-						actionQueue.push(field.getRequestHandler(item, req, function(err) {
-							if (err)
-								req.flash('error', field.label + ' upload failed - ' + err.message);
-							progress();
-						}));
-					break;
-					
-					case 'password':
-						// passwords should only be set if a value is provided
-						if (!req.body[field.path])
-							return;
-						// validate matching password fields
-						if (req.body[field.path] != req.body[field.paths.confirm])
-							validationErrors.push('Passwords must match.');
-					break;
-					
-					case 'email':
-						if (req.body[field.path] && !utils.isEmail(req.body[field.path]))
-							validationErrors.push('Please enter a valid email address in the ' + field.label + ' field.');
-					break;
-					
-				}
-				
-				// validate required fields.
-				if (field.required && !field.validateInput(req.body))
-					validationErrors.push(field.label + ' is required.');
-				
-				field.updateItem(item, req.body);
-				
-				/*
-				TODO: Support for location field geocoding
-				switch (field.fieldType) {
-					case 'location':
-						if (_.has(req.body, field.path)) {
-							var ol = item.get(field.path);
-							var nl = req.body[field.path];
-							var lc = false;
-							if (!Array.isArray(nl.geo) || nl.geo.length != 2 || !Number(nl.geo[0]) || !Number(nl.geo[1])) {
-								delete nl.geo;
-							}
-							if (nl.geo && (ol.geo[0] != nl.geo[0] || ol.geo[1] != nl.geo[1])) {
-								lc = true;
-								// console.log('geo changed');
-								// console.log((ol.geo && !nl.geo) || (!ol.geo && nl.geo));
-							}
-							for (var i in ol) {
-								if (i != 'geo' && ol[i] != nl[i]) {
-									lc = true;
-									// console.log('location.' + i + ' changed');
-									break;
-								}
-							}
-							if (lc)
-								item.set(field.path, req.body[field.path]);
-							// console.log('Location changed: ' + lc);
-							if (_.has(req.body, field.path + '_improve')) {
-								actionQueue.push(function() {
-									item['googleGeocode_' + field.path](progress, false);
-								});
-							}
-						}
-				}
-				*/
+				req.flash('success', 'Your changes have been saved.');
+				return res.redirect('/keystone/' + req.list.path + '/' + item.id);
 			});
 			
-			progress();
 			
 		} else {
 			renderView();
