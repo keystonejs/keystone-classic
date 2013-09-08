cef7aeab32e323acff05e09f0b3f402ea3431fae
Jed Watson <jed.watson@me.com>
2013-09-08 19:45:06 +1000

    More utils foxes and tests, fixes #2

--- tests/utils.js
+++ tests/utils.js
@@ -1,5 +1,18 @@
+/**
+	These tests are currently designed to be run from the command line like
+	`/keystone/tests node utils`
+	
+	Currently in this format for expermientation with what the expected output
+	of each util *should* be, will be implemented as proper unit tests when
+	the api stabilises.
+*/
+
 var utils = require('../lib/utils'),
 	_dashes_ = '------------------------------';
+
+console.log('\n==============================');
+console.log('Running Tests:');
+console.log('==============================\n');
 
 console.log(_dashes_);
 console.log('Titlecase:')
@@ -26,9 +39,45 @@ keyToLabel('one');
 keyToLabel('oneTwo');
 keyToLabel('one_two');
 keyToLabel('oneTwoThree');
+keyToLabel('oneTWOThree');
 keyToLabel('one twoThree');
 keyToLabel('oneTwo.three');
+keyToLabel('oneTwo3four');
 keyToLabel('oneTwo-threeFour');
-keyToLabel('oneTwo:three;four');
+keyToLabel('id');
+keyToLabel('someId');
 
+console.log(_dashes_);
+console.log('Key to Path:')
+console.log(_dashes_);
+
+var keyToPath = function(str) {
+	console.log(str + ' --> ' + utils.keyToPath(str, true));
+}
+
+keyToPath('faq');
+keyToPath('FAQs');
+keyToPath('theFAQs');
+keyToPath('oneTwo');
+keyToPath('One_two');
+keyToPath('one2three');
+keyToPath('id');
+keyToPath('SomeId');
+
+console.log(_dashes_);
+console.log('Key to Property:')
+console.log(_dashes_);
+
+var keyToProperty = function(str) {
+	console.log(str + ' --> ' + utils.keyToProperty(str, true));
+}
+
+keyToProperty('faq');
+keyToProperty('FAQs');
+keyToProperty('theFAQs');
+keyToProperty('oneTwo');
+keyToProperty('One_two');
+keyToProperty('one2three');
+keyToProperty('id');
+keyToProperty('SomeId');
 
