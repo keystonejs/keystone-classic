c419f227d7fe5898cc022f9d011f8258c4980469
Jed Watson <jed.watson@me.com>
2013-09-15 20:33:47 +1000

    Adding layout for docs, guide view and cleaning up

--- docs/web.js
+++ docs/web.js
@@ -1,6 +1,16 @@
 var http = require('http'),
+	_ = require('underscore'),
 	express = require('express'),
-	jade = require('jade');
+	jade = require('jade'),
+	content = require('./content/index.json');
+
+function view(view, options) {
+	return function(req, res, next) {
+		res.render(view, options);
+	}
+}
+
+// Initialise app
 
 var app = express();
 
@@ -14,13 +24,24 @@ app.use(express.static('public'));
 
 app.use(express.logger('dev'));
 
-require('./lib/routes')(app);
+// Configure app locals and routes
+
+app.locals.nav = content.nav;
+delete content.nav;
+
+app.locals.version = require('../package.json').version;
+
+_.each(content, function(options, key) {
+	app.get(options.path, view(options.template, options));
+});
 
 app.use(function(req, res, next) {
 	res.status(404).send("Sorry, no page could be found at this address (404)");
 });
 
 app.use(express.errorHandler());
+
+// Start server
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Keystone docs are available on port ' + app.get('port'));
