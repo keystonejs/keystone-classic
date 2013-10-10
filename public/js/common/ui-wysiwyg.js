d4fb1fbb449c8e448bd74f321bbd914b45b34ee3
Tom Walker <bladey@me.com>
2013-10-11 08:39:18 +1100

    Adding ability to drag cloudinary image(s) fields into WYSIWYG editor.

--- public/js/common/ui-wysiwyg.js
+++ public/js/common/ui-wysiwyg.js
@@ -1,15 +1,41 @@
 jQuery(function($) {
 	
-	// TODO: This script file should really only be included on the item screen, discuss
-	if (!window.tinymce)
-		return;
-	
 	tinymce.init({
 		selector: 'textarea.wysiwyg',
 		menubar: false,
 		plugins: [ 'code', 'link' ],
 		toolbar: 'bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | link | code',
 		content_css: '/keystone/js/lib/tinymce/skins/lightgray/content.css'
+	});
+	
+	// Image Fields
+	$('.image-field').bind('dragstart', function(e) { // .img-draggable
+		
+		// Retrieve dragged data
+		var data = e.originalEvent.dataTransfer.getData('text/html'),
+			$el = $(data);
+		
+		// Detect invalid data
+		if (!data || !$el.find('.img-draggable'.length)) {
+			console.log('No valid draggable element detected.');
+			e.originalEvent.dataTransfer.setData('text/html', '');
+			return;
+		}
+		
+		var $imgSrc = $el.find('.img-draggable'),
+			imgData = $imgSrc.data();
+		
+		// Log data
+		console.log(imgData);
+		
+		// Construct standardised HTML
+		var imageHTML = '<a href="' + imgData.originalSrc + '" class="ui-popup">' + 
+				'<img src="' + imgData.dragSrc + '" />' +
+			'</a>';
+		
+		// Send back custom HTML
+		e.originalEvent.dataTransfer.setData('text/html', imageHTML);
+		
 	});
 	
 });
