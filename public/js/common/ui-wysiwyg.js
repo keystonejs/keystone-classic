c2c746395c43fff7864c0e59a59057653021752f
Jed Watson <jed.watson@me.com>
2013-10-10 16:48:31 +1100

    Cleaning up WYSIWYG Script

--- public/js/common/ui-wysiwyg.js
+++ public/js/common/ui-wysiwyg.js
@@ -12,48 +12,4 @@ jQuery(function($) {
 		content_css: '/keystone/js/lib/tinymce/skins/lightgray/content.css'
 	});
 	
-	// Cloudinary Images Integration
-	$('.field.type-cloudinaryimages').bind('dragstart', function(e) {
-		
-		// Retrieve dragged data
-		var data = e.originalEvent.dataTransfer.getData('text/html'),
-			$data = $(data);
-		
-		// Detect invalid data
-		if (!data || !$data.hasClass('image-preview')) {
-			console.log('No valid data detected.');
-			e.originalEvent.dataTransfer.setData('text/html', '');
-			return;
-		}
-		
-		// Log original data
-		// console.log(data);
-		
-		// Retrieve a and img tags
-		var $link = $data.find('a'),
-			link = $link.data();
-		
-		var $img = $data.find('img'),
-			img = $img.data();
-		
-		// Log data
-		console.log(link);
-		console.log(img);
-		
-		// Construct enlarged image
-		var imgSrc = 'http://res.cloudinary.com/do48r7mh6/image/upload/w_500,h_500,c_fit/' + link.id + '.jpg';
-		
-		// Construct standardised HTML
-		var imageHTML = '<a href="' + $link.prop('href') + '" class="ui-popup">' + 
-				'<img src="' + imgSrc + '" />' +
-			'</a>';
-		
-		// Log new data
-		// console.log(imageHTML);
-		
-		// Send back custom HTML
-		e.originalEvent.dataTransfer.setData('text/html', imageHTML);
-		
-	});
-	
 });
