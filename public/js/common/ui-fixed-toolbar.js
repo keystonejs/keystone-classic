4152c8909626275f44359eb9ec8ada4744afca87
Jed Watson <jed.watson@me.com>
2013-10-11 15:55:58 +1100

    Rewrote ui-fixed-toolbar to use position: fixed, resolves #11

--- public/js/common/ui-fixed-toolbar.js
+++ public/js/common/ui-fixed-toolbar.js
@@ -2,9 +2,7 @@ jQuery(function($) {
 	// fix toolbar
 	(function() {
 		
-		var viewY = 0, // the lowest visible pixel
-			maxY = 0,
-			$window = $(window),
+		var $window = $(window),
 			$body = $('#body'),
 			$toolbar = $('.toolbar-fixed');
 		
@@ -13,7 +11,7 @@ jQuery(function($) {
 		
 		$toolbar.wrap("<div class='toolbar-wrapper' style='position: relative'>");
 		
-		var toolbarHeight = $toolbar.outerHeight(),
+		var toolbarHeight = $toolbar.outerHeight() + 15, // add 15px for margin
 			$wrap = $toolbar.parent().css("height", toolbarHeight);
 		
 		$toolbar.css({
@@ -21,16 +19,34 @@ jQuery(function($) {
 			position: 'absolute'
 		});
 		
+		var mode = 'inline';
+		
 		var onScroll = function() {
-			maxY = $wrap.offset().top + toolbarHeight + 15;
-			viewY = $window.scrollTop() + $window.height();
-			$toolbar.css('top', (viewY > maxY) ? 0 : 0 - (maxY - viewY));
+			
+			var maxY = $wrap.offset().top + toolbarHeight,
+				viewY = $window.scrollTop() + $window.height()
+				
+			if (viewY > maxY && mode != 'inline') {
+				mode = 'inline';
+				$toolbar.css({
+					top: 0,
+					position: 'absolute'
+				});
+			} else if (viewY <= maxY && mode == 'inline') {
+				mode = 'fixed';
+				$toolbar.css({
+					top: $window.height() - toolbarHeight,
+					position: 'fixed'
+				});
+			}
+			
 		}
 		
 		$window.scroll(onScroll);
 		$window.resize(onScroll);
 		$window.on('redraw', onScroll);
 		onScroll();
+		
 		// do it again in a few hundred ms to correct for other UI initialisation
 		setTimeout(onScroll, 200);
 		
