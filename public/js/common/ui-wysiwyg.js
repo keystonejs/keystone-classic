3743491aac9f808c293153e5e72a742e722c80f1
Tom Walker <bladey@me.com>
2013-10-12 17:49:56 +1100

    Progress commit for draggable images.

--- public/js/common/ui-wysiwyg.js
+++ public/js/common/ui-wysiwyg.js
@@ -3,37 +3,50 @@ jQuery(function($) {
 	tinymce.init({
 		selector: 'textarea.wysiwyg',
 		menubar: false,
+		statusbar: false,
 		plugins: [ 'code', 'link' ],
 		toolbar: 'bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | link | code',
 		content_css: '/keystone/js/lib/tinymce/skins/lightgray/content.css'
 	});
 	
-	// Image Fields
-	$('.image-field').bind('dragstart', function(e) { // .img-draggable
+	// Draggable Image Fields (WIP)
+	$('.img-thumbnail[draggable=true]').bind('dragstart', function(e) { // .img-draggable
 		
 		// Retrieve dragged data
-		var data = e.originalEvent.dataTransfer.getData('text/html'),
-			$el = $(data);
+		var data = e.originalEvent.dataTransfer.getData('text/html');
 		
 		// Detect invalid data
-		if (!data || !$el.find('.img-draggable'.length)) {
-			console.log('No valid draggable element detected.');
+		if (!data) {
+			console.log('[wysiwyg.image.dragging] - No valid draggable element detected.');
 			e.originalEvent.dataTransfer.setData('text/html', '');
 			return;
 		}
 		
-		var $imgSrc = $el.find('.img-draggable'),
-			imgData = $imgSrc.data();
+		var $imgSrc = $(data).find('img');
+		
+		if (!$imgSrc.length) {
+			console.log('[wysiwyg.image.dragging] - No image element detected.');
+			e.originalEvent.dataTransfer.setData('text/html', '');
+			return;
+		}
+		
+		var imgData = $imgSrc.data();
+		
+		if (!imgData.originalSrc || !imgData.dragSrc) {
+			console.log('[wysiwyg.image.dragging] - No original or drag src detected on image.', imgData);
+			e.originalEvent.dataTransfer.setData('text/html', '');
+			return;
+		}
 		
 		// Log data
-		console.log(imgData);
+		console.log('[wysiwyg.image.dragging] - Image data:', imgData);
 		
 		// Construct standardised HTML
 		var imageHTML = '<a href="' + imgData.originalSrc + '" class="ui-popup">' + 
 				'<img src="' + imgData.dragSrc + '" />' +
 			'</a>';
 		
-		// Send back custom HTML
+		// Send back optimised HTML
 		e.originalEvent.dataTransfer.setData('text/html', imageHTML);
 		
 	});
