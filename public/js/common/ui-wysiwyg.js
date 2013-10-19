602fc6a6a906719c0600df9c293e59e8f92641ac
Tom Walker <bladey@me.com>
2013-10-19 11:23:25 +1100

    Adding images plugin detection/init to WYSIWYG init script.

--- public/js/common/ui-wysiwyg.js
+++ public/js/common/ui-wysiwyg.js
@@ -3,11 +3,19 @@ jQuery(function($) {
 	if (!window.tinymce)
 		return;
 	
-	tinymce.init({
+	var images = $('.field.type-cloudinaryimage,.field.type-cloudinaryimages').find('img[data-wysiwyg=true]');
+	
+	var options = {
 		selector: 'textarea.wysiwyg',
 		menubar: false,
-		plugins: [ 'code', 'link', 'images' ],
-		toolbar: 'bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | link | images code'
-	});
+		plugins: [ 'code', 'link', 'image', 'images',],
+		toolbar: 'bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | link ' + ( images.length ? '| images' : '' ) + ' | code',
+		skin: 'keystone',
+	};
+	
+	if (images.length)
+		options.images_list = images;
+	
+	tinymce.init(options);
 	
 });
