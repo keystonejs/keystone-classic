d27fb8ebb9840d424b79c39207732c5448173f68
Jed Watson <jed.watson@me.com>
2014-01-30 17:25:00 +1100

    Implementing initial form for location fields #147

---
+++ public/js/common/ui-location.js
@@ -0,0 +1,34 @@
+jQuery(function($) {
+	
+	$('.field.type-location').each(function() {
+		
+		var $field = $(this),
+			$extras = $field.find('.extras'),
+			visible = 0;
+		
+		$extras.each(function() {
+			var $this = $(this);
+			if (_.any($(this).find('input'), function(i) { return $(i).val() })) {
+				visible++;
+				$this.show();
+			}
+		});
+		
+		if (visible >= $extras.length) {
+			$field.find('.btn-show-extras').remove();
+		} else {
+			$field.find('.btn-show-extras').on('click', function() {
+				$(this).remove();
+				$field.find('.extras').show();
+				$(window).trigger('redraw');
+			})
+		}
+		
+		$field.find('.autoimprove').on('change', function() {
+			$field.find('.overwrite')[$field.find('.autoimprove input').prop('checked') ? 'show' : 'hide']();
+			$(window).trigger('redraw');
+		});
+		
+	});
+	
+});
