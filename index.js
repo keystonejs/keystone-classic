c7657814dd6f0450d81239b585a401039ed73e75
Jed Watson <jed.watson@me.com>
2014-02-23 01:35:31 +1100

    Improving Email test route binding and allowing functions for locals in email tests

--- index.js
+++ index.js
@@ -729,23 +729,37 @@ Keystone.prototype.bindEmailTestRoutes = function(app, emails) {
 	
 	var keystone = this;
 	
+	var handleError = function(req, res, err) {
+		if (res.err) {
+			res.err(err);
+		} else {
+			// TODO: Nicer default error handler
+			res.status(500).send(JSON.stringify(err));
+		}
+	}
+	
 	// TODO: Index of email tests, and custom email test 404's (currently bounces to list 404)
 	
 	_.each(emails, function(vars, key) {
 		
-		app.get('/keystone/test-email/' + key, function(req, res) {
-			new keystone.Email(key).render(vars, function(err, email) {
+		var render = function(err, req, res, locals) {
+			new keystone.Email(key).render(locals, function(err, email) {
 				if (err) {
-					if (res.err) {
-						res.err(err);
-					} else {
-						// TODO: Nicer default error handler
-						res.status(500).send(JSON.stringify(err));
-					}
+					handleError(req, res, err);
 				} else {
 					res.send(email.html);
 				}
 			});
+		}
+		
+		app.get('/keystone/test-email/' + key, function(req, res) {
+			if ('function' == typeof vars) {
+				vars(req, res, function(err, locals) {
+					render(err, req, res, locals);
+				});
+			} else {
+				render(null, req, res, vars);
+			}
 		});
 		
 	});
