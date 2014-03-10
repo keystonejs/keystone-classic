985929d25b60ba57fce36508fb0223ad8cc46290
Jed Watson <jed.watson@me.com>
2014-03-10 16:32:10 +1100

    Implementing optional custom signout redirect

--- routes/views/signout.js
+++ routes/views/signout.js
@@ -4,6 +4,15 @@ var keystone = require('../../'),
 exports = module.exports = function(req, res) {
 	
 	session.signout(req, res, function() {
+		
+		if ('string' == typeof keystone.get('signout redirect')) {
+			res.redirect(keystone.get('signout redirect'));
+		} else if ('function' == typeof keystone.get('signout redirect')) {
+			keystone.get('signout redirect')(req, res);
+		} else {
+			res.redirect('/keystone');
+		}
+		
 		keystone.render(req, res, 'signout', {
 			logo: keystone.get('signin logo')
 		});
