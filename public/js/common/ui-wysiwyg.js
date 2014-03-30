2d708bdd817d2f5dfbb70c05fed1dd82bb06c220
Jed Watson <jed.watson@me.com>
2014-03-30 21:11:53 +1100

    Implementing optional image and uploadimage plugins for TinyMCE

--- public/js/common/ui-wysiwyg.js
+++ public/js/common/ui-wysiwyg.js
@@ -3,11 +3,26 @@ jQuery(function($) {
 	if (!window.tinymce)
 		return;
 	
+	var plugins = [ 'code', 'link' ],
+		toolbar = 'bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | link';
+	
+	if (Keystone.wysiwyg.options.enableImages) {
+		plugins.push('image');
+		toolbar += ' | image';
+	}
+	
+	if (Keystone.wysiwyg.options.enableCloudinaryUploads) {
+		plugins.push('uploadimage');
+		toolbar += (Keystone.wysiwyg.options.enableImages) ? ' uploadimage' : ' | uploadimage';
+	}
+	
+	toolbar += ' | code';
+	
 	tinymce.init({
 		selector: 'textarea.wysiwyg',
 		menubar: false,
-		plugins: [ 'code', 'link', 'uploadimage' ],
-		toolbar: 'bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | link | code | uploadimage',
+		plugins: plugins,
+		toolbar: toolbar,
 		skin: 'keystone',
 		uploadimage_form_url: '/keystone/api/cloudinary/upload'
 	});
