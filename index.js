16a4151939dfe27f08bd7f75fc3b01d9b9097653
Jed Watson <jed.watson@me.com>
2014-03-30 19:28:07 +1100

    Cleaning up keystone.route logic and nesting cloudinary upload api under /keystone/api/

--- index.js
+++ index.js
@@ -812,11 +812,13 @@ Keystone.prototype.routes = function(app) {
 		this.nav = this.initNav();
 	}
 	
+	// Cache compiled view templates if we are in Production mode
 	this.set('view cache', this.get('env') == 'production');
 	
-	var auth = this.get('auth');
+	// Bind auth middleware (generic or custom) to /keystone* routes, allowing
+	// access to the generic signin page if generic auth is used
 	
-	if (auth === true) {
+	if (this.get('auth') === true) {
 		
 		if (!this.get('signout url')) {
 			this.set('signout url', '/keystone/signout');
@@ -833,8 +835,8 @@ Keystone.prototype.routes = function(app) {
 		app.all('/keystone/signout', require('./routes/views/signout'));
 		app.all('/keystone*', this.session.keystoneAuth);
 		
-	} else if ('function' == typeof auth) {
-		app.all('/keystone*', auth);
+	} else if ('function' == typeof this.get('auth')) {
+		app.all('/keystone*', this.get('auth'));
 	}
 	
 	var initList = function(protect) {
@@ -848,20 +850,31 @@ Keystone.prototype.routes = function(app) {
 		}
 	}
 	
+	// Keystone Admin Route
+	app.all('/keystone', require('./routes/views/home'));
+	
+	// Email test routes
 	if (this.get('email tests')) {
 		this.bindEmailTestRoutes(app, this.get('email tests'));
 	}
 	
-	app.all('/keystone', require('./routes/views/home'));
+	// Cloudinary API for image uploading (only if Cloudinary is configured)
+	if (keystone.get('cloudinary wysiwyg upload')) {
+		if (!keystone.get('cloudinary config')) {
+			throw new Error("KeystoneJS Initialisaton Error:\n\nTo use cloudinary wysiwyg upload, the 'cloudinary config' setting must be configured.\n\n");
+		}
+		app.post('/keystone/api/cloudinary/upload', require('./routes/api/cloudinary').upload);
+	}
 	
-	app.all('/keystone/download/:list', initList(), require('./routes/download/list'));
+	// Generic Lists API
 	app.all('/keystone/api/:list/:action', initList(), require('./routes/api/list'));
 	
+	// Generic Lists Download Route
+	app.all('/keystone/download/:list', initList(), require('./routes/download/list'));
+	
+	// List and Item Details Admin Routes
 	app.all('/keystone/:list/:page([0-9]{1,5})?', initList(true), require('./routes/views/list'));
 	app.all('/keystone/:list/:item', initList(true), require('./routes/views/item'));
-	
-	// image upload
-	app.all('/keystone/cloudinary/upload', require('./routes/api/cloudinary').upload);
 
 	return this;
 	
