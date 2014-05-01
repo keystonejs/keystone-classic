4913d2b34afd05589ea3a5467ad0c7ea55870a93
Hubert OG <hubert@orlikarnia.com>
2014-05-01 15:22:40 +0200

    Version with mocking

--- index.js
+++ index.js
@@ -139,9 +139,9 @@ var remappedOptions = {
 		case 'nav':
 			this.nav = this.initNav(value);
 		break;
-		case 'routes':
-			this.namedRoutes.map(value);
-		break;
+		// case 'routes':
+			// this.namedRoutes.map(value);
+		// break;
 	}
 
 	this._options[key] = value;
@@ -640,7 +640,9 @@ Keystone.prototype.start = function(events) {
 
 	// Configure application routes
 	if ('function' == typeof this.get('routes')) {
-		this.get('routes')(app, function(){});
+		this.namedRoutes.mock(app);
+		this.get('routes')(app);
+		this.namedRoutes.unmock(app);
 	}
 
 	// Connect to database
