750864d50e9c7acca4b0b39096723cf6e2c35947
fbeaudet <fredericbeaudet@gmail.com>
2014-05-13 19:06:23 -0400

    Security fix: handled ECONNRESET

--- index.js
+++ index.js
@@ -905,13 +905,23 @@ Keystone.prototype.start = function(events) {
 		}
 
 		process.on('uncaughtException', function(e) {
+
 			if (e.code === 'EADDRINUSE') {
 				console.log('------------------------------------------------\n' +
 					keystone.get('name') + ' failed to start: address already in use\n' +
 					'Please check you are not already running a server on the specified port.');
 				process.exit();
+				throw (e);
+
+			} else if (e.code === 'ECONNRESET') {
+				//Connection reset by peer, ignore it instead of exiting server with a throw.
+				console.log('Connection reset by peer');
+				console.log(e);
+
+			} else {
+
+				throw (e);
 			}
-			throw (e);
 		});
 	};
 
