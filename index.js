e8b4ad35a12ea1314c596823fc62beaa1fbd6bd6
Jed Watson <jed.watson@me.com>
2014-05-14 20:46:34 +1000

    Minor cleanup

--- index.js
+++ index.js
@@ -903,26 +903,22 @@ Keystone.prototype.start = function(events) {
 		} else {
 			waitForServers--;
 		}
-
+		
 		process.on('uncaughtException', function(e) {
-
 			if (e.code === 'EADDRINUSE') {
 				console.log('------------------------------------------------\n' +
 					keystone.get('name') + ' failed to start: address already in use\n' +
 					'Please check you are not already running a server on the specified port.');
 				process.exit();
-				throw (e);
-
 			} else if (e.code === 'ECONNRESET') {
-				//Connection reset by peer, ignore it instead of exiting server with a throw.
+				// Connection reset by peer, ignore it instead of exiting server with a throw.
 				console.log('Connection reset by peer');
 				console.log(e);
-
 			} else {
-
 				throw (e);
 			}
 		});
+		
 	};
 
 	//mount the express app
