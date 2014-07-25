abac1653ce9d5507891c6ae08f83c4e7023eba7c
Jed Watson <jed.watson@me.com>
2014-07-25 14:48:23 +1000

    Adding tests for fields.location.updateItem

--- test/fields.js
+++ test/fields.js
@@ -15,7 +15,8 @@ before(function() {
 	Test.add({
 		date: Types.Date,
 		datetime: Types.Datetime,
-		bool: Types.Boolean
+		bool: Types.Boolean,
+		location: Types.Location
 	});
 	
 	Test.register();
@@ -49,19 +50,77 @@ describe("Fields", function() {
 	/** FieldType: Boolean */
 	describe("Boolean", function() {
 		
-		it('should update it\'s model if data passed is boolean true', function() {
+		it('should be true when passed the boolean true', function() {
 			Test.fields.bool.updateItem(testItem, {
-				'bool': true
+				bool: true
 			});
 			demand(testItem.bool).to.be.true();
 		});
 		
-	   	it('should update it\'s  model if data passed is string \'true\'', function() {
+		it('should be true when passed the string "true"', function() {
 			Test.fields.bool.updateItem(testItem, {
-				'bool': 'true'
+				bool: 'true'
 			});
 			demand(testItem.bool).to.be.true();
 		});
-	   	
+		
+	});
+	
+	/** FieldType: Boolean */
+	describe("Location", function() {
+		
+		it('should update its value from flat paths', function() {
+			Test.fields.location.updateItem(testItem, {
+				'location.number': 'number',
+				'location.name': 'name',
+				'location.street1': 'street 1',
+				'location.street2': 'street 2',
+				'location.suburb': 'suburb',
+				'location.state': 'state',
+				'location.postcode': 'postcode',
+				'location.country': 'country',
+				'location.geo_lat': '-33.865143',
+				'location.geo_lng': '151.2099'
+			});
+			demand(testItem.location.number).to.be('number');
+			demand(testItem.location.name).to.be('name');
+			demand(testItem.location.street1).to.be('street 1');
+			demand(testItem.location.street2).to.be('street 2');
+			demand(testItem.location.suburb).to.be('suburb');
+			demand(testItem.location.state).to.be('state');
+			demand(testItem.location.postcode).to.be('postcode');
+			demand(testItem.location.country).to.be('country');
+			demand(Array.isArray(testItem.location.geo)).to.be.true();
+			demand(testItem.location.geo[0]).to.be(151.2099);
+			demand(testItem.location.geo[1]).to.be(-33.865143);
+		});
+		
+		it('should update its value from nested paths', function() {
+			Test.fields.location.updateItem(testItem, {
+				location: {
+					number: 'number',
+					name: 'name',
+					street1: 'street 1',
+					street2: 'street 2',
+					suburb: 'suburb',
+					state: 'state',
+					postcode: 'postcode',
+					country: 'country',
+					geo: ['151.2099', '-33.865143']
+				}
+			});
+			demand(testItem.location.number).to.be('number');
+			demand(testItem.location.name).to.be('name');
+			demand(testItem.location.street1).to.be('street 1');
+			demand(testItem.location.street2).to.be('street 2');
+			demand(testItem.location.suburb).to.be('suburb');
+			demand(testItem.location.state).to.be('state');
+			demand(testItem.location.postcode).to.be('postcode');
+			demand(testItem.location.country).to.be('country');
+			demand(Array.isArray(testItem.location.geo)).to.be.true();
+			demand(testItem.location.geo[0]).to.be(151.2099);
+			demand(testItem.location.geo[1]).to.be(-33.865143);
+		});
+		
 	});
 });
