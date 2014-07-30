c717ebc3247ffa5da35de307abc938ec6f2694b3
Emmanuel Nelson <mnelson@onenorth.com>
2014-07-30 07:14:35 -0500

    Fixing Indentation

--- public/js/common/ui-wysiwyg.js
+++ public/js/common/ui-wysiwyg.js
@@ -22,44 +22,44 @@ jQuery(function($) {
 	        toolbar += (' | ' + additionalButtons[i]);
 	    }
 	}
-        if (Keystone.wysiwyg.options.additionalPlugins) {
-            var additionalPlugins = Keystone.wysiwyg.options.additionalPlugins.split(',');
-            for (var i=0; i<additionalPlugins.length; i++) {
-                plugins.push(additionalPlugins[i]);
-            }   
+    if (Keystone.wysiwyg.options.additionalPlugins) {
+        var additionalPlugins = Keystone.wysiwyg.options.additionalPlugins.split(',');
+        for (var i=0; i<additionalPlugins.length; i++) {
+            plugins.push(additionalPlugins[i]);
         }
+    }
 	toolbar += ' | code';
 
         //init editable wysiwygs
 	var tinymceOptions = {
-	selector: 'textarea.wysiwyg',
-	menubar: false,
-	plugins: plugins,
-	toolbar: toolbar,
-	skin: 'keystone',
-	uploadimage_form_url: '/keystone/api/cloudinary/upload'
+        selector: 'textarea.wysiwyg',
+        menubar: false,
+        plugins: plugins,
+        toolbar: toolbar,
+        skin: 'keystone',
+        uploadimage_form_url: '/keystone/api/cloudinary/upload'
 	};
 
 	if(Keystone.wysiwyg.options.additionalOptions){
-	$.extend(tinymceOptions,Keystone.wysiwyg.options.additionalOptions);
+    	$.extend(tinymceOptions,Keystone.wysiwyg.options.additionalOptions);
 	}
 	
 	tinymce.init(tinymceOptions);
 	
 	//init non-editable wysiwygs
 	var tinymceOptionsNonEditable = {
-	selector: 'textarea.wysiwyg-noedit',
-	mode: 'textareas',
-	readonly: true,
-	menubar: false,
-	plugins: plugins,
-	toolbar: 'code',
-	statusbar: false,
-	skin: 'keystone'
+        selector: 'textarea.wysiwyg-noedit',
+        mode: 'textareas',
+        readonly: true,
+        menubar: false,
+        plugins: plugins,
+        toolbar: 'code',
+        statusbar: false,
+        skin: 'keystone'
 	};
 	
 	if(Keystone.wysiwyg.options.additionalOptions){
-	$.extend(tinymceOptionsNonEditable,Keystone.wysiwyg.options.additionalOptions);
+    	$.extend(tinymceOptionsNonEditable,Keystone.wysiwyg.options.additionalOptions);
 	}
 	
 	
