51d81910d980a366cd30c530411d785674909988
Jed Watson <jed.watson@me.com>
2014-08-13 02:18:06 +1000

    Adding email component

--- admin/src/fields.js
+++ admin/src/fields.js
@@ -1,4 +1,5 @@
 module.exports = {
 	text: require('../../fields/types/text/component'),
-	boolean: require('../../fields/types/boolean/component')
+	boolean: require('../../fields/types/boolean/component'),
+	email: require('../../fields/types/email/component')
 };
---
+++ fields/types/email/component.js
@@ -0,0 +1,39 @@
+/** @jsx React.DOM */
+
+var React = require('react'),
+	Note = require('../../components/note');
+
+module.exports = React.createClass({
+	
+	/*
+		TODO:
+		- gravatar
+	 */
+	
+	valueChanged: function(event) {
+		this.props.onChange({
+			path: this.props.path,
+			value: event.target.value
+		});
+	},
+	
+	render: function() {
+		
+		var fieldClassName = 'field-ui width-' + this.props.width;
+		
+		var input = this.props.noedit ?
+			<div className="field-value">{this.props.value}</div> :
+			<input type="text" name={this.props.path} value={this.props.value} onChange={this.valueChanged} autoComplete="off" className="form-control" />;
+		
+		return (
+			<div className="field type-email">
+				<label className="field-label">{this.props.label}</label>
+				<div className={fieldClassName}>
+					{input}
+					<Note note={this.props.note} />
+				</div>
+			</div>
+		);
+	}
+	
+});
