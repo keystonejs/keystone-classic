a0eca6f24d818afa54f592f42bb75c38bf54620a
Jed Watson <jed.watson@me.com>
2014-08-19 13:19:18 +1000

    Fixing double-escaping of html in notes

--- fields/components/note.js
+++ fields/components/note.js
@@ -5,11 +5,9 @@ var React = require('react');
 module.exports = React.createClass({
 	
 	render: function() {
-		
 		return this.props.note ? 
-			<div className="field-note">{this.props.note}</div> :
+			<div className="field-note" dangerouslySetInnerHTML={{ __html: this.props.note }} /> :
 			null;
-		
 	}
 	
 });
