8ab256859ee288120e576cebdc5780d370db4b24
Jed Watson <jed.watson@me.com>
2014-08-19 15:14:55 +1000

    Adding location field type component

--- admin/src/fields.js
+++ admin/src/fields.js
@@ -1,5 +1,6 @@
 module.exports = {
 	text: require('../../fields/types/text/component'),
 	boolean: require('../../fields/types/boolean/component'),
-	email: require('../../fields/types/email/component')
+	email: require('../../fields/types/email/component'),
+	location: require('../../fields/types/location/component')
 };
