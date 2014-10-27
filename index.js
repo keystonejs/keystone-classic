bab3c7404b9d6c6fe6362630982ed114256eb66c
Jed Watson <jed.watson@me.com>
2014-10-27 12:46:01 +1100

    Refactoring more into /lib/core

--- index.js
+++ index.js
@@ -140,8 +140,10 @@ Keystone.prototype.start = require('./lib/core/start');
 Keystone.prototype.mount = require('./lib/core/mount');
 Keystone.prototype.routes = require('./lib/core/routes');
 Keystone.prototype.static = require('./lib/core/static');
+Keystone.prototype.render = require('./lib/core/render');
 Keystone.prototype.importer = require('./lib/core/importer');
 Keystone.prototype.createItems = require('./lib/core/createItems');
+Keystone.prototype.populateRelated = require('./lib/core/populateRelated');
 Keystone.prototype.redirect = require('./lib/core/redirect');
 Keystone.prototype.list = require('./lib/core/list');
 Keystone.prototype.getOrphanedLists = require('./lib/core/getOrphanedLists');
@@ -225,139 +227,6 @@ Keystone.prototype.applyUpdates = function(callback) {
 	require('./lib/updates').apply(callback);
 };
 
-
-/**
- * Renders a Keystone View
- *
- * @api private
- */
-
-Keystone.prototype.render = function(req, res, view, ext) {
-	
-	var keystone = this;
-		
-	var templatePath = __dirname + '/templates/views/' + view + '.jade';
-	
-	var jadeOptions = {
-		filename: templatePath,
-		pretty: keystone.get('env') !== 'production'
-	};
-	
-	// TODO: Allow custom basePath for extensions... like this or similar
-	// if (keystone.get('extensions')) {
-	// 	jadeOptions.basedir = keystone.getPath('extensions') + '/templates';
-	// }
-	
-	var compileTemplate = function() {
-		return jade.compile(fs.readFileSync(templatePath, 'utf8'), jadeOptions);
-	};
-	
-	var template = this.get('viewCache')
-		? templateCache[view] || (templateCache[view] = compileTemplate())
-		: compileTemplate();
-		
-	var flashMessages = {
-		info: res.req.flash('info'),
-		success: res.req.flash('success'),
-		warning: res.req.flash('warning'),
-		error: res.req.flash('error'),
-		hilight: res.req.flash('hilight')
-	};
-	
-	var locals = {
-		_: _,
-		moment: moment,
-		numeral: numeral,
-		env: this.get('env'),
-		brand: keystone.get('brand'),
-		nav: keystone.nav,
-		messages: _.any(flashMessages, function(msgs) { return msgs.length; }) ? flashMessages : false,
-		lists: keystone.lists,
-		js: 'javascript:;',
-		utils: utils,
-		User: keystone.list(keystone.get('user model')),
-		user: req.user,
-		title: 'Keystone',
-		signout: this.get('signout url'),
-		backUrl: this.get('back url') || '/',
-		section: {},
-		version: this.version,
-		csrf_token_key: keystone.security.csrf.TOKEN_KEY,
-		csrf_token_value: keystone.security.csrf.getToken(req, res),
-		csrf_query: '&' + keystone.security.csrf.TOKEN_KEY + '=' + keystone.security.csrf.getToken(req, res),
-		ga: {
-			property: this.get('ga property'),
-			domain: this.get('ga domain')
-		},
-		wysiwygOptions: {
-			enableImages: keystone.get('wysiwyg images') ? true : false,
-			enableCloudinaryUploads: keystone.get('wysiwyg cloudinary images') ? true : false,
-			additionalButtons: keystone.get('wysiwyg additional buttons') || '',
-			additionalPlugins: keystone.get('wysiwyg additional plugins') || '',
-			additionalOptions: keystone.get('wysiwyg additional options') || {},
-			overrideToolbar: keystone.get('wysiwyg override toolbar'),
-			skin: keystone.get('wysiwyg skin') || 'keystone',
-			menubar: keystone.get('wysiwyg menubar')
-		}
-	};
-	
-	// optional extensions to the local scope
-	_.extend(locals, ext);
-	
-	// add cloudinary locals if configured
-	if (keystone.get('cloudinary config')) {
-		try {
-			var cloudinaryUpload = cloudinary.uploader.direct_upload();
-			locals.cloudinary = {
-				cloud_name: keystone.get('cloudinary config').cloud_name,
-				api_key: keystone.get('cloudinary config').api_key,
-				timestamp: cloudinaryUpload.hidden_fields.timestamp,
-				signature: cloudinaryUpload.hidden_fields.signature,
-				prefix: keystone.get('cloudinary prefix') || '',
-				folders: keystone.get('cloudinary folders'),
-				uploader: cloudinary.uploader
-			};
-			locals.cloudinary_js_config = cloudinary.cloudinary_js_config();
-		} catch(e) {
-			if (e === 'Must supply api_key') {
-				throw new Error('Invalid Cloudinary Config Provided\n\n' +
-					'See http://keystonejs.com/docs/configuration/#services-cloudinary for more information.');
-			} else {
-				throw e;
-			}
-		}
-	}
-	
-	// fieldLocals defines locals that are provided to each field's `render` method
-	locals.fieldLocals = _.pick(locals, '_', 'moment', 'numeral', 'env', 'js', 'utils', 'user', 'cloudinary');
-	
-	var html = template(_.extend(locals, ext));
-	
-	res.send(html);
-};
-
-/**
- * Populates relationships on a document or array of documents
- *
- * WARNING: This is currently highly inefficient and should only be used in development, or for
- * small data sets. There are lots of things that can be done to improve performance... later.
- *
- * @api public
- */
-
-Keystone.prototype.populateRelated = function(docs, relationships, callback) {
-	
-	if (Array.isArray(docs)) {
-		async.each(docs, function(doc, done) {
-			doc.populateRelated(relationships, done);
-		}, callback);
-	} else if (docs && docs.populateRelated) {
-		docs.populateRelated(relationships, callback);
-	} else {
-		callback();
-	}
-
-};
 
 /**
  * Logs a configuration error to the console
