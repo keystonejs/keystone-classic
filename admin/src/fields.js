cda564bac5b0404afc09a9691bde05399591be41
Morgan Craft <mgan59@gmail.com>
2014-11-18 00:05:28 -0500

    added support for markdown field

--- admin/src/fields.js
+++ admin/src/fields.js
@@ -4,6 +4,7 @@ module.exports = {
 	datetime:	require('../../fields/types/datetime/datetime'),
 	email:		require('../../fields/types/email/email'),
 	location:	require('../../fields/types/location/location'),
+	markdown:	require('../../fields/types/markdown/markdown'),
 	name:		require('../../fields/types/name/name'),
 	select:		require('../../fields/types/select/select'),
 	text:		require('../../fields/types/text/text')
