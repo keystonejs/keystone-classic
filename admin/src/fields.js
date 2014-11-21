44ad639d172abb1b144d747f3ae2b60bfde3088a
Sebastian McKenzie <sebmck@gmail.com>
2014-11-21 16:21:12 +1100

    Add cloudinaryimages react component

--- admin/src/fields.js
+++ admin/src/fields.js
@@ -1,13 +1,14 @@
 module.exports = {
-	boolean:         require('../../fields/types/boolean/boolean'),
-	date:            require('../../fields/types/date/date'),
-	datetime:        require('../../fields/types/datetime/datetime'),
-	email:           require('../../fields/types/email/email'),
-	location:        require('../../fields/types/location/location'),
-	markdown:        require('../../fields/types/markdown/markdown'),
-	name:            require('../../fields/types/name/name'),
-	password:        require('../../fields/types/password/password'),
-	select:          require('../../fields/types/select/select'),
-	text:            require('../../fields/types/text/text'),
-	cloudinaryimage: require('../../fields/types/cloudinaryimage/cloudinaryimage')
+	boolean:          require('../../fields/types/boolean/boolean'),
+	date:             require('../../fields/types/date/date'),
+	datetime:         require('../../fields/types/datetime/datetime'),
+	email:            require('../../fields/types/email/email'),
+	location:         require('../../fields/types/location/location'),
+	markdown:         require('../../fields/types/markdown/markdown'),
+	name:             require('../../fields/types/name/name'),
+	password:         require('../../fields/types/password/password'),
+	select:           require('../../fields/types/select/select'),
+	text:             require('../../fields/types/text/text'),
+	cloudinaryimage:  require('../../fields/types/cloudinaryimage/cloudinaryimage'),
+	cloudinaryimages: require('../../fields/types/cloudinaryimages/cloudinaryimages')
 };
