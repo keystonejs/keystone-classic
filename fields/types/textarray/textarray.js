7939212bd3485be68d2e73e78a79dc68224b6ba6
Jed Watson <jed.watson@me.com>
2014-12-17 15:57:06 +1100

    Fixing issues with the TextArray React component (ref #784)

--- fields/types/textarray/textarray.js
+++ fields/types/textarray/textarray.js
@@ -2,36 +2,58 @@ var _ = require('underscore'),
 	Field = require('../field'),
 	React = require('react');
 
+var lastId = 0;
+
+function newItem(value) {
+	lastId = lastId + 1;
+	return { key: 'i' + lastId, value: value };
+}
+
 module.exports = Field.create({
-	getInitialState: function () {
+	getInitialState: function() {
 		return {
-			value: this.props.value
+			values: this.props.value.map(newItem)
 		};
 	},
-
-	addItem: function () {
-		this.state.value.push("");
-		this.forceUpdate();
+	
+	addItem: function() {
+		this.setState({
+			values: this.state.values.concat(newItem(''))
+		});
 	},
-
-	removeItem: function (i) {
-		this.state.value.splice(i, 1);
-		this.forceUpdate();
+	
+	removeItem: function(i) {
+		this.setState({
+			values: _.without(this.state.values, i)
+		});
 	},
-
+	
+	updateItem: function(i, event) {
+		var updatedValues = this.state.values;
+		var updateIndex = updatedValues.indexOf(i);
+		updatedValues[updateIndex].value = event.target.value;
+		this.setState({
+			values: updatedValues
+		});
+	},
+	
+	renderItem: function(i) {
+		return (
+			<div key={i.key} className='field-item'>
+				<input className='form-control multi' type='text' name={this.props.path} value={i.value} onChange={this.updateItem.bind(this, i)} autoComplete='off' />
+				<a className='btn btn-link btn-cancel' onClick={this.removeItem.bind(this, i)}>
+					<span className='ion-close-round' />
+				</a>
+			</div>
+		);
+	},
+	
 	renderField: function () {
-		var self = this;
-
-		return <div>
-			{this.state.value.map(function (value, i) {
-				return <div key={self.props.path + '-' + i} className='field-item'>
-					<input className='form-control multi' type='text' name={self.props.path} defaultValue={value} autoComplete='off' />
-					<a className='btn btn-link btn-cancel btn-remove-item' onClick={self.removeItem.bind(self, i)}>
-						<span className='ion-close-round' />
-					</a>
-				</div>;
-			})}
-			<a className='btn btn-xs btn-default btn-add-item' onClick={this.addItem}>Add item</a>
-		</div>;
+		return (
+			<div>
+				{this.state.values.map(this.renderItem)}
+				<a className='btn btn-xs btn-default' onClick={this.addItem}>Add item</a>
+			</div>
+		);
 	}
 });
