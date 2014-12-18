a47068144985498821ba9b364fc62b28900643fa
Jed Watson <jed.watson@me.com>
2014-12-18 16:01:17 +1100

    Stripping non-numeric chars from Number field input (#792)

--- fields/types/number/number.js
+++ fields/types/number/number.js
@@ -7,6 +7,15 @@ module.exports = Field.create({
 		width: true
 	},
 	
+	valueChanged: function(event) {
+		var newValue = event.target.value.replace(/[^\d]/g, '');
+		if (newValue === this.props.value) return;
+		this.props.onChange({
+			path: this.props.path,
+			value: newValue
+		});
+	},
+	
 	renderField: function() {
 		return <input type="text" name={this.props.path} ref="focusTarget" value={this.props.value} onChange={this.valueChanged} autoComplete="off" className="form-control" />;
 	}
