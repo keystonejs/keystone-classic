ebfa03f3913446e292386d600c0d7e3a1d3b7196
Sebastian McKenzie <sebmck@gmail.com>
2014-12-24 11:48:10 +1100

    Delegate testing tasks from gulp to a a much simpler Makefile

---
+++ Makefile
@@ -0,0 +1,25 @@
+ISTANBUL_CMD = node_modules/istanbul/lib/cli.js cover
+JSHINT_CMD = node_modules/jshint/bin/jshint
+MOCHA_CMD = node_modules/mocha/bin/_mocha
+
+export NODE_ENV = test
+
+.PHONY: lint test test-cov test-spec test-travis
+
+lint:
+	$(JSHINT_CMD) --reporter node_modules/jshint-stylish/stylish.js .
+
+test:
+	make lint
+	$(MOCHA_CMD)
+
+test-cov:
+	rm -rf coverage
+	node $(ISTANBUL_CMD) $(MOCHA_CMD) --
+
+test-spec:
+	node $(ISTANBUL_CMD) $(MOCHA_CMD) -- --reporter spec
+
+test-travis:
+	make test-spec
+	if test -n "$$CODECLIMATE_REPO_TOKEN"; then codeclimate < coverage/lcov.info; fi
--- gulpfile.js
+++ gulpfile.js
@@ -13,19 +13,6 @@ var _ = require('underscore'),
 	chalk = require('chalk'),
 	del = require('del');
 
-// Common project paths
-var paths = {
-	'src': ['./index.js', './lib/**/*.js','./routes/**/*.js'],
-	'tests': ['./test/**/*.js']
-};
-
-// An error handler for the tests during gulp-watch
-// Otherwise the gulp-watch will terminate
-var handleError = function(err) {
-	console.log(chalk.red(err.name + ': ' + err.plugin + ' - ' + err.message));
-	return;
-};
-
 /**
  * Build Tasks
  */
@@ -87,67 +74,4 @@ gulp.task('watch-scripts', function() {
 	
 	return rebundle();
 	
-});
-
-
-/**
- * Testing & Linting Tasks
- */
-
-// lint source with jshint
-gulp.task('lint', function(){
-	return gulp.src(paths.src)
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
-
-});
-
-// run the mocha tests with the default dot reporter
-gulp.task('test', function(){
-	return gulp.src(paths.tests)
-		.pipe(mocha({
-			reporter: 'dot'
-		}))
-		.on('error', handleError);
-
-});
-
-// run the mocha tests with the spec reporter
-gulp.task('spec', function(){
-	return gulp.src(paths.tests)
-		.pipe(mocha({
-			reporter: 'spec'
-		}))
-		.on('error', handleError);
-});
-
-// generate a coverage report
-gulp.task('coverage', function(){
-	return gulp.src(paths.tests)
-		.pipe(cover.instrument({
-			pattern: paths.src,
-			debugDirectory: '.coverdebug'
-		}))
-		.pipe(mocha({
-			reporter: 'spec'
-		}))
-		.pipe(cover.report({
-			outFile: 'coverage.html'
-		}))
-		.on('error', handleError);
-});
-
-// delete the coverage report
-gulp.task('clean-coverage', function(done){
-	del(['.coverdebug', '.coverdata', '.coverrun', 'coverage.html'], done);
-});
-
-
-/*
- * auto/watch gulp tasks that will trigger the tests on
- * file changes
- */
-
-gulp.task('autotest', function(){
-	gulp.watch(paths.src.concat(paths.tests), ['test']);
 });
