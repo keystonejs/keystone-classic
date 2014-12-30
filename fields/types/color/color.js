266057b1afea2eb5e8669c9bce0b2063d49d21a6
Jed Watson <jed.watson@me.com>
2014-12-30 21:11:09 +1100

    Improving color field

--- fields/types/color/color.js
+++ fields/types/color/color.js
@@ -1,14 +1,45 @@
 var React = require('react'),
-	Field = require('../field'),
-	$     = require('jquery');
+	Field = require('../field');
 
-require('./lib/bootstrap-colorpicker');
+// require('./lib/bootstrap-colorpicker');
 
 module.exports = Field.create({
+	
+	valueChanged: function(event) {
+		var newValue = event.target.value;
+		if (/^([0-9A-F]{3}){1,2}$/.test(newValue)) {
+			newValue = '#' + newValue;
+		}
+		if (newValue === this.props.value) return;
+		console.log(newValue);
+		this.props.onChange({
+			path: this.props.path,
+			value: newValue
+		});
+	},
+	
 	renderField: function() {
-		return [
-			<input ref='field' type='text' className='form-control' onChange={this.valueChanged} name={this.props.path} value={this.props.value} autoComplete='off' />,
-			<div className='color-preview' style={ { color: this.props.value } } />
-		];
+		
+		var colorPreview = null;
+		
+		if (this.props.value) {
+			colorPreview = <div style={{
+				position: 'absolute',
+				top: 5,
+				right: 20,
+				width: 24,
+				height: 24,
+				borderRadius: 5,
+				background: this.props.value
+			}} />;
+		}
+		
+		return (
+			<div>
+				<input ref='field' type='text' className='form-control' onChange={this.valueChanged} name={this.props.path} value={this.props.value} autoComplete='off' />
+				{colorPreview}
+			</div>
+		);
 	}
+	
 });
