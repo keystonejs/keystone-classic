a591fe78d4a10da9a22475c4cb14f204e7fdd08f
Snowkeeper <snowkeeper@snowpi.org>
2015-01-02 23:24:57 -0500

    add back fixed toolbar

--- admin/src/components/toolbar.js
+++ admin/src/components/toolbar.js
@@ -6,9 +6,73 @@ var React = require('react');
  */
 
 var Toolbar = React.createClass({
+	componentDidMount: function() {
+		(function() {
+		
+			var $window = $(window),
+				$body = $('#body'),
+				$toolbar = $('.toolbar');
+			
+			if (!$toolbar.length)
+				return;
+			
+			$toolbar.wrap("<div class='toolbar-wrapper' style='position: relative'>");
+			
+			var toolbarHeight = $toolbar.outerHeight() + 15, // add 15px for margin
+				$wrap = $toolbar.parent().css("height", toolbarHeight);
+			
+			$toolbar.css({
+				width: $toolbar.outerWidth(),
+				position: 'absolute'
+			});
+			
+			var mode = 'inline',
+				windowSize = {
+					x: $window.width(),
+					y: $window.height()
+				};
+			
+			var onScroll = function() {
+				
+				var maxY = $wrap.offset().top + toolbarHeight,
+					viewY = $window.scrollTop() + $window.height(),
+					newSize = {
+						x: $window.width(),
+						y: $window.height()
+					},
+					sizeChanged = (newSize.x != windowSize.x || newSize.y != windowSize.y);
+				
+				if (viewY > maxY && (sizeChanged || mode != 'inline')) {
+					mode = 'inline';
+					windowSize = newSize;
+					$toolbar.css({
+						top: 0,
+						position: 'absolute'
+					});
+				} else if (viewY <= maxY && (sizeChanged || mode == 'inline')) {
+					mode = 'fixed';
+					windowSize = newSize;
+					$toolbar.css({
+						top: $window.height() - toolbarHeight,
+						position: 'fixed'
+					});
+				}
+				
+			};
+			
+			$window.scroll(onScroll);
+			$window.resize(onScroll);
+			$window.on('redraw', onScroll);
+			onScroll();
+			
+			// do it again in a few hundred ms to correct for other UI initialisation
+			setTimeout(onScroll, 200);
+			
+		})();
+	},
 	render: function() {
 		return (
-			<div className="toolbar">{this.props.children}</div>
+			<div className="toolbar " >{this.props.children}</div>
 		);
 	}
 });
