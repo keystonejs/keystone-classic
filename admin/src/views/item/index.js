f276f7f1a1dfea099eaf6e7c9fbbe1855addb22e
Jed Watson <jed.watson@me.com>
2015-01-06 23:46:14 +1100

    New React item details view

---
+++ admin/src/views/item/index.js
@@ -0,0 +1,10 @@
+var React = require('react'),
+	Form = require('./Form'),
+	Toolbar = require('./Toolbar');
+
+module.exports = {
+	render: function(view) {
+		React.render(<Toolbar list={view.list} drilldown={view.drilldown} />, document.getElementById('item-toolbar'));
+		React.render(<Form list={view.list} data={view.item} />, document.getElementById('item-form'));
+	}
+};
