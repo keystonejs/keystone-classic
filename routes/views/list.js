4fd6bebb30378f51369f45e0c11f1a5447be2154
David Banham <david@banham.id.au>
2015-01-12 16:11:16 +1100

    Log error on CSRF failure

--- routes/views/list.js
+++ routes/views/list.js
@@ -139,6 +139,7 @@ exports = module.exports = function(req, res) {
 	var checkCSRF = function() {
 		var pass = keystone.security.csrf.validate(req);
 		if (!pass) {
+			console.error('CSRF failure');
 			req.flash('error', 'There was a problem with your request, please try again.');
 		}
 		return pass;
