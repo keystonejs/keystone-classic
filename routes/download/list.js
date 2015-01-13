c48c56b033edfd6b56641be379fafddb626497df
gilles <gilles@pilloud.fr>
2015-01-13 19:42:08 +0100

    reindentation for #748

--- routes/download/list.js
+++ routes/download/list.js
@@ -68,17 +68,17 @@ exports = module.exports = function(req, res) {
 
 		var sendCSV = function(data) {
 
-		var columns = data.length ? Object.keys(data[0]) : [];
-		res.attachment(req.list.path + '-' + moment().format('YYYYMMDD-HHMMSS') + '.csv')
-		res.setHeader('Content-Type', 'application/octet-stream');
+			var columns = data.length ? Object.keys(data[0]) : [];
+			res.attachment(req.list.path + '-' + moment().format('YYYYMMDD-HHMMSS') + '.csv')
+			res.setHeader('Content-Type', 'application/octet-stream');
 
-
-		csv().from(data).to.options({
-			header: true,
-			columns: columns,
-			delimiter: keystone.get('csv field delimiter') || ','
-		}).to.string(function(data) {   res.end("\ufeff"+data, 'utf-8'); });
-
+			csv().from(data).to.options({
+				header: true,
+				columns: columns,
+				delimiter: keystone.get('csv field delimiter') || ','
+			}).to.string(function(data) {
+				res.end("\ufeff" + data, 'utf-8');
+			});
 		};
 
 		if (!results.length) {
