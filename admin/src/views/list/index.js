dd252c588766e71a2cf116fd7f3a40ab3c432b43
Jed Watson <jed.watson@me.com>
2015-01-14 23:56:52 +1100

    Adding React-powered Create Item to List view

---
+++ admin/src/views/list/index.js
@@ -0,0 +1,61 @@
+var React = require('react'),
+	CreateForm = require('../../components/CreateForm');
+
+var View = React.createClass({
+	
+	// TODO: Handle auto-display of create form when validation failed
+	
+	getInitialState: function() {
+		return {
+			createIsVisible: false
+		};
+	},
+	
+	toggleCreate: function(visible) {
+		this.setState({
+			createIsVisible: visible
+		});
+	},
+	
+	renderCreateButton: function() {
+		if (Keystone.list.autocreate) {
+			return (
+				<div className="toolbar">
+					<a href={'?new' + Keystone.csrf.query} className="btn btn-default btn-create btn-create-item">
+						<span className="ion-plus-round" />
+						Create {Keystone.list.singular}
+					</a>
+				</div>
+			);
+		}
+		return (
+			<div className="toolbar-default">
+				<button type="button" className="btn btn-default btn-create btn-create-item" onClick={this.toggleCreate.bind(this, true)}>
+					<span className="ion-plus-round" />
+					Create {Keystone.list.singular}
+				</button>
+			</div>
+		);
+	},
+	
+	renderCreateForm: function() {
+		if (!this.state.createIsVisible) return null;
+		return <CreateForm list={Keystone.list} onCancel={this.toggleCreate.bind(this, false)} />
+	},
+	
+	render: function() {
+		if (Keystone.list.nocreate) return null;
+		return (
+			<div className="create-item">
+				{this.renderCreateButton()}
+				{this.renderCreateForm()}
+				<hr />
+			</div>
+		);
+	}
+	
+});
+
+exports.render = function() {
+	React.render(<View />, document.getElementById('list-view'));
+};
