58b0a7e32e3a3b2aff818f8a180f5c4ad2f3a5ab
Christian Andrade <christian@icemobile.com>
2015-02-04 16:48:52 +0100

    added upload image button for Cloudinary on bootstrap-markdown plugin

--- fields/types/markdown/MarkdownField.js
+++ fields/types/markdown/MarkdownField.js
@@ -84,7 +84,75 @@ module.exports = Field.create({
 					headingCallback(e, '####');
 				}
 			}]
-		}];
+		},
+			{
+				name: 'groupLink',
+				data: [{
+					name: 'cmdCloudinary',
+					title: 'Upload to Cloudinary',
+					btnText: 'UPL',
+					callback: function(e) {
+						var chunk, cursor, selected = e.getSelection(), file, data, cloudinary;
+						var cloudinary = Keystone.cloudinary;
+						var input = $('<input/>').attr('type', 'file');
+
+						if (selected.length === 0) {
+							// Give extra word
+							chunk = e.__localize('enter image description here');
+						} else {
+							chunk = selected.text;
+						}
+
+						input.on('change', function(event){
+							file = event.target.files[0];
+
+							data = new FormData();
+							data.append('file', file);
+							data.append('api_key', cloudinary.api_key);
+							data.append('timestamp', cloudinary.timestamp);
+							data.append('signature', cloudinary.signature);
+
+							$.ajax({
+								url: 'https://api.cloudinary.com/v1_1/' + cloudinary.cloud_name +'/image/upload',
+								type: 'POST',
+								data: data,
+								cache: false,
+								dataType: 'json',
+								processData: false, // Don't process the files
+								contentType: false,
+								success: function(data, textStatus, jqXHR) {
+									if(typeof data.error === 'undefined') {
+										var sanitizedLink = $('<div>' + data.url + '</div>').text();
+
+										// transform selection and set the cursor into chunked text
+										e.replaceSelection('!['+chunk+']('+sanitizedLink+' "'+e.__localize('enter image title here')+'")');
+										cursor = selected.start+2;
+
+										// Set the next tab
+										e.setNextTab(e.__localize('enter image title here'));
+
+										// Set the cursor
+										e.setSelection(cursor,cursor+chunk.length);
+									}
+									else
+									{
+										// Handle errors here
+										console.log('ERRORS: ' + data.error);
+									}
+								},
+								error: function(jqXHR, textStatus, errorThrown) {
+									// Handle errors here
+									console.log('ERRORS: ' + textStatus);
+									// STOP LOADING SPINNER
+								}
+							});
+						});
+
+						input.click();
+					}
+				}]
+			}
+		];
 	},
 	
 	renderField: function() {
