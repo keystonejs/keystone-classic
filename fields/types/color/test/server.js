fba0d0f84e6b519c888d3d11149a8df3f8cd8b92
Sebastian McKenzie <sebmck@gmail.com>
2015-02-09 10:15:46 +1100

    Add color field server tests

---
+++ fields/types/color/test/server.js
@@ -0,0 +1,51 @@
+var assert = require('assert'),
+	demand = require('must'),
+	UpdateHandler = require('../../../../lib/updateHandler'),
+	ColorType = require('../ColorType');
+
+exports.initList = function(List) {
+	List.add({
+		color: { type: ColorType },
+		nested: {
+			color: { type: ColorType }
+		}
+	});
+}
+
+exports.createData = function(List) {
+
+};
+
+exports.testFilters = function(List) {
+
+};
+
+exports.testFieldType = function(List) {
+	var testItem = new List.model();
+
+	it('should update top level fields', function() {
+		List.fields.color.updateItem(testItem, {
+			color: '#ffffff'
+		});
+		demand(testItem.color).be('#ffffff');
+		testItem.color = undefined;
+	});
+	
+	it('should update nested fields', function() {
+		List.fields['nested.color'].updateItem(testItem, {
+			nested: {
+				color: '#ffffff'
+			}
+		});
+		demand(testItem.nested.color).be('#ffffff');
+		testItem.nested.color = undefined;
+	});
+	
+	it('should update nested fields with flat paths', function() {
+		List.fields['nested.color'].updateItem(testItem, {
+			'nested.color': '#ffffff'
+		});
+		demand(testItem.nested.color).be('#ffffff');
+		testItem.nested.color = undefined;
+	});
+};
