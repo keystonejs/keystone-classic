3fecceea7f156ff342641efa323830bece3bba0a
Jed Watson <jed.watson@me.com>
2015-02-11 21:50:06 +1100

    Adding more comprehensive textarray tests

--- fields/types/textarray/test/server.js
+++ fields/types/textarray/test/server.js
@@ -1,44 +1,83 @@
-var assert = require('assert'),
-	keystone = require('../../../..'),
-	demand = require('must'),
-	UpdateHandler = require('../../../../lib/updateHandler'),
+var demand = require('must'),
 	TextArrayType = require('../TextArrayType');
 
 exports.initList = function(List) {
 	List.add({
-		text: TextArrayType,
+		textarr: TextArrayType,
 		nested: {
-			text: TextArrayType
+			textarr: TextArrayType
 		}
 	});
 };
 
 exports.testFieldType = function(List) {
 	var testItem = new List.model();
+	
+	it('should default to an empty array', function() {
+		demand(testItem.get('textarr')).eql([]);
+	});
+	
+	it('should validate input', function() {
+		demand(List.fields.textarr.validateInput({
+			textarr: ['a']
+		})).be(true);
+		demand(List.fields.textarr.validateInput({
+			textarr: ['a', 'b']
+		})).be(true);
+	});
+	
+	it('should validate no input', function() {
+		demand(List.fields.textarr.validateInput({})).be(true);
+		demand(List.fields.textarr.validateInput({}, true)).be(false);
+		testItem.textarr = ['a'];
+		demand(List.fields.textarr.validateInput({}, true, testItem)).be(true);
+		testItem.textarr = undefined;
+	});
+	
+	it('should validate length when required', function() {
+		demand(List.fields.textarr.validateInput({
+			textarr: []
+		}, true)).be(false);
+	});
 
 	it('should update top level fields', function() {
-		List.fields.text.updateItem(testItem, {
-			text: ['foo', 'bar']
+		List.fields.textarr.updateItem(testItem, {
+			textarr: ['a', 'b']
 		});
-		demand(testItem.text).eql(['foo', 'bar']);
-		testItem.text = undefined;
+		demand(testItem.textarr).eql(['a', 'b']);
+		testItem.textarr = undefined;
 	});
 	
 	it('should update nested fields', function() {
-		List.fields['nested.text'].updateItem(testItem, {
+		List.fields['nested.textarr'].updateItem(testItem, {
 			nested: {
-				text: ['foo', 'bar']
+				textarr: ['a', 'b']
 			}
 		});
-		demand(testItem.nested.text).eql(['foo', 'bar']);
-		testItem.nested.text = undefined;
+		demand(testItem.nested.textarr).eql(['a', 'b']);
+		testItem.nested.textarr = undefined;
 	});
 	
 	it('should update nested fields with flat paths', function() {
-		List.fields['nested.text'].updateItem(testItem, {
-			'nested.text': ['foo', 'bar']
+		List.fields['nested.textarr'].updateItem(testItem, {
+			'nested.textarr': ['a', 'b']
 		});
-		demand(testItem.nested.text).eql(['foo', 'bar']);
-		testItem.nested.text = undefined;
+		demand(testItem.nested.textarr).eql(['a', 'b']);
+		testItem.nested.textarr = undefined;
+	});
+	
+	it('should update empty arrays', function() {
+		List.fields.textarr.updateItem(testItem, {
+			textarr: []
+		});
+		demand(testItem.textarr).eql([]);
+	});
+
+	it('should allow a single string value', function() {
+		List.fields.textarr.updateItem(testItem, {
+			textarr: 'a'
+		});
+		demand(testItem.textarr).eql(['a']);
+		testItem.textarr = undefined;
 	});
 };
