29c974656fc53bcab93e4a738b0f0653aa5b767d
Daniel Zurawski <zurawski.daniel@gmail.com>
2015-03-14 13:19:22 +0000

    Add options section to UrlType and change function name to more removeProtocolPrefix

--- fields/types/url/UrlType.js
+++ fields/types/url/UrlType.js
@@ -14,7 +14,8 @@ var util = require('util'),
 function url(list, path, options) {
 	this._nativeType = String;
 	this._underscoreMethods = ['format'];
-	this.formatUrl = options.format;
+	this._formatUrl = options.format || removeProtocolPrefix;
+
 	url.super_.call(this, list, path, options);
 }
 
@@ -26,19 +27,25 @@ util.inherits(url, super_);
 
 
 /**
- * Formats the field value
- *
- * Strips the leading protocol from the value for simpler display
+ * Formats the field value using either a supplied format function or default
+ * which strips the leading protocol from the value for simpler display
  *
  * @api public
  */
 
 url.prototype.format = function(item) {
 	var url = (item.get(this.path) || '');
-
-	return this.formatUrl ? this.formatUrl(url) : url.replace(/^[a-zA-Z]+\:\/\//, '');
+	return this._formatUrl(url);
 };
 
+/**
+ * Remove the protocol prefix from url
+ *
+ * @api private
+ */
+function removeProtocolPrefix(url) {
+	return url.replace(/^[a-zA-Z]+\:\/\//, '');
+}
 
 // TODO: Proper url validation
 
