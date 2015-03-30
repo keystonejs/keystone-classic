c486171ba213ef11dd3960ada2bb5c5ab53c0804
Camille Reynders <camille.reynders@gmail.com>
2015-03-30 18:41:20 +0200

    Fix dropped error in store adapter uploadFile

--- fields/types/file/FileType.js
+++ fields/types/file/FileType.js
@@ -266,6 +266,7 @@ file.prototype.uploadFile = function(item, file, update, callback) {
 	this.callHook('pre:upload', [item, file], function(err) {
 		if (err) return callback(err);
 		self.store.uploadFile(self, item, file, function(err, data) {
+			if (err) return callback(err);
 			self.callHook('post:upload', [item, file, data], function(err) {
 				if (!err && update) {
 					item.set(self.path, data);
