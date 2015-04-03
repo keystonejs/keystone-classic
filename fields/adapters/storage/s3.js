34cff1b24dce0d453630030535c227b56fcc7142
Camille Reynders <camille.reynders@gmail.com>
2015-04-03 12:11:49 +0200

    Fix busted keystone require

--- fields/adapters/storage/s3.js
+++ fields/adapters/storage/s3.js
@@ -1,4 +1,4 @@
-var keystone = require('../../../../');
+var keystone = require('../../../');
 
 module.exports = require('../BuildPkgCloudStorage')('amazon', function() {
 	return keystone.get('s3 config');
