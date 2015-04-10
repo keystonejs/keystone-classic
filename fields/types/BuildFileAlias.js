a671424bf0201769dace93bb1180fc2bf4d85439
Camille Reynders <camille.reynders@gmail.com>
2015-04-10 08:35:14 +0200

    Fix Buildfilealias storage creation

--- fields/types/BuildFileAlias.js
+++ fields/types/BuildFileAlias.js
@@ -1,20 +1,19 @@
 var keystone = require('../../'),
 	util = require('util'),
+	_ = require('underscore'),
 	super_ = require('./Type');
 
 module.exports = function(adapter, multiple) {
 	function file(list, path, options) {
-		var storeName = list.name + '-' + path.name;
-		new keystone.Store(storeName, {
-			adapter: adapter,
-			dest: options.dest
-		});
-
+		var storeName = list.key + '-' + path;
+		new keystone.Store(storeName, _.defaults({
+			adapter: adapter
+		}, options));
 		var Field = keystone.Field.Types.File;
 		if (multiple) Field = keystone.Field.Types.Files;
-		return new Field(list, path, {
+		return new Field(list, path, _.defaults({
 			store: storeName
-		});
+		}, options));
 	}
 
 	util.inherits(file, super_);
