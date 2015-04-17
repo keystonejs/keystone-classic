21f1db4b025fbeacb6afb326db4140d2047b9255
Camille Reynders <camille.reynders@gmail.com>
2015-04-17 10:15:40 +0200

    Fix s3 file deletions

--- fields/adapters/storage/s3.js
+++ fields/adapters/storage/s3.js
@@ -8,32 +8,34 @@ var StorageAdapter = require('../StorageAdapter'),
 
 function s3file() {
 	StorageAdapter.apply(this, arguments);
-	
-	if(! this.s3config){
+
+	if (!this.s3config) {
 		throw new Error('Invalid Configuration\n\n' +
-			'S3s stores require the "s3 config" option to be set.\n\n' +
-			'See http://keystonejs.com/docs/configuration/#services-amazons3 for more information.\n');
-		
+		'S3s stores require the "s3 config" option to be set.\n\n' +
+		'See http://keystonejs.com/docs/configuration/#services-amazons3 for more information.\n');
+
 	}
 }
 
 util.inherits(s3file, StorageAdapter);
 
 /**
-* Exposes the custom or keystone s3 config settings
-*/
+ * Exposes the custom or keystone s3 config settings
+ */
 
-Object.defineProperty(s3file.prototype, 's3config', { get: function() {
-	return this.options.s3config || keystone.get('s3 config');
-}});
+Object.defineProperty(s3file.prototype, 's3config', {
+	get: function() {
+		return this.options.s3config || keystone.get('s3 config');
+	}
+});
 
 s3file.prototype.getPaths = function(basePaths) {
 	return _.defaults({
-		url : String
+		url: String
 	}, basePaths);
 };
 
-s3file.prototype.fileExists = function(item, paths){
+s3file.prototype.fileExists = function(item, paths) {
 	return (item.get(paths.url) ? true : false);
 };
 
@@ -51,24 +53,25 @@ var validateHeader = function(header, callback) {
 		validKeys = [HEADER_NAME_KEY, HEADER_VALUE_KEY],
 		filteredKeys;
 
-	if (!_.has(header, HEADER_NAME_KEY)){
+	if (!_.has(header, HEADER_NAME_KEY)) {
 		return callback(new Error('Unsupported Header option: missing required key "' + HEADER_NAME_KEY + '" in ' + JSON.stringify(header)));
 	}
-	if (!_.has(header, HEADER_VALUE_KEY)){
+	if (!_.has(header, HEADER_VALUE_KEY)) {
 		return callback(new Error('Unsupported Header option: missing required key "' + HEADER_VALUE_KEY + '" in ' + JSON.stringify(header)));
 	}
 
-	filteredKeys = _.filter(_.keys(header), function (key){ return _.indexOf(validKeys, key) > -1 });
+	filteredKeys = _.filter(_.keys(header), function(key) {
+		return _.indexOf(validKeys, key) > -1
+	});
 
-	_.each(filteredKeys, function (key){
-		if (!_.isString(header[key])){
+	_.each(filteredKeys, function(key) {
+		if (!_.isString(header[key])) {
 			return callback(new Error('Unsupported Header option: value for ' + key + ' header must be a String ' + header[key].toString()));
 		}
 	});
-	
+
 	return true;
 };
-
 
 /**
  * Convenience method to validate a headers object
@@ -81,21 +84,20 @@ var validateHeader = function(header, callback) {
 var validateHeaders = function(headers, callback) {
 	var _headers = [];
 
-	if (!_.isObject(headers)){
+	if (!_.isObject(headers)) {
 		return callback(new Error('Unsupported Header option: headers must be an Object ' + JSON.stringify(headers)));
 	}
 
-	_.each(headers, function (value, key){
-		_headers.push({ name: key, value: value });
+	_.each(headers, function(value, key) {
+		_headers.push({name: key, value: value});
 	});
 
-	_.each(_headers, function (header){
+	_.each(_headers, function(header) {
 		validateHeader(header, callback);
 	});
 
 	return true;
 };
-
 
 /**
  * Generates a headers object for this item to use during upload
@@ -106,78 +108,75 @@ var validateHeaders = function(headers, callback) {
  * @api public
  */
 
-s3file.prototype.generateHeaders = function (item, file, callback) {
+s3file.prototype.generateHeaders = function(item, file, callback) {
 	var field = this,
 		filetype = file.mimetype || file.type,
 		headers = {
 			'Content-Type': filetype,
-			'x-amz-acl': 'public-read'
+			'x-amz-acl'   : 'public-read'
 		},
 		customHeaders = {},
 		headersOption = {},
 		computedHeaders,
 		defaultHeaders;
 
-
-	if (_.has(field.s3config, 'default headers')){
+	if (_.has(field.s3config, 'default headers')) {
 		defaultHeaders = field.s3config['default headers'];
-		if (_.isArray(defaultHeaders)){
-			_.each(defaultHeaders, function (header){
+		if (_.isArray(defaultHeaders)) {
+			_.each(defaultHeaders, function(header) {
 				var _header = {};
-				if (validateHeader(header, callback)){
+				if (validateHeader(header, callback)) {
 					_header[header.name] = header.value;
-					customHeaders = _.extend(customHeaders, _header); 
+					customHeaders = _.extend(customHeaders, _header);
 				}
 			});
-		} else if (_.isObject(defaultHeaders)){
-			customHeaders = _.extend(customHeaders, defaultHeaders);  
+		} else if (_.isObject(defaultHeaders)) {
+			customHeaders = _.extend(customHeaders, defaultHeaders);
 		} else {
 			return callback(new Error('Unsupported Header option: defaults headers must be either an Object or Array ' + JSON.stringify(defaultHeaders)));
 		}
 	}
 
-	if (field.options.headers){
+	if (field.options.headers) {
 		headersOption = field.options.headers;
-		
-		if (_.isFunction(headersOption)){
+
+		if (_.isFunction(headersOption)) {
 			computedHeaders = headersOption.call(field, item, file);
 
-			if (_.isArray(computedHeaders)){
-				_.each(computedHeaders, function (header){
+			if (_.isArray(computedHeaders)) {
+				_.each(computedHeaders, function(header) {
 					var _header = {};
-					if (validateHeader(header, callback)){
+					if (validateHeader(header, callback)) {
 						_header[header.name] = header.value;
-						customHeaders = _.extend(customHeaders, _header); 
+						customHeaders = _.extend(customHeaders, _header);
 					}
 				});
-			} else if (_.isObject(computedHeaders)){
+			} else if (_.isObject(computedHeaders)) {
 				customHeaders = _.extend(customHeaders, computedHeaders);
 			} else {
 				return callback(new Error('Unsupported Header option: computed headers must be either an Object or Array ' + JSON.stringify(computedHeaders)));
 			}
 
-		} else if (_.isArray(headersOption)){
-			_.each(headersOption, function (header){
+		} else if (_.isArray(headersOption)) {
+			_.each(headersOption, function(header) {
 				var _header = {};
-				if (validateHeader(header, callback)){
+				if (validateHeader(header, callback)) {
 					_header[header.name] = header.value;
-					customHeaders = _.extend(customHeaders, _header); 
+					customHeaders = _.extend(customHeaders, _header);
 				}
 			});
-		} else if (_.isObject(headersOption)){
+		} else if (_.isObject(headersOption)) {
 			customHeaders = _.extend(customHeaders, headersOption);
-		} 
+		}
 	}
 
-	if (validateHeaders(customHeaders, callback)){
+	if (validateHeaders(customHeaders, callback)) {
 		headers = _.extend(headers, customHeaders);
 	}
 
 	return headers;
 
 };
-
-
 
 /**
  * Uploads the file for this field
@@ -210,15 +209,20 @@ s3file.prototype.uploadFile = function(field, item, file, callback) {
 			url = res.req.url.replace(/^https?:/i, protocol);
 
 		var fileData = {
-			filename: filename,
+			filename    : filename,
 			originalname: file.originalname,
-			path: path,
-			size: file.size,
-			filetype: filetype,
-			url: url
+			path        : path,
+			size        : file.size,
+			filetype    : filetype,
+			url         : url
 		};
 		callback(null, fileData);
 
 	});
+};
+
+s3file.prototype.deleteFile = function(field, file, callback) {
+	var dest = field.options.s3path ? field.options.s3path + '/' : '';
+	knox.createClient(this.s3config).deleteFile(path.join(dest, file.filename),callback);
 };
 module.exports = s3file;
