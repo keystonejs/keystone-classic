25606b9d1e42bcaf9dba1c0514dbdab4759e4137
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-04-30 15:58:01 +1000

    added react component for boolean field

--- admin/src/columns.js
+++ admin/src/columns.js
@@ -2,6 +2,7 @@ var React = require('react');
 
 var Columns = {
 	text: require('../../fields/types/text/TextColumn')
+	boolean: require('../../fields/types/boolean/BooleanColumn')
 };
 
 Columns.__unrecognised__ = Columns.text;
