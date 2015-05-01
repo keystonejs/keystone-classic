703e18186d24481a259de74a5eb9f1d915f07c56
Jed Watson <jed.watson@me.com>
2015-05-01 23:50:48 +1000

    Removing linter ignores and fixing the underlying causes

--- fields/types/datearray/DateArrayField.js
+++ fields/types/datearray/DateArrayField.js
@@ -1,8 +1,5 @@
-/* eslint-disable no-unused-vars*/ //temporary fix for https://github.com/yannickcr/eslint-plugin-react/issues/50#issuecomment-96708326
-var _ = require('underscore'),
-	Field = require('../Field'),
+var Field = require('../Field'),
 	ArrayFieldMixin = require('../../mixins/DateArrayField');
-/* eslint-enable */
 
 module.exports = Field.create({
 	
