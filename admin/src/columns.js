095909ff358c3ca2bc4a0d9672e1a4aeb7fb65ac
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-03 13:32:26 +1000

    Revised

--- admin/src/columns.js
+++ admin/src/columns.js
@@ -3,8 +3,7 @@ var React = require('react');
 var Columns = {
 	text: require('../../fields/types/text/TextColumn'),
 	boolean: require('../../fields/types/boolean/BooleanColumn'),
-	number: require('../../fields/types/number/NumberColumn'),
-	password: require('../../fields/types/password/PasswordColumn'),
+	password: require('../../fields/types/password/PasswordColumn')
 };
 
 Columns.__unrecognised__ = Columns.text;
