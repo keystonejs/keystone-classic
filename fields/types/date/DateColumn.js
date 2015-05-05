4a44ec303f7466f812a90eafeaedcfbe98055ec5
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-05 14:40:04 +1000

    Converted date field to react

---
+++ fields/types/date/DateColumn.js
@@ -0,0 +1,27 @@
+var React = require('react');
+var moment = require('moment');
+
+var DateColumn = React.createClass({
+
+	render: function() {
+		var value = this.props.data.fields[this.props.col.path];
+		var formattedValue = moment(value).format('MMMM Do YYYY');
+		console.log(formattedValue)
+		if (value) {
+			return (
+				<td>
+					<div className="col-value">{formattedValue}</div>
+				</td>
+			)
+		} else {
+			return (
+				<td>
+					<div className="col-value"></div>
+				</td>
+			)
+		}
+	}
+	
+});
+
+module.exports = DateColumn;
