4f1a69a06fe37f9d8540fcce320114cfd1a62d69
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-06 12:59:43 +1000

    Converted Geopoint field to react

--- admin/src/columns.js
+++ admin/src/columns.js
@@ -22,7 +22,8 @@ var Columns = {
 	key: require('../../fields/types/key/KeyColumn'),
 	s3file: require('../../fields/types/s3file/S3FileColumn'),
 	localfile: require('../../fields/types/localfile/LocalFileColumn'),
-	localfiles: require('../../fields/types/localfiles/LocalFilesColumn')
+	localfiles: require('../../fields/types/localfiles/LocalFilesColumn'),
+	geopoint: require('../../fields/types/geopoint/GeoPointColumn')
 };
 
 Columns.__unrecognised__ = Columns.text;
