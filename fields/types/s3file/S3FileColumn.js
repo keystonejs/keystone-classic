26bb8257fefcd12783d66e9e820b5ef182f07a59
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-09 08:42:03 +1000

    refactored S3file:

--- fields/types/s3file/S3FileColumn.js
+++ fields/types/s3file/S3FileColumn.js
@@ -4,9 +4,10 @@ var S3FileColumn = React.createClass({
 
 	render: function() {
 		var value = this.props.data.fields[this.props.col.path];
+		var isVal = value.url ? value.url : null
 		return (
 			<td>
-				<div className="col-value"><a href={value.url} target='_blank'>{ value ? value.url : null }</a></div>
+				<div className="col-value"><a href={isVal} target='_blank'>{ isVal }</a></div>
 			</td>
 		);	
 	}
