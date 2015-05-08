a1041c8434da2ce1962c624c59d170057db1602b
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-09 08:36:56 +1000

    refactored textarea

--- fields/types/textarea/TextareaColumn.js
+++ fields/types/textarea/TextareaColumn.js
@@ -4,21 +4,11 @@ var TextareaColumn = React.createClass({
 
 	render: function() {
 		var value = this.props.data.fields[this.props.col.path];
-		if (value) {
-			var limitedValue = value.substring(0, 500);
-			return (
-				<td>
-					<div className='col-value'>{limitedValue}</div>
-				</td>
-			);
-		} else {
-			return (
-				<td>
-					<div className='col-value'></div>
-				</td>
-			);
-		}
-		
+		return (
+			<td>
+				<div className='col-value'>{value ? value.substring(0, 500) : null}</div>
+			</td>
+		);
 	}
 });
 
