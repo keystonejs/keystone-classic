e06430a9b972b51eacb4af2ace286064d04c0b20
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-09 09:47:03 +1000

    refactored key column

--- fields/types/key/KeyColumn.js
+++ fields/types/key/KeyColumn.js
@@ -4,20 +4,11 @@ var KeyColumn = React.createClass({
 
 	render: function() {
 		var value = this.props.data.fields[this.props.col.path];
-		if (value) {
-			return (
-				<td>
-					<div className='col-value'>{value}</div>
-				</td>
-			);
-		} else {
-			return (
-				<td>
-					<div className='col-value'></div>
-				</td>
-			);
-		}
-		
+		return (
+			<td>
+				<div className='col-value'>{value ? value : null}</div>
+			</td>
+		);		
 	}
 });
 
