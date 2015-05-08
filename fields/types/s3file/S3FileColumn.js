8b5135a2d950f9478aec75566cdbc4b8788c7129
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-08 11:49:33 +1000

    refactored s3file field

--- fields/types/s3file/S3FileColumn.js
+++ fields/types/s3file/S3FileColumn.js
@@ -4,19 +4,11 @@ var S3FileColumn = React.createClass({
 
 	render: function() {
 		var value = this.props.data.fields[this.props.col.path];
-		if (value) {
-			return (
-				<td>
-					<div className="col-value"><a href={value.url} target='_blank'>{value.url}</a></div>
-				</td>
-			);
-		} else {
-			return (
-				<td>
-					<div className="col-value"></div>
-				</td>
-			);
-		}	
+		return (
+			<td>
+				<div className="col-value"><a href={value.url} target='_blank'>{ value ? value.url : null }</a></div>
+			</td>
+		);	
 	}
 	
 });
