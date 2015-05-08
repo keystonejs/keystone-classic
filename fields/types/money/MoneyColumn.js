e9337c1900cbb067f6206afa921849288eefee8a
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-09 09:18:28 +1000

    reafacted money column

--- fields/types/money/MoneyColumn.js
+++ fields/types/money/MoneyColumn.js
@@ -6,19 +6,11 @@ var MoneyColumn = React.createClass({
 	render: function() {
 		var value = this.props.data.fields[this.props.col.path];
 		var formattedValue = numeral(value).format('$0,0.00');
-		if (formattedValue && formattedValue > '$0.00') {
-			return (
-				<td>
-					<div className="col-value">{formattedValue}</div>
-				</td>
-			);
-		} else {
-			return (
-				<td>
-					<div className="col-value"></div>
-				</td>
-			);
-		}
+		return (
+			<td>
+				<div className="col-value">{formattedValue ? formattedValue : null}</div>
+			</td>
+		);
 	}
 	
 });
