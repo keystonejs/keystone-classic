a3c8befbe09b5e18f60a624badd9702497014a7c
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-09 09:03:18 +1000

    refactored name column

--- fields/types/name/NameColumn.js
+++ fields/types/name/NameColumn.js
@@ -1,22 +1,16 @@
 var React = require('react');
+var _ = require('underscore');
 
 var NameColumn = React.createClass({
 
 	render: function() {
 		var value = this.props.data.fields[this.props.col.path];
-		if (value) {
-			return (
-				<td>
-					<div className="col-value"><a href={'/keystone/users/'+ this.props.data.id}>{value.first + ' ' + value.last}</a></div>
-				</td>
-			);
-		} else {
-			return (
-				<td>
-					<div className="col-value"></div>
-				</td>
-			);
-		}
+		if (!value || !_.size(value)) return;
+		return (
+			<td>
+				<div className="col-value"><a href={'/keystone/users/'+ this.props.data.id}>{value.first + ' ' + value.last}</a></div>
+			</td>
+		);
 	}
 });
 
