68cf0edfbcd032ce34b1364d98657ca68c617d37
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-09 10:25:20 +1000

    refactored azurefile

--- fields/types/azurefile/AzureFileColumn.js
+++ fields/types/azurefile/AzureFileColumn.js
@@ -2,21 +2,19 @@ var React = require('react');
 
 var AzureFileColumn = React.createClass({
 
-	render: function() {
+	renderValue: function() {
 		var value = this.props.data.fields[this.props.col.path];
-		if (value) {
-			return (
-				<td>
-					<div className="col-value"><a href={value.url} target='_blank'>{value.url}</a></div>
-				</td>
-			);
-		} else {
-			return (
-				<td>
-					<div className="col-value"></div>
-				</td>
-			);
-		}	
+		if (!value) return
+
+		return <a href={value.url} target='_blank'>{value.url}</a>
+	},
+
+	render: function() {
+		return (
+			<td>
+				<div className="col-value">{this.renderValue()}</div>
+			</td>
+		);
 	}
 	
 });
