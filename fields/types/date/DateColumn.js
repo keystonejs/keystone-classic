d7f0e552f80442d7d0d894dc22e03924f1655e81
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-09 10:11:31 +1000

    refactored date column

--- fields/types/date/DateColumn.js
+++ fields/types/date/DateColumn.js
@@ -6,19 +6,11 @@ var DateColumn = React.createClass({
 	render: function() {
 		var value = this.props.data.fields[this.props.col.path];
 		var formattedValue = moment(value).format('MMMM Do YYYY');
-		if (value) {
-			return (
-				<td>
-					<div className="col-value">{formattedValue}</div>
-				</td>
-			);
-		} else {
-			return (
-				<td>
-					<div className="col-value"></div>
-				</td>
-			);
-		}
+		return (
+			<td>
+				<div className="col-value">{formattedValue ? formattedValue : null}</div>
+			</td>
+		);
 	}
 	
 });
