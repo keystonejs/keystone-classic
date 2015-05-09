a4f2684381c927ed756eb58cd345de60703c9a0e
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-05-09 10:00:57 +1000

    refactored embedly column

--- fields/types/embedly/EmbedlyColumn.js
+++ fields/types/embedly/EmbedlyColumn.js
@@ -2,22 +2,19 @@ var React = require('react');
 
 var EmbedlyColumn = React.createClass({
 
-	render: function() {
+	renderValue: function() {
 		var value = this.props.data.fields[this.props.col.path];
-		if (value && Object.keys(value).length !== 0) {
-			return (
-				<td>
-					<div className='col-value'><a href={value.url} target='_blank'>{value.url}</a></div>
-				</td>
-			);
-		} else {
-			return (
-				<td>
-					<div className='col-value'></div>
-				</td>
-			);
-		}
-		
+		if (!value || !_.size(value)) return;
+
+		return <a href={value.url} target='_blank'>{value.url}</a>
+	},
+
+	render: function() {
+		return (
+			<td>
+				<div className='col-value'>{this.renderValue()}</div>
+			</td>
+		);
 	}
 });
 
