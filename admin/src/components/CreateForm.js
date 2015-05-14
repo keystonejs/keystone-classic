b9aef451ddace07a7d040114f0583f6534d9cabf
Joss Mackison <joss.mackison@gmail.com>
2015-05-14 14:09:26 +1000

    make the create modal a horizontal form

--- admin/src/components/CreateForm.js
+++ admin/src/components/CreateForm.js
@@ -123,7 +123,7 @@ var Form = React.createClass({
 		
 		return (
 			<Modal isOpen onChange={this.props.onCancel} headerTitle={'Create a new ' + list.singular} headerHasCloseButton backdropClosesModal>
-				<form encType="multipart/form-data" method="post" action={formAction}>
+				<form encType="multipart/form-data" method="post" action={formAction} className="horizontal-form create-form">
 					<input type="hidden" name="action" value="create" />
 					<input type="hidden" name={Keystone.csrf.key} value={Keystone.csrf.value} />
 					<Modal.Body>
