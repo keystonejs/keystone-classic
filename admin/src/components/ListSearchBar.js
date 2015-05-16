8080ba0f246ebfbbce5652aac4e1a9efe131d7c8
Joss Mackison <joss.mackison@gmail.com>
2015-05-16 10:39:05 +1000

    move filter styles to elemental as Tag component

--- admin/src/components/ListSearchBar.js
+++ admin/src/components/ListSearchBar.js
@@ -4,6 +4,7 @@ var Button = require('elemental').Button;
 var Dropdown = require('elemental').Dropdown;
 var FormInput = require('elemental').FormInput;
 var InputGroup = require('elemental').InputGroup;
+var Tag = require('elemental').Tag;
 
 const CURRENT_FILTERS = [
 	'Created Date between 21/08/2014 and 21/09/2014',
@@ -32,11 +33,9 @@ var ListSearchBar = React.createClass({
 	
 	renderPagination: function() {
 		return (
-			<div className="ListFilter-pagination-bar clearfix">
-				<div className="list-pagination pull-left">
-					<div className="count">Showing 1 to 50 of 933</div>
-					<ul className="pagination"><li className="active"><a href="/keystone/listings/1">1</a></li><li><a href="/keystone/listings/2">2</a></li><li><a href="/keystone/listings/3">3</a></li><li><a href="/keystone/listings/4">4</a></li><li><a href="/keystone/listings/5">5</a></li><li><a href="/keystone/listings/6">6</a></li><li><a href="/keystone/listings/7">7</a></li><li><a href="/keystone/listings/8">8</a></li><li><a href="/keystone/listings/9">9</a></li><li><a href="/keystone/listings/10">10</a></li><li><a href="/keystone/listings/19">...</a></li></ul>
-				</div>
+			<div className="ListHeader__pagination">
+				<div className="count">Showing 1 to 50 of 933</div>
+				<ul className="pagination"><li className="active"><a href="/keystone/listings/1">1</a></li><li><a href="/keystone/listings/2">2</a></li><li><a href="/keystone/listings/3">3</a></li><li><a href="/keystone/listings/4">4</a></li><li><a href="/keystone/listings/5">5</a></li><li><a href="/keystone/listings/6">6</a></li><li><a href="/keystone/listings/7">7</a></li><li><a href="/keystone/listings/8">8</a></li><li><a href="/keystone/listings/9">9</a></li><li><a href="/keystone/listings/10">10</a></li><li><a href="/keystone/listings/19">...</a></li></ul>
 			</div>
 		);
 	},
@@ -44,14 +43,11 @@ var ListSearchBar = React.createClass({
 	renderFilters: function() {
 		var currentFilters = CURRENT_FILTERS.map(function(filter, i) {
 			return (
-				<div className="list-filter">
-					<span className="list-filter__label">{filter}</span>
-					<span className="list-filter__close">&times;</span>
-				</div>
+				<Tag label={filter} hasClearButton />
 			);
 		});
 		return (
-			<div className="ListFilter-filters mb-1">
+			<div className="ListHeader__filters mb-2">
 				{currentFilters}
 			</div>
 		);
@@ -59,10 +55,10 @@ var ListSearchBar = React.createClass({
 	
 	render: function() {
 		return (
-			<div className="ListFilter">
+			<div className="ListHeader">
 				<div className="container">
-					<h2 className="ListFilter__header">7287 Events sorted by name</h2>
-					<InputGroup contiguous={false}>
+					<h2 className="ListHeader__title">7287 Events sorted by name</h2>
+					<InputGroup contiguous={false} className="ListHeader__searchbar">
 						<InputGroup.Section>
 							<Dropdown items={[{ label: 'Listing state matches "published"' },{ label: 'Email matches "gmail"' }]}>
 								<Button title="Recent Filters">
