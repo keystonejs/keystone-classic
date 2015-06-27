878ef7aed59f495445a5988f0b781b487aaa58c4
webteckie <avp4carlos@gmail.com>
2015-06-27 00:08:44 -0400

    Add support for importing an array of model directories.  When a model array is specified, each directory in the array is assumed to be an absolute path.  When the specified model directory is a string, the import will be behave as before and resolve its full path using the module root.

--- index.js
+++ index.js
@@ -154,29 +154,34 @@ keystone.security = {
 keystone.utils = utils;
 
 /**
- * returns all .js modules (recursively) in the path specified, relative
+ * returns all .js modules (recursively) in the path(s) specified.
+ *
+ * If the input dirname is a string then it will be assumed to be a relative path
  * to the module root (where the keystone project is being consumed from).
+ *
+ * If the input dirname is an array then it will be assumed that all paths in the
+ * array are fully qualified and will be imported as is.
  *
  * ####Example:
  *
- *     var models = keystone.import('models');
+ *     var models = keystone.import('relative-path-models-dir');
+ *     or
+ *     var models = keystone.import(['absolute-path-model-dir1', 'absolute-path-model-dir2',...]);
  *
- * @param {String} dirname
+ * @param {String | Array} dirname
  * @api public
  */
 
 Keystone.prototype.import = function(dirname) {
 
-	var initialPath = path.join(this.get('module root'), dirname);
+	var imported = {};
 
 	var doImport = function(fromPath) {
-
-		var imported = {};
 
 		fs.readdirSync(fromPath).forEach(function(name) {
 
 			var fsPath = path.join(fromPath, name),
-			info = fs.statSync(fsPath);
+				info = fs.statSync(fsPath);
 
 			// recur
 			if (info.isDirectory()) {
@@ -189,14 +194,28 @@ Keystone.prototype.import = function(dirname) {
 					imported[base] = require(fsPath);
 				}
 			}
-
 		});
-
-		return imported;
 	};
 
-	return doImport(initialPath);
+	if ('string' !== typeof dirname) {
+
+		if (Array.isArray(dirname)) {
+
+			_.each(dirname, function(dir) {
+				doImport(dir);
+			});
+
+		} else {
+			console.error("Invalid models path specified.");
+		}
+	} else {
+		var initialPath = path.join(this.get('module root'), dirname);
+		doImport(initialPath);
+	}
+
+	return imported;
 };
+
 
 
 /**
