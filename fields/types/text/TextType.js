86b908473e70a928450d6e80f9ea97d9b40dbeb2
Jed Watson <jed.watson@me.com>
2015-07-19 00:24:57 +1000

    Cleanup

--- fields/types/text/TextType.js
+++ fields/types/text/TextType.js
@@ -1,17 +1,15 @@
 /*!
  * Module dependencies.
  */
-
-var util = require('util'),
-	utils = require('keystone-utils'),
-	super_ = require('../Type');
+var util = require('util');
+var utils = require('keystone-utils');
+var super_ = require('../Type');
 
 /**
  * Text FieldType Constructor
  * @extends Field
  * @api public
  */
-
 function text(list, path, options) {
 	this._nativeType = String;
 	this._underscoreMethods = ['crop'];
@@ -21,23 +19,18 @@ function text(list, path, options) {
 /*!
  * Inherit from Field
  */
-
 util.inherits(text, super_);
-
 
 /**
  * Crops the string to the specifed length.
  *
  * @api public
  */
-
 text.prototype.crop = function(item, length, append, preserveWords) {
 	return utils.cropString(item.get(this.path), length, append, preserveWords);
 };
 
-
 /*!
- * Export class
+ * Export Field
  */
-
 exports = module.exports = text;
