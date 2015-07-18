2398968cc3e7c365e519f0619d670e8d31b31243
Jed Watson <jed.watson@me.com>
2015-07-19 00:51:06 +1000

    New addFilterToQuery method for text fields

--- fields/types/text/TextType.js
+++ fields/types/text/TextType.js
@@ -1,9 +1,6 @@
-/*!
- * Module dependencies.
- */
 var util = require('util');
 var utils = require('keystone-utils');
-var super_ = require('../Type');
+var FieldType = require('../Type');
 
 /**
  * Text FieldType Constructor
@@ -15,22 +12,35 @@ function text(list, path, options) {
 	this._underscoreMethods = ['crop'];
 	text.super_.call(this, list, path, options);
 }
-
-/*!
- * Inherit from Field
- */
-util.inherits(text, super_);
+util.inherits(text, FieldType);
 
 /**
  * Crops the string to the specifed length.
- *
- * @api public
  */
 text.prototype.crop = function(item, length, append, preserveWords) {
 	return utils.cropString(item.get(this.path), length, append, preserveWords);
 };
 
-/*!
- * Export Field
+/**
+ * Add filters to a query
  */
+text.prototype.addFilterToQuery = function(filter, query) {
+	query = query || {};
+	if (filter.mode === 'match' && !filter.value) {
+		query[this.path] = filter.invert ? { $nin: ['', null] } : { $in: ['', null] };
+		return;
+	}
+	var value = utils.escapeRegExp(filter.value);
+	if (filter.mode === 'startsWith') {
+		value = '^' + value;
+	} else if (filter.mode === 'endsWith') {
+		value = value + '$';
+	} else if (filter.mode === 'match') {
+		value = '^' + value + '$';
+	}
+	value = new RegExp(value, filter.caseSensitive ? '' : 'i');
+	query[this.path] = filter.invert ? { $not: value } : value;
+};
+
+/* Export Field */
 exports = module.exports = text;
