52803c7180c9aa91ed50c9d999a479de344226bc
Jed Watson <jed.watson@me.com>
2015-08-01 23:55:08 +1000

    Implementing simple GET keystone/api/:list route for #1544

---
+++ admin/api/list/get.js
@@ -0,0 +1,24 @@
+var async = require('async');
+
+module.exports = function(req, res) {
+	var query = req.list.model.find();
+	async.series({
+		count: function(next) {
+			query.count(next);
+		},
+		items: function(next) {
+			query.find();
+			query.limit(Number(req.query.limit) || 100);
+			query.skip(Number(req.query.skip) || 0);
+			query.exec(next);
+		}
+	}, function(err, results) {
+		if (err) return res.apiError('database error', err);
+		return res.json({
+			results: results.items.map(function (item) {
+				return req.list.getData(item);
+			}),
+			count: results.count
+		});
+	});
+};
