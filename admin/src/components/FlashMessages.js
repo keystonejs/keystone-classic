ff229bc37211ec53916f01fa69154c48087a5ab0
Joss Mackison <joss.mackison@gmail.com>
2015-08-11 15:34:04 +1000

    [WIP] FlashMessages component

---
+++ admin/src/components/FlashMessages.js
@@ -0,0 +1,75 @@
+const React = require('react');
+const blacklist = require('blacklist');
+const { Alert } = require('elemental');
+
+var FlashMessage = React.createClass({
+	displayName: 'FlashMessage',
+	
+	propTypes: {
+		message: React.PropTypes.oneOfType([
+			React.PropTypes.object,
+			React.PropTypes.string,
+		]),
+		type: React.PropTypes.string,
+	},
+	
+	renderMessage () {
+		let { message } = this.props;
+		
+		if (typeof message === 'string') {
+			return <span>{message}</span>;
+		}
+		
+		let title = message.title ? <h4>{message.title}</h4> : null;
+		let detail = message.detail ? <p>{message.detail}</p> : null;
+		let list = message.list ? (
+			<ul>
+				{message.list.map(item => <li>{item}</li>)}
+			</ul>
+		) : null;
+			
+		return (
+			<span>
+				{title}
+				{detail}
+				{list}
+			</span>
+		);
+	},
+	
+	render: function() {
+		// sanitize types
+		let type = (this.props.type === 'error') ? 'danger' : this.props.type;
+		
+		return <Alert type={type}>{this.renderMessage()}</Alert>;
+	}
+	
+});
+
+var FlashMessages = React.createClass({
+	displayName: 'FlashMessages',
+	propTypes: {
+		messages: React.PropTypes.shape({
+			error: React.PropTypes.array,
+			hilight: React.PropTypes.array,
+			info: React.PropTypes.array,
+			success: React.PropTypes.array,
+			warning: React.PropTypes.array,
+		});
+	},
+	
+	renderErrorAlerts () {
+		return ();
+	},
+	
+	render: function() {
+		return (
+			<div className="flash-messages">
+				{this.renderErrorAlerts()}
+			</div>
+		);
+	}
+	
+});
+
+module.exports = FlashMessages;
