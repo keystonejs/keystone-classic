8d7f7bc112dbe8a6b6f96683408d2b090fbe0844
Joss Mackison <joss.mackison@gmail.com>
2015-08-14 13:43:24 +1000

    update ListDownloadForm to use PopoutListItem components

--- admin/src/components/ListDownloadForm.js
+++ admin/src/components/ListDownloadForm.js
@@ -4,6 +4,7 @@ import React from 'react';
 var Transition = React.addons.CSSTransitionGroup;
 var CurrentListStore = require('../stores/CurrentListStore');
 var Popout = require('./Popout');
+var PopoutList = require('./PopoutList');
 
 var { Button, Checkbox, Form, FormField, InputGroup, SegmentedControl } = require('elemental');
 
@@ -73,6 +74,19 @@ var ListDownloadForm = React.createClass({
 		});
 	},
 	
+	toggleCurrentlySelectedColumns (e) {
+		let newState = {
+			useCurrentColumns: !this.state.useCurrentColumns
+		}
+		
+		// clear selected fields
+		if (e.target.value) {
+			newState.selectedColumns =  {}
+		}
+		
+		this.setState(newState)
+	},
+	
 	handleDownloadRequest () {
 		console.info(`Download ${this.state.format.toUpperCase()} with columns:`, Object.keys(this.state.selectedColumns));
 		this.togglePopout(false);
@@ -83,25 +97,18 @@ var ListDownloadForm = React.createClass({
 		
 		let possibleColumns = this.getListUIElements().map((el, i) => {
 			if (el.type === 'heading') {
-				return <div key={'item-' + i} className="Popout__list__header">{el.content}</div>
+				return <PopoutList.Heading key={'heading_' + i}>{el.content}</PopoutList.Heading>;
 			}
 			
 			let columnKey = el.field.path;
 			let columnValue = this.state.selectedColumns[columnKey];
 			
-			var itemClassname = classnames('Popout__list__item', {
-				'is-selected': columnValue
-			});
-			var iconClassname = classnames('Popout__list__item__icon octicon',
-				columnValue ? 'octicon-check' : 'octicon-dash'
-			);
-			
-			return (
-				<button type="button" key={'item-' + el.field.path} onClick={this.toggleColumn.bind(this, columnKey, !columnValue)} title={el.field.label} className={itemClassname}>
-					<span className={iconClassname} />
-					{el.field.label}
-				</button>
-			);
+			return <PopoutList.Item
+				key={'item_' + el.field.path}
+				icon={columnValue ? 'check' : 'dash'}
+				isSelected={columnValue}
+				label={el.field.label}
+				onClick={this.toggleColumn.bind(this, columnKey, !columnValue)} />;
 		});
 		
 		return (
@@ -129,7 +136,7 @@ var ListDownloadForm = React.createClass({
 								<SegmentedControl equalWidthSegments options={FORMAT_OPTIONS} value={this.state.format} onChange={this.changeFormat} />
 							</FormField>
 							<FormField label="Columns:">
-								<Checkbox label="Use currently selected" onChange={() => this.setState({useCurrentColumns:!useCurrentColumns})} checked={useCurrentColumns} />
+								<Checkbox label="Use currently selected" onChange={this.toggleCurrentlySelectedColumns} value={true} checked={useCurrentColumns} />
 							</FormField>
 							{this.renderColumnSelect()}
 						</Form>
