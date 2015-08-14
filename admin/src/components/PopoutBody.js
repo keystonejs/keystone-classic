81b27132721d7fc63c421b1bc2cd68a5eaf39702
Joss Mackison <joss.mackison@gmail.com>
2015-08-14 11:43:06 +1000

    make PopoutBody more functional/extendable

--- admin/src/components/PopoutBody.js
+++ admin/src/components/PopoutBody.js
@@ -1,13 +1,21 @@
-var React = require('react');
+import blacklist from 'blacklist';
+import classnames from 'classnames';
+import React from 'react';
 
 var PopoutBody = React.createClass({
 	displayName: 'PopoutBody',
 	propTypes: {
-		children: React.PropTypes.node.isRequired
+		children: React.PropTypes.node.isRequired,
+		scrollable: React.PropTypes.bool,
 	},
 	
 	render () {
-		return <div className="Popout__body Popout__scrollable-area" {...this.props} />;
+		let className = classnames('Popout__body', {
+			'Popout__scrollable-area': this.props.scrollable
+		}, this.props.className);
+		let props = blacklist(this.props, 'className', 'scrollable')
+		
+		return <div className={className} {...props} />;
 	}
 	
 });
