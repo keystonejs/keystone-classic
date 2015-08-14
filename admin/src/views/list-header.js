26b7ba8a9f66d81e93f2cc8ac0f452a06a660dff
Joss Mackison <joss.mackison@gmail.com>
2015-08-14 14:45:44 +1000

    remove old junk from list-header

--- admin/src/views/list-header.js
+++ admin/src/views/list-header.js
@@ -1,44 +1,8 @@
 var React = require('react');
-var CreateForm = require('../components/CreateForm');
-var Toolbar = require('../components/Toolbar');
 var ListHeader = require('../components/ListHeader');
 
-var { Button } = require('elemental');
-
 var Header = React.createClass({
-	
-	displayName: 'ListHeader',
-	
-	getInitialState: function() {
-		return {
-			createIsOpen: Keystone.showCreateForm
-		};
-	},
-	
-	toggleCreateModal: function(visible) {
-		this.setState({
-			createIsOpen: visible
-		});
-	},
-	
-	renderCreateButton: function() {
-		var props = { type: 'success' };
-		if (Keystone.list.autocreate) {
-			props.href = '?new' + Keystone.csrf.query;
-		} else {
-			props.onClick = this.toggleCreateModal.bind(this, true);
-		}
-		return (
-			<Button {...props}>
-				<span className="octicon octicon-plus" />
-				Create {Keystone.list.singular}
-			</Button>
-		);
-	},
-	
-	renderCreateForm: function() {
-		return <CreateForm list={Keystone.list} isOpen={this.state.createIsOpen} onCancel={this.toggleCreateModal.bind(this, false)} values={Keystone.createFormData} err={Keystone.createFormErrors} />;
-	},
+	displayName: 'Header',
 	
 	render: function() {
 		if (Keystone.list.nocreate) return null;
