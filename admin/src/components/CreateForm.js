f50ccb70dbb0e6f4b17b875566a5fbd2c295a80e
Jed Watson <jed.watson@me.com>
2015-08-19 03:30:39 +1000

    Fixing issues with the Create dialog, including name field and focus management

--- admin/src/components/CreateForm.js
+++ admin/src/components/CreateForm.js
@@ -18,19 +18,15 @@ var CreateForm = React.createClass({
 	},
 
 	getInitialState () {
-
 		var values = this.props.values;
-
 		_.each(this.props.list.fields, function(field) {
 			if (!values[field.path]) {
 				values[field.path] = field.defaultValue;
 			}
 		});
-
 		return {
 			values: values
 		};
-
 	},
 
 	handleChange (event) {
@@ -42,16 +38,11 @@ var CreateForm = React.createClass({
 	},
 
 	componentDidUpdate (prevProps, prevState) {
-		let bodyStyle = document.body.style;
-
-		if (this.refs.focusTarget) {
-			this.refs.focusTarget.focus();
-		}
-
-		if (this.props.isOpen) {
-			bodyStyle.overflow = 'hidden';
-		} else {
-			bodyStyle.overflow = '';
+		if (this.props.isOpen !== prevProps.isOpen) {
+			document.body.style.overflow = (this.props.isOpen) ? 'hidden' : '';
+			if (this.refs.focusTarget) {
+				this.refs.focusTarget.focus();
+			}
 		}
 	},
 
@@ -71,36 +62,38 @@ var CreateForm = React.createClass({
 		return props;
 	},
 
-	render () {
+	renderAlerts () {
+		if (!this.props.err || !this.props.err.errors) return;
 
-		var alert = null;
+		var alertContent;
+		var errorCount = Object.keys(this.props.err.errors).length;
+
+		var messages = _.map(this.props.err.errors, (err, path) => {
+			return errorCount > 1 ? <li key={path}>{err.message}</li> : <div key={path}>{err.message}</div>;
+		});
+
+		if (errorCount > 1) {
+			alertContent = (
+				<div>
+					<h4>There were {errorCount} errors creating the new {list.singular}:</h4>
+					<ul>{messages}</ul>
+				</div>
+			);
+		} else {
+			alertContent = messages;
+		}
+
+		return <Alert type="danger">{alertContent}</Alert>;
+	},
+
+	renderForm () {
+
+		if (!this.props.isOpen) return;
 		var form = [];
 		var list = this.props.list;
 		var formAction = '/keystone/' + list.path;
 		var nameField = this.props.list.nameField;
 		var focusRef;
-
-		if (this.props.err && this.props.err.errors) {
-			var alertContent;
-			var errorCount = Object.keys(this.props.err.errors).length;
-			var msgs = {};
-
-			_.each(this.props.err.errors, function(err, path) {
-				msgs[path] = errorCount > 1 ? <li>{err.message}</li> : <div>{err.message}</div>;
-			});
-
-			if (errorCount > 1) {
-				alertContent = (
-					<div>
-						<h4>There were {errorCount} errors creating the new {list.singular}:</h4>
-						<ul>{msgs}</ul>
-					</div>
-				);
-			} else {
-				alertContent = msgs;
-			}
-			alert = <Alert type="danger">{alertContent}</Alert>;
-		}
 
 		if (list.nameIsInitial) {
 			var nameFieldProps = this.getFieldProps(nameField);
@@ -110,7 +103,7 @@ var CreateForm = React.createClass({
 				nameFieldProps.placeholder = nameField.label;
 				nameFieldProps.label = false;
 			}
-			form[nameField.path] = React.createElement(Fields[nameField.type], nameFieldProps);
+			form.push(React.createElement(Fields[nameField.type], nameFieldProps));
 		}
 
 		_.each(list.initialFields, function(path) {
@@ -127,20 +120,26 @@ var CreateForm = React.createClass({
 		}, this);
 
 		return (
+			<Form type="horizontal" encType="multipart/form-data" method="post" action={formAction} className="create-form">
+				<input type="hidden" name="action" value="create" />
+				<input type="hidden" name={Keystone.csrf.key} value={Keystone.csrf.value} />
+				<Modal.Header text={'Create a new ' + list.singular} onClose={this.props.onCancel} showCloseButton />
+				<Modal.Body>
+					{this.renderAlerts()}
+					{form}
+				</Modal.Body>
+				<Modal.Footer>
+					<Button type="success" submit>Create</Button>
+					<Button type="link-cancel" onClick={this.props.onCancel}>Cancel</Button>
+				</Modal.Footer>
+			</Form>
+		);
+	},
+
+	render () {
+		return (
 			<Modal isOpen={this.props.isOpen} onCancel={this.props.onCancel} backdropClosesModal>
-				<Form type="horizontal" encType="multipart/form-data" method="post" action={formAction} className="create-form">
-					<input type="hidden" name="action" value="create" />
-					<input type="hidden" name={Keystone.csrf.key} value={Keystone.csrf.value} />
-					<Modal.Header text={'Create a new ' + list.singular} onClose={this.props.onCancel} showCloseButton />
-					<Modal.Body>
-						{alert}
-						{form}
-					</Modal.Body>
-					<Modal.Footer>
-						<Button type="success" submit>Create</Button>
-						<Button type="link-cancel" onClick={this.props.onCancel}>Cancel</Button>
-					</Modal.Footer>
-				</Form>
+				{this.renderForm()}
 			</Modal>
 		);
 	}
