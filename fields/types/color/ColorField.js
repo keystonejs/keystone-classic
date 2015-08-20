cf20e0de59da271352d060b1fed191306647baab
Joss Mackison <joss.mackison@gmail.com>
2015-08-20 13:56:16 +1000

    ColorField: fix re-rendering bug that would make the field jump

--- fields/types/color/ColorField.js
+++ fields/types/color/ColorField.js
@@ -19,17 +19,10 @@ module.exports = Field.create({
 	},
 	
 	renderField () {
-		
-		var colorPreview = null;
-		
-		if (this.props.value) {
-			colorPreview = <div className="field-type-color__preview" style={{ background: this.props.value }} />;
-		}
-		
 		return (
 			<div className="field-type-color__wrapper">
 				<FormInput ref="field" onChange={this.valueChanged} name={this.props.path} value={this.props.value} autoComplete="off" />
-				{colorPreview}
+				<div className="field-type-color__preview" style={{ background: this.props.value }} />
 			</div>
 		);
 	}
