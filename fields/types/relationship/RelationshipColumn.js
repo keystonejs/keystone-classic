55c5806d2258f370c3c42a20bd3b9be50fe02f99
Jed Watson <jed.watson@me.com>
2015-09-02 12:43:32 +1000

    Populating relationship columns in the List view, closes #1617

--- fields/types/relationship/RelationshipColumn.js
+++ fields/types/relationship/RelationshipColumn.js
@@ -7,6 +7,9 @@ var TextColumn = React.createClass({
 		data: React.PropTypes.object,
 		href: React.PropTypes.string
 	},
+	renderCell (children) {
+		return <td>{children}</td>;
+	},
 	renderNoValue () {
 		return (
 			<div className="ItemList__col-value">
@@ -14,22 +17,33 @@ var TextColumn = React.createClass({
 			</div>
 		);
 	},
-	renderValue () {
+	renderMany (value) {
+		var items = [];
+		for (var i = 0; i < 3; i++) {
+			if (!value[i]) break;
+			if (i) {
+				items.push(<span key={'comma' + i} className="ItemList__col-separator">, </span>);
+			}
+			items.push(this.renderValue(value[i]));
+		}
+		if (items.length > 3) {
+			items.push(<span key="more" className="ItemList__col-more">{items.length - 3} more</span>);
+		}
+		return items;
+	},
+	renderValue (value) {
 		var refList = this.props.col.field.refList;
-		var value = this.props.data.fields[this.props.col.path];
 		return (
-			<a href={'/keystone/' + refList.path + '/' + value} className="ItemList__col-value">
-				{value}
+			<a href={'/keystone/' + refList.path + '/' + value.id} key={value.id}>
+				{value.name}
 			</a>
 		);
 	},
 	render: function() {
 		var value = this.props.data.fields[this.props.col.path];
-		return (
-			<td>
-				{value ? this.renderValue() : this.renderNoValue()}
-			</td>
-		);
+		var many = this.props.col.field.many;
+		if (!value || (many && !value.length)) return this.renderCell(this.renderNoValue());
+		return this.renderCell(many ? this.renderMany(value) : this.renderValue(value));
 	}
 });
 
