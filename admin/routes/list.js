97d4361e8ee0a9360f694f8d9ef59993c082c692
Jed Watson <jed.watson@me.com>
2015-09-03 15:56:42 +1000

    Removing old code to load items in the admin list route

--- admin/routes/list.js
+++ admin/routes/list.js
@@ -8,144 +8,15 @@ exports = module.exports = function(req, res) {
 		validationErrors: {},
 		showCreateForm: _.has(req.query, 'new')
 	};
-
-	var sort = { by: req.query.sort || req.list.defaultSort };
-	var filters = req.list.processFilters(req.query.q);
-	var cleanFilters = {};
-	var queryFilters = req.list.getSearchFilters(req.query.search, filters);
-	var columns = (req.query.cols) ? req.list.expandColumns(req.query.cols) : req.list.defaultColumns;
-
-	_.each(filters, function(filter, path) {
-		cleanFilters[path] = _.omit(filter, 'field');
-	});
-
-	if (sort.by) {
-
-		sort.inv = sort.by.charAt(0) === '-';
-		sort.path = (sort.inv) ? sort.by.substr(1) : sort.by;
-		sort.field = req.list.fields[sort.path];
-
-		var clearSort = function() {
-			delete req.query.sort;
-			var qs = querystring.stringify(req.query);
-			return res.redirect(req.path + ((qs) ? '?' + qs : ''));
-		};
-
-		// clear the sort query value if it is the default sort value for the list
-		if (req.query.sort === req.list.defaultSort) {
-			return clearSort();
-		}
-
-		if (sort.field) {
-			// the sort is set to a field, use its label
-			sort.label = sort.field.label;
-			// some fields have custom sort paths
-			if (sort.field.type === 'name') {
-				sort.by = sort.by + '.first ' + sort.by + '.last';
-			}
-		} else if (req.list.get('sortable') && (sort.by === 'sortOrder' || sort.by === '-sortOrder')) {
-			// the sort is set to the built-in sort order, set the label correctly
-			sort.label = 'display order';
-		} else if (req.query.sort) {
-			// it looks like an invalid path has been specified (no matching field), so clear the sort
-			return clearSort();
-		}
-
-	}
-
+	var appName = keystone.get('name') || 'Keystone';
 	var renderView = function() {
-
-		var query = req.list.paginate({ filters: queryFilters, page: req.params.page, perPage: req.list.get('perPage') }).sort(sort.by);
-
-		req.list.selectColumns(query, columns);
-
-		var link_to = function(params) {
-			var p = params.page || '';
-			delete params.page;
-			var queryParams = _.clone(req.query);
-			for (var i in params) {
-				if (params[i] === undefined) {
-					delete params[i];
-					delete queryParams[i];
-				}
-			}
-			params = querystring.stringify(_.defaults(params, queryParams));
-			return '/keystone/' + req.list.path + (p ? '/' + p : '') + (params ? '?' + params : '');
-		};
-
-		query.exec(function(err, items) {
-
-			if (err) {
-				console.error(err);
-				return res.status(500).send('Error querying items:<br><br>' + JSON.stringify(err));
-			}
-
-			// if there were results but not on this page, reset the page
-			if (req.params.page && items.total && !items.results.length) {
-				return res.redirect('/keystone/' + req.list.path);
-			}
-
-			// go straight to the result if there was a search, and only one result
-			if (req.query.search && items.total === 1 && items.results.length === 1) {
-				return res.redirect('/keystone/' + req.list.path + '/' + items.results[0].id);
-			}
-
-			var download_link = '/keystone/download/' + req.list.path;
-			var downloadParams = {};
-
-			if (req.query.q) {
-				downloadParams.q = req.query.q;
-			}
-			if (req.query.search) {
-				downloadParams.search = req.query.search;
-			}
-			if (req.query.cols) {
-				downloadParams.cols = req.query.cols;
-			}
-
-			downloadParams = querystring.stringify(downloadParams);
-
-			if (downloadParams) {
-				download_link += '?' + downloadParams;
-			}
-
-			var appName = keystone.get('name') || 'Keystone';
-
-			/* Simplify data structures for React */
-
-			columns = columns.map(function(col) {
-				col = _.clone(col);
-				if (col.field) col.field = col.field.getOptions();
-				if (col.refList) col.refList = col.refList.getOptions();
-				if (col.subField) col.subField = col.subField.getOptions();
-				return col;
-			});
-
-			items.results = items.results.map(function(i) {
-				return req.list.getData(i);
-			});
-
-			/* Render the list template */
-
-			keystone.render(req, res, 'list', _.extend(viewLocals, {
-				section: keystone.nav.by.list[req.list.key] || {},
-				title: appName + ': ' + req.list.plural,
-				page: 'list',
-				link_to: link_to,
-				download_link: download_link,
-				list: req.list,
-				sort: sort,
-				filters: cleanFilters,
-				search: req.query.search,
-				columns: columns,
-				colPaths: _.pluck(columns, 'path'),
-				items: items,
-				submitted: req.body || {},
-				query: req.query
-			}));
-
-		});
-
+		keystone.render(req, res, 'list', _.extend(viewLocals, {
+			section: keystone.nav.by.list[req.list.key] || {},
+			title: appName + ': ' + req.list.plural,
+			page: 'list',
+			list: req.list,
+			submitted: req.body || {}
+		}));
 	};
 
 	var checkCSRF = function() {
@@ -190,7 +61,6 @@ exports = module.exports = function(req, res) {
 
 		item = new req.list.model();
 		item.save(function(err) {
-
 			if (err) {
 				console.log('There was an error creating the new ' + req.list.singular + ':');
 				console.error(err);
@@ -200,7 +70,6 @@ exports = module.exports = function(req, res) {
 				req.flash('success', 'New ' + req.list.singular + ' ' + req.list.getDocumentName(item) + ' created.');
 				return res.redirect('/keystone/' + req.list.path + '/' + item.id);
 			}
-
 		});
 
 	} else if (!req.list.get('nocreate') && req.method === 'POST' && req.body.action === 'create') {
@@ -233,8 +102,6 @@ exports = module.exports = function(req, res) {
 		});
 
 	} else {
-
 		renderView();
-
 	}
 };
