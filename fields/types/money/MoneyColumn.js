5aad5ff47450d0bf1bb40415fb18d4ad9ec613bf
Joss Mackison <joss.mackison@gmail.com>
2015-09-08 18:21:27 +1000

    cleanup number style columns

--- fields/types/money/MoneyColumn.js
+++ fields/types/money/MoneyColumn.js
@@ -1,16 +1,1 @@
-var React = require('react');
-var numeral = require('numeral');
-
-var MoneyColumn = React.createClass({
-	render: function() {
-		var value = this.props.data.fields[this.props.col.path];
-		var formattedValue = numeral(value).format('$0,0.00');
-		return (
-			<td className="ItemList__col">
-				<div className="ItemList__value ItemList__value--money">{formattedValue ? formattedValue : null}</div>
-			</td>
-		);
-	}
-});
-
-module.exports = MoneyColumn;
+module.exports = require('../number/NumberColumn');
--- fields/types/number/NumberColumn.js
+++ fields/types/number/NumberColumn.js
@@ -1,12 +1,29 @@
-var React = require('react');
+import React from 'react';
+import numeral from 'numeral';
+import ItemsTableCell from '../../../admin/src/components/ItemsTableCell';
+import ItemsTableValue from '../../../admin/src/components/ItemsTableValue';
 
 var NumberColumn = React.createClass({
+	displayName: 'NumberColumn',
+	propTypes: {
+		col: React.PropTypes.object,
+		data: React.PropTypes.object,
+	},
+	renderValue: function() {
+		let value = this.props.data.fields[this.props.col.path];
+		if (!value || isNaN(value)) return null
+
+		let formattedValue = (this.props.col.path === 'money') ? numeral(value).format('$0,0.00') : value;
+
+		return formattedValue;
+	},
 	render: function() {
-		var value = this.props.data.fields[this.props.col.path];
 		return (
-			<td className="ItemList__col">
-				<div className="ItemList__value ItemList__value--number">{ !isNaN(value) ? value : null }</div>
-			</td>
+			<ItemsTableCell>
+				<ItemsTableValue field={this.props.col.path}>
+					{this.renderValue()}
+				</ItemsTableValue>
+			</ItemsTableCell>
 		);
 	}
 });
