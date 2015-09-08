ea4b9bea69ff596683ae1f81a2ddc2913c42b8cc
Joss Mackison <joss.mackison@gmail.com>
2015-09-08 18:35:18 +1000

    cleanup password column

--- fields/types/password/PasswordColumn.js
+++ fields/types/password/PasswordColumn.js
@@ -1,12 +1,24 @@
-var React = require('react');
+import React from 'react';
+import ItemsTableCell from '../../../admin/src/components/ItemsTableCell';
+import ItemsTableValue from '../../../admin/src/components/ItemsTableValue';
 
 var PasswordColumn = React.createClass({
+	displayName: 'PasswordColumn',
+	propTypes: {
+		col: React.PropTypes.object,
+		data: React.PropTypes.object,
+	},
+	renderValue: function() {
+		let value = this.props.data.fields[this.props.col.path];
+		return value ? '********' : '';
+	},
 	render: function() {
-		var value = this.props.data.fields[this.props.col.path];
 		return (
-			<td className="ItemList__col">
-				<div className="ItemList__value ItemList__value--password">{value ? '********' : ''}</div>
-			</td>
+			<ItemsTableCell>
+				<ItemsTableValue field={this.props.col.path}>
+					{this.renderValue()}
+				</ItemsTableValue>
+			</ItemsTableCell>
 		);
 	}
 });
