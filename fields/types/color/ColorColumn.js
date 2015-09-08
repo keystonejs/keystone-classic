cfdd0a0984da1fd091af7d7c55b8af31bdb59223
Joss Mackison <joss.mackison@gmail.com>
2015-09-08 18:45:50 +1000

    cleanup color column

--- fields/types/color/ColorColumn.js
+++ fields/types/color/ColorColumn.js
@@ -1,27 +1,41 @@
-var React = require('react');
+import React from 'react';
+import ItemsTableCell from '../../../admin/src/components/ItemsTableCell';
+import ItemsTableValue from '../../../admin/src/components/ItemsTableValue';
 
 var ColorColumn = React.createClass({
-	render: function() {
-		var value = this.props.data.fields[this.props.col.path];
-		if (!value) {
-			return (
-				<td className="ItemList__col">
-					<div className="ItemList__value ItemList__value--color"></div>
-				</td>
-			);
-		}
-		var colorBoxStyle = {
-			display: 'inline-block',
+	displayName: 'ColorColumn',
+	propTypes: {
+		col: React.PropTypes.object,
+		data: React.PropTypes.object,
+	},
+	renderValue () {
+		let value = this.props.data.fields[this.props.col.path];
+		if (!value) return null;
+
+		let colorBoxStyle = {
 			backgroundColor: value,
-			borderRadius: '5px',
-			marginRight: '10px',
-			height: '20px',
-			width: '20px'
+			borderRadius: 3,
+			display: 'inline-block',
+			height: 18,
+			marginRight: 10,
+			verticalAlign: 'middle',
+			width: 18,
 		};
+
 		return (
-			<td className="ItemList__col">
-				<div className="ItemList__value ItemList__value--color"><span style={colorBoxStyle}></span>{value}</div>
-			</td>
+			<ItemsTableValue truncate={false} field={this.props.col.path}>
+				<div style={{ lineHeight: '18px' }}>
+					<span style={colorBoxStyle} />
+					<span style={{ display: 'inline-block', verticalAlign: 'middle' }}>{value}</span>
+				</div>
+			</ItemsTableValue>
+		);
+	},
+	render: function() {
+		return (
+			<ItemsTableCell>
+				{this.renderValue()}
+			</ItemsTableCell>
 		);
 	}
 });
