19785b7ca4885a152e6720c1363bb2500ad8cc1c
Joss Mackison <joss.mackison@gmail.com>
2015-09-08 18:35:46 +1000

    cleanup select column

--- fields/types/select/SelectColumn.js
+++ fields/types/select/SelectColumn.js
@@ -1,16 +1,26 @@
-var React = require('react');
+import React from 'react';
+import ItemsTableCell from '../../../admin/src/components/ItemsTableCell';
+import ItemsTableValue from '../../../admin/src/components/ItemsTableValue';
 
 var SelectColumn = React.createClass({
+	displayName: 'SelectColumn',
+	propTypes: {
+		col: React.PropTypes.object,
+		data: React.PropTypes.object,
+	},
 	renderValue () {
 		var value = this.props.data.fields[this.props.col.path];
 		var option = this.props.col.field.ops.filter(i => i.value === value)[0];
-		return option ? <span>{option.label}</span> : '';
+
+		return option ? option.label : null;
 	},
 	render () {
 		return (
-			<td>
-				<div>{this.renderValue()}</div>
-			</td>
+			<ItemsTableCell>
+				<ItemsTableValue field={this.props.col.path}>
+					{this.renderValue()}
+				</ItemsTableValue>
+			</ItemsTableCell>
 		);
 	}
 });
