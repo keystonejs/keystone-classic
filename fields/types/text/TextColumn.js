e00bef2f81b94dc6ad0da1cef001f9a559d585c6
Joss Mackison <joss.mackison@gmail.com>
2015-09-08 16:49:47 +1000

    cleanup text columns

--- fields/types/text/TextColumn.js
+++ fields/types/text/TextColumn.js
@@ -1,34 +1,26 @@
-var React = require('react');
+import React from 'react';
+import ItemsTableCell from '../../../admin/src/components/ItemsTableCell';
+import ItemsTableValue from '../../../admin/src/components/ItemsTableValue';
 
 var TextColumn = React.createClass({
+	displayName: 'TextColumn',
 	propTypes: {
 		col: React.PropTypes.object,
-		list: React.PropTypes.object,
 		data: React.PropTypes.object,
-		href: React.PropTypes.string
+		linkTo: React.PropTypes.string,
 	},
+	// cropping text is necessary for textarea, which uses this column
 	renderValue: function() {
-		return this.props.data.fields[this.props.col.path];
-	},
-	renderText () {
-		return (
-			<div className="ItemList__value ItemList__value--text">
-				{this.renderValue()}
-			</div>
-		);
-	},
-	renderLink () {
-		return (
-			<a href={this.props.linkTo} className="ItemList__value ItemList__value--text ItemList__link--padded ItemList__link--interior">
-				{this.renderValue()}
-			</a>
-		);
+		let value = this.props.data.fields[this.props.col.path];
+		return value ? value.substr(0, 100) : null;
 	},
 	render: function() {
 		return (
-			<td className="ItemList__col">
-				{this.props.linkTo ? this.renderLink() : this.renderText()}
-			</td>
+			<ItemsTableCell>
+				<ItemsTableValue href={this.props.linkTo} padded interior field={this.props.col.path}>
+					{this.renderValue()}
+				</ItemsTableValue>
+			</ItemsTableCell>
 		);
 	}
 });
--- fields/types/textarea/TextareaColumn.js
+++ fields/types/textarea/TextareaColumn.js
@@ -1,14 +1,1 @@
-var React = require('react');
-
-var TextareaColumn = React.createClass({
-	render: function() {
-		var value = this.props.data.fields[this.props.col.path];
-		return (
-			<td>
-				<div className='ItemList__value'>{value ? value.substring(0, 500) : null}</div>
-			</td>
-		);
-	}
-});
-
-module.exports = TextareaColumn;
+module.exports = require('../text/TextColumn');
