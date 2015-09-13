f49e0466ee6f46a0dabd9b29eb1dddcf4237ba4e
Jed Watson <jed.watson@me.com>
2015-09-13 20:23:12 +1000

    Cleaning up AltText component

--- admin/src/components/AltText.js
+++ admin/src/components/AltText.js
@@ -1,11 +1,8 @@
-var React = require('react');
-var blacklist = require('blacklist');
-var vkey = require('vkey');
+import React from 'react';
+import blacklist from 'blacklist';
+import vkey from 'vkey';
 
 var AltText = React.createClass({
-
-	displayName: 'AltText',
-
 	getDefaultProps () {
 		return {
 			component: 'span',
@@ -14,43 +11,35 @@ var AltText = React.createClass({
 			modified: ''
 		};
 	},
-
 	getInitialState () {
 		return {
 			modified: false
 		};
 	},
-
 	componentDidMount () {
 		document.body.addEventListener('keydown', this.handleKeyDown, false);
 		document.body.addEventListener('keyup', this.handleKeyUp, false);
 	},
-
+	componentWillUnmount () {
+		document.body.removeEventListener('keydown', this.handleKeyDown);
+		document.body.removeEventListener('keyup', this.handleKeyUp);
+	},
 	handleKeyDown (e) {
 		if (vkey[e.keyCode] !== this.props.modifier) return;
 		this.setState({
 			modified: true
 		});
 	},
-
 	handleKeyUp (e) {
 		if (vkey[e.keyCode] !== this.props.modifier) return;
 		this.setState({
 			modified: false
 		});
 	},
-
-	componentWillUnmount () {
-		document.body.removeEventListener('keydown', this.handleKeyDown);
-		document.body.removeEventListener('keyup', this.handleKeyUp);
-	},
-
 	render () {
 		var props = blacklist(this.props, 'component', 'modifier', 'normal', 'modified');
-
 		return React.createElement(this.props.component, props, this.state.modified ? this.props.modified : this.props.normal);
 	}
-
 });
 
 module.exports = AltText;
