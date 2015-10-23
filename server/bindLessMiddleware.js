050b452c5e853299c2bed5444ea6a288ecdf3470
Jed Watson <jed.watson@me.com>
2015-10-24 01:46:33 +1100

    Moving LESS middleware to server/bindLessMiddleware

---
+++ server/bindLessMiddleware.js
@@ -0,0 +1,17 @@
+module.exports = function bindLessMiddleware (keystone, app) {
+	// the less option can be a single path, or array of paths
+	// when set, we configure the less middleware
+
+	var lessPaths = keystone.get('less');
+	var lessOptions = keystone.get('less options') || {};
+
+	if (typeof lessPaths === 'string') {
+		lessPaths = [lessPaths];
+	}
+
+	if (Array.isArray(lessPaths)) {
+		lessPaths.forEach(function(value) {
+			app.use(require('less-middleware')(keystone.expandPath(value), lessOptions));
+		});
+	}
+};
