dfeb1c50948216dce5009e22ac904d53d92ba0c9
Jed Watson <jed.watson@me.com>
2015-10-24 02:03:48 +1100

    This should be keystone, need require('path')

--- server/initViewEngine.js
+++ server/initViewEngine.js
@@ -1,10 +1,12 @@
+var path = require('path');
+
 module.exports = function initViewEngine (keystone, app) {
 	// Allow usage of custom view engines
-	if (this.get('custom engine')) {
-		app.engine(this.get('view engine'), this.get('custom engine'));
+	if (keystone.get('custom engine')) {
+		app.engine(keystone.get('view engine'), keystone.get('custom engine'));
 	}
 
 	// Set location of view templates and view engine
-	app.set('views', this.getPath('views') || path.sep + 'views');
-	app.set('view engine', this.get('view engine'));
+	app.set('views', keystone.getPath('views') || path.sep + 'views');
+	app.set('view engine', keystone.get('view engine'));
 };
