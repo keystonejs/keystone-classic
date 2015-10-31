a146e39f4d44d32bcd0d912380225ff97a1d8981
Jed Watson <jed.watson@me.com>
2015-10-31 21:48:39 +1100

    Removing unnecessary debug

--- server/bindBodyParser.js
+++ server/bindBodyParser.js
@@ -1,12 +1,10 @@
 var multer = require('multer');
 var bodyParser = require('body-parser');
-var debug = require('debug');
 
 module.exports = function bindIPRestrictions (keystone, app) {
 	// Set up body options and cookie parser
 	var bodyParserParams = {};
 	if (keystone.get('file limit')) {
-		debug('adding file limit');
 		bodyParserParams.limit = keystone.get('file limit');
 	}
 	app.use(bodyParser.json(bodyParserParams));
