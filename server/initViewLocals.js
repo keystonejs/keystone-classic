f9db7ec63026737230619a037643b35d529bec7b
jpersson <jonathan.persson@creuna.se>
2015-11-09 21:37:42 -0500

    Remove redundant Object.assign() polyfill
    Since `babel-plugin-object-assign` is already used in the codebase this
    polyfill is redundant

--- server/initViewLocals.js
+++ server/initViewLocals.js
@@ -1,9 +1,7 @@
-var assign = require('object-assign');
-
 module.exports = function initTrustProxy (keystone, app) {
 	// Apply locals
 	if (typeof keystone.get('locals') === 'object') {
-		assign(app.locals, keystone.get('locals'));
+		Object.assign(app.locals, keystone.get('locals'));
 	}
 
 	// Default "pretty html" mode except in production
