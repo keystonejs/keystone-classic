fee03f71eb57b9c9ae72c5c8bd4233f165e13af1
jpersson <jonathan.persson@creuna.se>
2015-11-25 11:45:07 -0500

    Remove `underscore` dependency in `evalDependsOn`

--- fields/utils/evalDependsOn.js
+++ fields/utils/evalDependsOn.js
@@ -1,18 +1,20 @@
-var _ = require('underscore');
+function isObject(arg) {
+    return Object.prototype.toString.call(arg) === '[object Object]';
+};
 
 module.exports = function evalDependsOn(dependsOn, values) {
-	if (!_.isObject(dependsOn)) return true;
-	var keys = _.keys(dependsOn);
-	return (keys.length) ? _.every(keys, function(key) {
+	if (!isObject(dependsOn)) return true;
+	var keys = Object.keys(dependsOn);
+	return (keys.length) ? keys.every(function(key) {
 		var dependsValue = dependsOn[key];
-		if (_.isBoolean(dependsValue)) {
-			if (_.isBoolean(values[key])) {
+		if (typeof dependsValue === 'boolean') {
+			if (typeof values[key] === 'boolean') {
 				return dependsValue === values[key];
 			} else {
-				return dependsValue !== _.isEmpty(values[key]);
+				return dependsValue !== (Object.keys(values[key]).length === 0);
 			}
 		}
-		var matches = _.isArray(dependsValue) ? dependsValue : [dependsValue];
-		return _.contains(matches, values[key]);
-	}, this) : true;
+		var matches = Array.isArray(dependsValue) ? dependsValue : [dependsValue];
+		return matches.indexOf(values[key]) !== -1;
+	}) : true;
 };
