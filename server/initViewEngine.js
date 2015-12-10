afc6527590c8cb9b430c2875dae27fc650f93393
Jed Watson <jed.watson@me.com>
2015-12-11 09:10:54 +1100

    Don't set the view option when it's not initialised
    cc @ericelliott @Rmannn

--- server/initViewEngine.js
+++ server/initViewEngine.js
@@ -9,5 +9,9 @@ module.exports = function initViewEngine (keystone, app) {
 	// Set location of view templates and view engine
 	app.set('views', keystone.getPath('views') || path.sep + 'views');
 	app.set('view engine', keystone.get('view engine'));
-	app.set('view', keystone.get('view'));
+
+	var customView = keystone.get('view');
+	if (customView) {
+		app.set('view', customView);
+	}
 };
