308c01df87599d8c914d566d10e8d3affee012c8
Andrew Linfoot <ajlin500@yahoo.com>
2016-01-26 19:18:40 -0500

    Add: ability to specify Date inputFormat

--- fields/types/date/DateType.js
+++ fields/types/date/DateType.js
@@ -12,9 +12,10 @@ function date(list, path, options) {
 	this._nativeType = Date;
 	this._underscoreMethods = ['format', 'moment', 'parse'];
 	this._fixedSize = 'medium';
-	this._properties = ['formatString', 'yearRange', 'isUTC'];
-	this.parseFormatString = options.parseFormat || 'YYYY-MM-DD';
+	this._properties = ['formatString', 'yearRange', 'isUTC', 'inputFormat'];
+	this.parseFormatString = options.inputFormat || 'YYYY-MM-DD';
 	this.formatString = (options.format === false) ? false : (options.format || 'Do MMM YYYY');
+
 	this.yearRange = options.yearRange;
 	this.isUTC = options.utc || false;
 	if (this.formatString && 'string' !== typeof this.formatString) {
