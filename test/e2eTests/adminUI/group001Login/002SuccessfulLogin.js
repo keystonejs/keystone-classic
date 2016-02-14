c8499ed43da235c5d46faaf59cb4fe10ea663829
webteckie <avp4carlos@gmail.com>
2016-02-13 20:42:27 -0500

    Another attempt at successfully building/running in travis with saucelabs integration.

---
+++ test/e2eTests/adminUI/group001Login/002SuccessfulLogin.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'Keystone should allow admins to login to the Admin UI' : function (client) {
+    client
+      .url('http://localhost:3000/keystone')
+      .waitForElementVisible('div#signin-view', 3000)
+      .setValue('input[name=email]', 'test@test.e2e')
+      .setValue('input[name=password]', 'test')
+      .click('button[type=submit]')
+      .pause(1000)
+      .url('http://localhost:3000/keystone')  // just in case we're redirected somewhere other than home page
+      .waitForElementVisible('div#home-view', 1000)
+      .pause(1000)
+      .end();
+  }
+};
