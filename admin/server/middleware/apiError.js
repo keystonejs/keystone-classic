7ddea2b135d934f3f01ed1c74b14b0cdc8957680
Jed Watson <jed.watson@me.com>
2016-02-15 16:46:38 +1100

    Rethinking the standard apiError response middleware for the Admin server

--- admin/server/middleware/apiError.js
+++ admin/server/middleware/apiError.js
@@ -1,21 +1,35 @@
+/*
+	This middleware simplifies returning errors from the API.
+
+	Example usage patterns and expected responses:
+		apiError('database error', err) => 500 { err }
+		apiError(403, 'invalid csrf') => 403 { error: 'invalid csrf' }
+		apiError(403, 'validation errors', err) => 403 { error: 'validation errors', detail: err }
+		apiError(403, 'not allowed', 'You can not delete yourself') => 403 { error: 'not allowed', detail: 'You can not delete yourself' }
+		apiError(400, err) => 400 { err }
+		apiError(err) => 500 { err }
+*/
+
 module.exports = function (req, res, next) {
-	res.apiError = function apiError (key, err) {
-		var statusCode = 500;
-		if (key === 404) {
-			statusCode = 404;
-			key = 'not found';
+	res.apiError = function apiError (statusCode, error, detail) {
+		if (typeof statusCode !== 'number' && arguments.length < 3) {
+			detail = error;
+			error = statusCode;
+			statusCode = 500;
 		}
-		if (!key) {
-			key = 'unknown error';
+		if (statusCode) {
+			res.status(statusCode);
 		}
-		if (!err) {
-			err = 'API Error';
+		if (error instanceof Error) {
+			error = error.name !== 'Error' ? error.name + ': ' + error.message : error.message;
 		}
-		if (typeof err === 'object' && err.message) {
-			err = err.message;
+		if (detail instanceof Error) {
+			detail = detail.name !== 'Error' ? detail.name + ': ' + detail.message : detail.message;
 		}
-		res.status(statusCode);
-		res.json({ err: err, key: key });
+		var data = typeof error === 'string' || (error && detail)
+			? { error: error, detail: detail }
+			: error;
+		res.json(data);
 	};
 	next();
 };
