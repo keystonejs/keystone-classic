74cdceb0f60abeef3ca5a26aa9820b578281534c
Jed Watson <jed.watson@me.com>
2016-02-15 16:53:03 +1100

    Removing console.log calls from the create API
    These shouldn’t be here, they don’t respect the keystone logger settings

--- admin/server/api/list/create.js
+++ admin/server/api/list/create.js
@@ -2,11 +2,9 @@ var keystone = require('../../../../');
 
 module.exports = function (req, res) {
 	if (!keystone.security.csrf.validate(req)) {
-		console.log(`Refusing to create item; CSRF failure`);
 		return res.apiError(403, 'invalid csrf');
 	}
 	if (req.list.get('nocreate')) {
-		console.log(`Refusing to create item; List.nocreate is true`);
 		return res.apiError(400, 'nocreate');
 	}
 	var item = new req.list.model();
