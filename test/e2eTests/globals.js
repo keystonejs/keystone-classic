ab415973ebf620d79d833de3e9107b5f03989313
Jed Watson <jed.watson@me.com>
2016-02-16 08:36:34 +1100

    Cleaning up code style in e2eTests
    The actual test files still need cleanup

--- test/e2eTests/globals.js
+++ test/e2eTests/globals.js
@@ -1,48 +1,48 @@
 module.exports = {
-    // this controls whether to abort the test execution when an assertion failed and skip the rest
-    // it's being used in waitFor commands and expect assertions
-    abortOnAssertionFailure : true,
+	// this controls whether to abort the test execution when an assertion failed and skip the rest
+	// it's being used in waitFor commands and expect assertions
+	abortOnAssertionFailure : true,
 
-    // this will overwrite the default polling interval (currently 500ms) for waitFor commands
-    // and expect assertions that use retry
-    waitForConditionPollInterval : 300,
+	// this will overwrite the default polling interval (currently 500ms) for waitFor commands
+	// and expect assertions that use retry
+	waitForConditionPollInterval : 300,
 
-    // default timeout value in milliseconds for waitFor commands and implicit waitFor value for
-    // expect assertions
-    waitForConditionTimeout : 5000,
+	// default timeout value in milliseconds for waitFor commands and implicit waitFor value for
+	// expect assertions
+	waitForConditionTimeout : 5000,
 
-    // this will cause waitFor commands on elements to throw an error if multiple
-    // elements are found using the given locate strategy and selector
-    throwOnMultipleElementsReturned : true,
+	// this will cause waitFor commands on elements to throw an error if multiple
+	// elements are found using the given locate strategy and selector
+	throwOnMultipleElementsReturned : true,
 
-    // controls the timeout time for async hooks. Expects the done() callback to be invoked within this time
-    // or an error is thrown
-    asyncHookTimeout : 10000,
+	// controls the timeout time for async hooks. Expects the done() callback to be invoked within this time
+	// or an error is thrown
+	asyncHookTimeout : 10000,
 
-    'default' : {
-    },
+	'default' : {
+	},
 
-    'saucelabs' : {
-    },
+	'saucelabs' : {
+	},
 
-    before : function(cb) {
-        cb();
-    },
+	before : function(cb) {
+		cb();
+	},
 
-    beforeEach : function(browser, cb) {
-        cb();
-    },
+	beforeEach : function(browser, cb) {
+		cb();
+	},
 
-    after : function(cb) {
-        cb();
-    },
+	after : function(cb) {
+		cb();
+	},
 
-    afterEach : function(browser, cb) {
-        cb();
-    },
+	afterEach : function(browser, cb) {
+		cb();
+	},
 
-    reporter : function(results, cb) {
-        //console.log(results);
-        cb();
-    }
+	reporter : function(results, cb) {
+		//console.log(results);
+		cb();
+	},
 };
--- test/e2eTests/server.js
+++ test/e2eTests/server.js
@@ -5,43 +5,42 @@ var engine = ReactEngine.server.create({});
 var Nightwatch = require('nightwatch/lib/index.js');
 
 keystone.init({
-    'name': 'e2e',
-    'brand': 'e2e',
+	'name': 'e2e',
+	'brand': 'e2e',
 
-    'less': 'public',
-    'static': 'public',
-    'favicon': 'public/favicon.ico',
-    'views': 'templates/views',
-    'view engine': '.jsx',
-    'custom engine': engine,
-    'view': view,
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': '.jsx',
+	'custom engine': engine,
+	'view': view,
 
-    'auto update': true,
-    'session': true,
-    'auth': true,
-    'user model': 'User',
-    'cookie secret': 'Secret'
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+	'cookie secret': 'Secret',
 });
 
 keystone.import('../models');
 
 keystone.start({
-    onStart: function() {
-        console.log('keystone done starting');
-
-        try {
-            Nightwatch.cli(function(argv) {
-                Nightwatch.runner(argv, function(){
-                    process.exit();
-                });
-            });
-        } catch (ex) {
-            console.error('There was an error while starting the nightwatch test runner:\n\n');
-            process.stderr.write(ex.stack + '\n');
-            process.exit(2);
-        }
-    },
-    onMount: function(){
-        console.log('keystone done mounting');
-    }
+	onMount: function(){
+		console.log('KeystoneJS Mounted Successfuly');
+	},
+	onStart: function() {
+		console.log('KeystoneJS Started Successfully');
+		try {
+			Nightwatch.cli(function(argv) {
+				Nightwatch.runner(argv, function(){
+					process.exit();
+				});
+			});
+		} catch (ex) {
+			console.error('\nThere was an error while starting the nightwatch test runner:\n\n');
+			process.stderr.write(ex.stack + '\n');
+			process.exit(2);
+		}
+	},
 });
