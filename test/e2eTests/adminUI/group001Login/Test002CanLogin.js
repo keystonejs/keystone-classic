f13cc99ada056c42df77b5fbdf2ef3368ac975e8
webteckie <avp4carlos@gmail.com>
2016-02-15 17:42:03 -0500

    Increase e2e test timeouts as the latency between travis and saucelabs may need it.

--- test/e2eTests/adminUI/group001Login/Test002CanLogin.js
+++ test/e2eTests/adminUI/group001Login/Test002CanLogin.js
@@ -2,13 +2,13 @@ module.exports = {
   'Keystone should allow admins to login to the Admin UI' : function (client) {
     client
       .url('http://localhost:3000/keystone')
-      .waitForElementVisible('div#signin-view', 3000)
+      .waitForElementVisible('div#signin-view', 30000)
       .setValue('input[name=email]', 'test@test.e2e')
       .setValue('input[name=password]', 'test')
       .click('button[type=submit]')
       .pause(1000)
       .url('http://localhost:3000/keystone')  // just in case we're redirected somewhere other than home page
-      .waitForElementVisible('div#home-view', 1000)
+      .waitForElementVisible('div#home-view', 10000)
       .pause(1000)
       .end();
   }
