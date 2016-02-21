6c6e3d30d40998bbdd37cf99a7ba5fa2080e281b
webteckie <avp4carlos@gmail.com>
2016-02-20 20:15:29 -0500

    e2e: attempt to bypass logging errors at start of tests by delaying the tests execution to give the keystone test app some time to settle.

--- test/e2eTests/server.js
+++ test/e2eTests/server.js
@@ -4,6 +4,7 @@ var ReactEngine = require('react-engine');
 var view = require('react-engine/lib/expressView');
 var engine = ReactEngine.server.create({});
 var request = require('superagent');
+var moment = require('moment');
 var Nightwatch = require('nightwatch/lib/index.js');
 
 keystone.init({
@@ -27,8 +28,13 @@ keystone.init({
 
 keystone.import('../models');
 
+keystone.set('nav', {
+	'access': ['users'],
+	'content': ['posts'],
+});
+
 function checkKeystoneReady(callback, results){
-	console.log('Checking if KeystoneJS ready for request.');
+	console.log([moment().format('HH:mm:ss:SSS')] + ' e2e: checking if KeystoneJS ready for request.');
 	request
 		.get('http://localhost:3000/keystone')
 		.end(callback);
@@ -48,22 +54,33 @@ function runNightwatch() {
 	}
 }
 
+console.log([moment().format('HH:mm:ss:SSS')] + ' e2e: starting KeystoneJS...');
+
 keystone.start({
 	onMount: function(){
-		console.log('KeystoneJS Mounted Successfuly');
+		console.log([moment().format('HH:mm:ss:SSS')] + ' e2e: KeystoneJS mounted Successfuly');
 	},
 	onStart: function() {
-		console.log('KeystoneJS Started Successfully');
+		console.log([moment().format('HH:mm:ss:SSS')] + ' e2e: KeystoneJS Started Successfully');
 
-		// make sure keystone returns 200 before starting Nightwatch testing
-		async.retry({times: 10, interval: 3000}, checkKeystoneReady, function(err, result) {
-			if (!err) {
-				console.log('KeystoneJS Ready!');
-				runNightwatch();
-			} else {
-				console.log('Nightwatch tests not ran!');
-				process.exit();
-			}
-		});
+		// if --notest was specified then do not run the test; the user wants to
+		// just run the keystone server app.
+		if (process.argv.indexOf('--notest') == -1) {
+			// in travis-ci keystone does not seem to be ready for some 10 seconds or so
+			// after the onStart fires.  The following is to account for that and prevent
+			// any errors in the travis log.
+			setTimeout(function() {
+				// make sure keystone returns 200 before starting Nightwatch testing
+				async.retry({times: 10, interval: 3000}, checkKeystoneReady, function (err, result) {
+					if (!err) {
+						console.log([moment().format('HH:mm:ss:SSS')] + ' e2e: KeystoneJS Ready!');
+						runNightwatch();
+					} else {
+						console.log([moment().format('HH:mm:ss:SSS')] + ' e2e: Nightwatch tests not ran!');
+						process.exit();
+					}
+				})
+			}, 10000);
+		}
 	},
 });
