69091ae1a89f5e7ee8512c86808434d9f3c1b864
Jed Watson <jed.watson@me.com>
2016-02-26 14:45:19 +1100

    Fixing inconsistent whitespace in dependsOn test

--- test/unit/lib/list/dependsOn.js
+++ test/unit/lib/list/dependsOn.js
@@ -9,67 +9,67 @@ var DependsOn = keystone.list('DependsOn');
 
 describe('Test dependsOn and required', function() {
 
-    it('Ignore required if evalDependsOn is not `true` by setting `state` to `draft`', function(done) {
-        // remove any Post documents
-        DependsOn.model.find({}).remove(function(error) {
-            if (error) {
-                done(error);
-            }
+	it('Ignore required if evalDependsOn is not `true` by setting `state` to `draft`', function(done) {
+		// remove any Post documents
+		DependsOn.model.find({}).remove(function(error) {
+			if (error) {
+				done(error);
+			}
 
 			var newPost = new DependsOn.model({
 				title: 'new post',
 				state: 'draft'
 			});
-			
-            newPost.save(done);
-           
-        });
-    });
+
+			newPost.save(done);
+
+		});
+	});
 
 
 
-    it('Save will fail if `state` set to `published` and `publishedDate` is not defined', function(done) {   
-        // remove any Post documents
-        DependsOn.model.find({}).remove(function(error) {
-            if (error) {
-                done(error);
-            }
+	it('Save will fail if `state` set to `published` and `publishedDate` is not defined', function(done) {
+		// remove any Post documents
+		DependsOn.model.find({}).remove(function(error) {
+			if (error) {
+				done(error);
+			}
 
 			var newPost = new DependsOn.model({
 				title: 'new post',
 				state: 'published'
 			});
-            newPost.save(function(err) {
+			newPost.save(function(err) {
 				demand(err).be.a.object();
 				done();
 			});
-           
-        });
 
-    });
+		});
 
-    it('Save will succeed if `state` set to `published` and `publishedDate` is defined', function(done) {
+	});
 
-        // remove any Post documents
-        DependsOn.model.find({}).remove(function(error) {
-            if (error) {
-                done(error);
-            }
+	it('Save will succeed if `state` set to `published` and `publishedDate` is defined', function(done) {
+
+		// remove any Post documents
+		DependsOn.model.find({}).remove(function(error) {
+			if (error) {
+				done(error);
+			}
 
 			var newPost = new DependsOn.model({
 				title: 'new post',
 				state: 'published',
 				publishedDate: new Date()
 			});
-            newPost.save(done);
-           
-        });
-    });
+			newPost.save(done);
 
-    after(function(done) {
-        // remove any remaining test data
-        DependsOn.model.find({}).remove(function(error) {
-            done(error);
-        });
-    });
+		});
+	});
+
+	after(function(done) {
+		// remove any remaining test data
+		DependsOn.model.find({}).remove(function(error) {
+			done(error);
+		});
+	});
 });
