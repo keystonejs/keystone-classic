53ace203092a9121f0fcb2796ca4114cd307eea1
Jed Watson <jed.watson@me.com>
2016-03-02 23:59:20 +1100

    Adding validateInput and validateRequiredInput for GeoPoint fields

--- fields/types/geopoint/GeoPointType.js
+++ fields/types/geopoint/GeoPointType.js
@@ -1,6 +1,7 @@
 var _ = require('underscore');
 var FieldType = require('../Type');
 var util = require('util');
+var utils = require('keystone-utils');
 
 // Validation and value parsing regular expression
 var REGEXP_LNGLAT = /^\s*(\-?\d+(?:\.\d+)?)\s*\,\s*(\-?\d+(?:\.\d+)?)\s*$/;
@@ -43,6 +44,34 @@ geopoint.prototype.format = function (item) {
 		return item.get(this.path).reverse().join(', ');
 	}
 	return null;
+};
+
+/**
+ * Asynchronously confirms that the provided value is valid
+ */
+geopoint.prototype.validateInput = function (data, callback) {
+	var value = this.getValueFromData(data);
+	var result = false;
+	if (value === undefined) {
+		result = true;
+	} else {
+		if (Array.isArray(value)) {
+			value = value.length === 2 ? value.join(',') : '';
+		}
+		if (typeof values === 'string') {
+			result = REGEXP_LNGLAT.test(value);
+		}
+	}
+	utils.defer(callback, result);
+};
+
+/**
+ * Asynchronously confirms that the a value is present
+ */
+geopoint.prototype.validateRequiredInput = function (item, data, callback) {
+	var value = this.getValueFromData(data);
+	var result = (value || (value === undefined && item.get(this.path) && item.get(this.path).length === 2)) ? true : false;
+	utils.defer(callback, result);
 };
 
 /**
