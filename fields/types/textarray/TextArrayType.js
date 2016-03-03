c13058be12247da162dfc6b2f672c2aa48f3ae21
Jed Watson <jed.watson@me.com>
2016-03-03 23:37:02 +1100

    Adding validateInput and validateRequiredInput for textarray fields

--- fields/types/textarray/TextArrayType.js
+++ fields/types/textarray/TextArrayType.js
@@ -14,6 +14,48 @@ function textarray (list, path, options) {
 util.inherits(textarray, FieldType);
 
 /**
+ * Asynchronously confirms that the provided value is valid
+ */
+textarray.prototype.validateInput = function (data, callback) {
+	var value = this.getValueFromData(data);
+	var result = true;
+	if (value !== undefined) {
+		if (!Array.isArray(value)) {
+			value = [value];
+		}
+		value.forEach(function (str) {
+			if (typeof str !== 'string') {
+				result = false;
+			}
+		});
+	}
+	utils.defer(callback, result);
+};
+
+/**
+ * Asynchronously confirms that the a value is present
+ */
+textarray.prototype.validateRequiredInput = function (item, data, callback) {
+	var value = this.getValueFromData(data);
+	var result = false;
+	if (value === undefined) {
+		if (item.get(this.path) && item.get(this.path).length) {
+			result = true;
+		}
+	} else if (typeof value === 'string') {
+		result = true;
+	} else if (Array.isArray(value)) {
+		for (var i = 0; i < value.length; i++) {
+			if (typeof value[i] === 'string' && value[i].length) {
+				result = true;
+				break;
+			}
+		}
+	}
+	utils.defer(callback, result);
+};
+
+/**
  * Validates that a value for this field has been provided in a data object
  *
  * Deprecated
