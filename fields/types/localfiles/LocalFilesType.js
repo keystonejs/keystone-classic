3194afbeeee60e51f5bf04f6e4dc2ba5a7b2143c
nectarabica <admin@atlasntic.com>
2016-03-09 15:04:10 +0000

    replacing lodash .containts by .includes
    _.contains has been removed in lodash v4 - use _.includes instead

--- fields/types/localfiles/LocalFilesType.js
+++ fields/types/localfiles/LocalFilesType.js
@@ -297,7 +297,7 @@ localfiles.prototype.uploadFiles = function (item, files, update, callback) {
 		var filename = prefix + file.name;
 		var filetype = file.mimetype || file.type;
 
-		if (field.options.allowedTypes && !_.contains(field.options.allowedTypes, filetype)) {
+		if (field.options.allowedTypes && !_.includes(field.options.allowedTypes, filetype)) {
 			return processedFile(new Error('Unsupported File Type: ' + filetype));
 		}
 
