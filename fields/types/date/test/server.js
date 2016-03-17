3ceb52851ae98b4fafdd901a0f59b4c1fe64206a
Max <contact@mstoiber.com>
2016-03-17 16:24:24 +1100

    Structure and add missing tests to date field

--- fields/types/date/test/server.js
+++ fields/types/date/test/server.js
@@ -8,36 +8,56 @@ exports.initList = function (List) {
 };
 
 exports.testFieldType = function (List) {
-	var testItem = new List.model();
-
 	it('should parse date input and return a moment object', function () {
 		var m = List.fields.date.parse('2016-02-27');
 		demand(m.format('YYYY-MM-DD')).equal('2016-02-27');
 	});
 
 	it('should format the date value using moment', function () {
+		var testItem = new List.model();
 		testItem.date = new Date(2013, 11, 4);
 		demand(testItem._.date.format()).equal('4th Dec 2013');
 		demand(testItem._.date.format('YYYYMMDD')).equal('20131204');
 	});
 
 	it('should return a moment object set to the field value', function () {
+		var testItem = new List.model();
 		testItem.date = new Date(2013, 11, 4);
 		demand(testItem._.date.moment()._isAMomentObject);
 		demand(testItem._.date.moment().format('YYYYMMDD')).equal('20131204');
 	});
 
-	it('should validate input present', function (done) {
-		List.fields.date.validateRequiredInput(this, { date: 'a' }, function (result) {
-			demand(result).be(true);
-			done();
+	describe('validateRequiredInput', function () {
+		it('should validate input present', function (done) {
+			var testItem = new List.model();
+			List.fields.date.validateRequiredInput(testItem, { date: 'a' }, function (result) {
+				demand(result).be(true);
+				done();
+			});
 		});
-	});
 
-	it('should invalidate input not present', function (done) {
-		List.fields.date.validateRequiredInput(this, { date: '' }, function (result) {
-			demand(result).be(false);
-			done();
+		it('should invalidate empty string', function (done) {
+			var testItem = new List.model();
+			List.fields.date.validateRequiredInput(testItem, { date: '' }, function (result) {
+				demand(result).be(false);
+				done();
+			});
+		});
+
+		it('should invalidate null', function (done) {
+			var testItem = new List.model();
+			List.fields.date.validateRequiredInput(testItem, { date: null }, function (result) {
+				demand(result).be(false);
+				done();
+			});
+		});
+
+		it('should invalidate undefined', function (done) {
+			var testItem = new List.model();
+			List.fields.date.validateRequiredInput(testItem, { date: undefined }, function (result) {
+				demand(result).be(false);
+				done();
+			});
 		});
 	});
 };
