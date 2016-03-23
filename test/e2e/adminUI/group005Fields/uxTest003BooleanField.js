e67a0f2129bdf612bd437bbdb37eeaa9deb0c3a1
John Oerter <joerter@gmail.com>
2016-03-22 21:27:47 -0500

    test(e2e): ux boolean field

---
+++ test/e2e/adminUI/group005Fields/uxTest003BooleanField.js
@@ -0,0 +1,54 @@
+var adminUI = require('../adminUI');
+
+module.exports = {
+	before: function (browser) {
+		browser
+			.url(adminUI.url)
+			.waitForElementVisible(adminUI.cssSelector.signinView.id)
+			.setValue(adminUI.cssSelector.signinView.emailInput, adminUI.login.email)
+			.setValue(adminUI.cssSelector.signinView.passwordInput, adminUI.login.password)
+			.pause(browser.globals.defaultPauseTimeout)
+			.click(adminUI.cssSelector.signinView.submitButton)
+			.pause(browser.globals.defaultPauseTimeout)
+			.url(adminUI.url)
+			.waitForElementVisible(adminUI.cssSelector.homeView.id)
+			.pause(browser.globals.defaultPauseTimeout);
+	},
+	after: function (browser) {
+		browser
+			.click(adminUI.cssSelector.allView.logoutIconLink)
+			.pause(browser.globals.defaultPauseTimeout)
+			.end();
+	},
+	'Boolean field can be created via the initial modal': function (browser) {
+		browser
+			.click(adminUI.cssSelector.homeView.plusIconLinkForBooleansTabUnderDashboardFieldsSubheading)
+			.waitForElementVisible(adminUI.cssSelector.initialModalView.id)
+			.pause(browser.globals.defaultPauseTimeout)
+			.setValue(adminUI.cssSelector.initialModalView.fieldType.bool.bool.name.value, 'One')
+			.pause(browser.globals.defaultPauseTimeout)
+			.click(adminUI.cssSelector.initialModalView.fieldType.bool.bool.testA.button)
+			.pause(browser.globals.defaultPauseTimeout)
+			.click(adminUI.cssSelector.initialModalView.buttonCreate)
+			.waitForElementVisible(adminUI.cssSelector.itemView.id)
+			.pause(browser.globals.defaultPauseTimeout);
+
+		browser.expect.element(adminUI.cssSelector.itemView.flashMessage)
+			.text.to.equal('New Boolean One created.');
+		browser.expect.element(adminUI.cssSelector.itemView.fieldType.bool.bool.testA.value)
+			.to.have.value.that.equals('true');
+	},
+	'Boolean field can be edited via the edit form': function (browser) {
+		browser
+			.click(adminUI.cssSelector.itemView.fieldType.bool.bool.testA.button)
+			.pause(browser.globals.defaultPauseTimeout)
+			.click(adminUI.cssSelector.itemView.itemSaveButton)
+			.waitForElementVisible(adminUI.cssSelector.itemView.id)
+			.pause(browser.globals.defaultPauseTimeout);
+
+		browser.expect.element(adminUI.cssSelector.itemView.flashMessage)
+			.text.to.equal('Your changes have been saved.');
+		browser.expect.element(adminUI.cssSelector.itemView.fieldType.bool.bool.testA.value)
+			.to.have.value.that.equals('false');
+	},
+};
