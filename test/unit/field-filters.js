fffb63e60f7f09211281be2cbe42b1fa90a63f4f
Jed Watson <jed.watson@me.com>
2016-03-25 23:01:39 +1100

    Adding field-filters test runner

---
+++ test/unit/field-filters.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var keystone = require('../../index.js');
+var path = require('path');
+
+keystone.init();
+
+var typesLoc = path.resolve('fields/types');
+var types = fs.readdirSync(typesLoc);
+
+types.forEach(function (name) {
+	var filtersTestPath = typesLoc + '/' + name + '/test/filters.js';
+	if (!fs.existsSync(filtersTestPath)) return;
+
+	var listKey = name + 'FiltersTest';
+
+	// nocreate option prevents warnings for required / not initial fields
+	var List = keystone.List(listKey, { nocreate: true });
+	var test = require(filtersTestPath);
+
+	test.initList(List);
+	List.register();
+
+	var testItems = {};
+	testItems[listKey] = test.getTestItems();
+
+	describe('FieldType ' + name, function () {
+		before(function (done) {
+			List.model.remove().exec(function (err) {
+				if (err) throw err;
+				keystone.createItems(testItems, done);
+			});
+		});
+		test.testFilters(List);
+	});
+});
