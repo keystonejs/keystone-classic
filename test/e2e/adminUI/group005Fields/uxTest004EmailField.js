486823d036f6ebd9824ddbeaa8940999641a37a2
webteckie <avp4carlos@gmail.com>
2016-03-29 23:00:40 -0400

    test(e2e): add email ux test coverage.

--- test/e2e/adminUI/group005Fields/uxTest004EmailField.js
+++ test/e2e/adminUI/group005Fields/uxTest004EmailField.js
@@ -20,10 +20,48 @@ module.exports = {
 			.pause(browser.globals.defaultPauseTimeout)
 			.end();
 	},
-	'TODO': function (browser) {
+	'Email field can be created via the initial modal': function (browser) {
 		browser
 			.click(adminUI.cssSelector.homeView.plusIconLinkForEmailsTabUnderDashboardFieldsSubheading)
 			.waitForElementVisible(adminUI.cssSelector.initialModalView.id)
 			.pause(browser.globals.defaultPauseTimeout)
+			.setValue(adminUI.cssSelector.initialModalView.fieldType.email.email.name.value, 'Email Field Test')
+			.setValue(adminUI.cssSelector.initialModalView.fieldType.email.email.fieldA.value, 'email1@test.e2e')
+			.pause(browser.globals.defaultPauseTimeout)
+			.click(adminUI.cssSelector.initialModalView.buttonCreate)
+			.waitForElementVisible(adminUI.cssSelector.itemView.id)
+			.pause(browser.globals.defaultPauseTimeout);
+
+		browser.expect.element(adminUI.cssSelector.itemView.flashMessage)
+			.text.to.equal('New Email Email Field Test created.');
+
+		browser.getValue(adminUI.cssSelector.itemView.fieldType.email.email.name.value, function(result) {
+			this.assert.equal(typeof result, "object");
+			this.assert.equal(result.status, 0);
+			this.assert.equal(result.value, "Email Field Test");
+		});
+
+		browser.expect.element(adminUI.cssSelector.itemView.fieldType.email.email.fieldA.value)
+			.to.have.value.that.equals('email1@test.e2e');
+	},
+	'Email field can be created via the edit form': function (browser) {
+		browser
+			.setValue(adminUI.cssSelector.itemView.fieldType.email.email.fieldB.value, 'email2@test.e2e')
+			.pause(browser.globals.defaultPauseTimeout)
+			.click(adminUI.cssSelector.itemView.itemSaveButton)
+			.waitForElementVisible(adminUI.cssSelector.itemView.id)
+			.pause(browser.globals.defaultPauseTimeout);
+
+		browser.expect.element(adminUI.cssSelector.itemView.flashMessage)
+			.text.to.equal('Your changes have been saved.');
+
+		browser.getValue(adminUI.cssSelector.itemView.fieldType.email.email.name.value, function(result) {
+			this.assert.equal(typeof result, "object");
+			this.assert.equal(result.status, 0);
+			this.assert.equal(result.value, "Email Field Test");
+		});
+
+		browser.expect.element(adminUI.cssSelector.itemView.fieldType.email.email.fieldB.value)
+			.to.have.value.that.equals('email2@test.e2e');
 	},
 };
