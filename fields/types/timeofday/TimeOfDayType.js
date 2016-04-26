96444347f7e202f25f691bf32bca89344f2c1c18
Caleb Thompson <caleb.thompson@welltok.com>
2016-04-26 13:45:00 -0700

    Put TimeOfDay as UTC.

--- fields/types/timeofday/TimeOfDayType.js
+++ fields/types/timeofday/TimeOfDayType.js
@@ -1,81 +1,78 @@
-var moment = require('moment');
-var DateType = require('../date/DateType');
-var FieldType = require('../Type');
-var util = require('util');
-var utils = require('keystone-utils');
-var TextType = require('../text/TextType');
+var moment = require('moment')
+var DateType = require('../date/DateType')
+var FieldType = require('../Type')
+var util = require('util')
+var utils = require('keystone-utils')
+var TextType = require('../text/TextType')
 
-var parseFormats = ['h:m a', 'H:m'];
+var parseFormats = ['h:m a', 'H:m']
 
 function timeofday (list, path, options) {
-	this._nativeType = Date;
-	this._underscoreMethods = ['format', 'moment', 'parse'];
-	this._fixedSize = 'full';
-	this._properties = ['formatString', 'isUTC'];
-	this.typeDescription = 'time of day';
-	this.parseFormatString = options.parseFormat || parseFormats;
-	this.formatString = (options.format === false) ? false : (options.format || 'h:m a');
-	this.isUTC = options.utc || false;
+	this._nativeType = Date
+	this._underscoreMethods = ['format', 'moment', 'parse']
+	this._fixedSize = 'full'
+	this._properties = ['formatString', 'isUTC']
+	this.typeDescription = 'time of day'
+	this.parseFormatString = options.parseFormat || parseFormats
+	this.formatString = (options.format === false) ? false : (options.format || 'h:m a')
+	this.isUTC = true
 	if (this.formatString && typeof this.formatString !== 'string') {
-		throw new Error('FieldType.timeofday: options.format must be a string.');
+		throw new Error('FieldType.timeofday: options.format must be a string.')
 	}
-	timeofday.super_.call(this, list, path, options);
+	timeofday.super_.call(this, list, path, options)
 	this.paths = {
 		time: this._path.append('_time'),
-	};
+	}
 }
-util.inherits(timeofday, FieldType);
+util.inherits(timeofday, FieldType)
 
-timeofday.prototype.validateRequiredInput = TextType.prototype.validateRequiredInput;
+timeofday.prototype.validateRequiredInput = TextType.prototype.validateRequiredInput
 
-timeofday.prototype.addFilterToQuery = DateType.prototype.addFilterToQuery;
-timeofday.prototype.format = DateType.prototype.format;
-timeofday.prototype.moment = DateType.prototype.moment;
+timeofday.prototype.addFilterToQuery = DateType.prototype.addFilterToQuery
+timeofday.prototype.format = DateType.prototype.format
+timeofday.prototype.moment = DateType.prototype.moment
 
 
 timeofday.prototype.getInputFromData = function (data) {
-	return this.getValueFromData(data, '_time');
-};
+	return this.getValueFromData(data, '_time')
+}
 
 timeofday.prototype.parse = function (value) {
-	
-	console.log('parse')
-	//add date part to time
 	var newDate = new Date('Jan 1 1970 ' + value)
-	console.log(newDate)
-	return newDate
+	var newUtcDate = Date.UTC(newDate.getFullYear(), newDate.getMonth(), newDate.getDate(), newDate.getHours(), newDate.getMinutes())
+	return new Date(newUtcDate)
 }
 
 timeofday.prototype.validateInput = function (data, callback) {
 
 	console.log('validateInput')
-	var value = this.getInputFromData(data);
+	var value = this.getInputFromData(data)
 	// If the value is null, undefined or an empty string
 	// bail early since updateItem sanitizes that just fine
-	var result = true;
+	var result = true
 	if (value) {
-		result = this.parse(value, this.parseFormatString, true).isValid();
+		result = this.parse(value, this.parseFormatString, true).isValid()
 	}
-	utils.defer(callback, result);
+	utils.defer(callback, result)
 }
 
 timeofday.prototype.inputIsValid = function (data, required, item) {
 
-	if (!(this.path in data && !(this.paths.time in data)) && item && item.get(this.path)) return true;
-	var newValue = moment(this.getInputFromData(data), parseFormats);
+	if (!(this.path in data && !(this.paths.time in data)) && item && item.get(this.path)) return true
+	var newValue = moment(this.getInputFromData(data), parseFormats)
 	if (required && (!newValue || !newValue.isValid())) {
-		return false;
+		return false
 	} else if (this.getInputFromData(data) && newValue && !newValue.isValid()) {
-		return false;
+		return false
 	} else {
-		return true;
+		return true
 	}
 }
 
 
 timeofday.prototype.updateItem = function (item, data, callback) {
 
-	var value = this.getInputFromData(data);
+	var value = this.getInputFromData(data)
 	if (value !== undefined) {
 		if (value !== null && value !== '') {
 			// If the value is not null, empty string or undefined, parse it
@@ -84,10 +81,10 @@ timeofday.prototype.updateItem = function (item, data, callback) {
 		}
 		// If it's null or empty string, clear it out
 	} else {
-		item.set(this.path, null);
+		item.set(this.path, null)
 	}
-	process.nextTick(callback);
+	process.nextTick(callback)
 }
 
 /* Export Field Type */
-module.exports = timeofday;
+module.exports = timeofday
