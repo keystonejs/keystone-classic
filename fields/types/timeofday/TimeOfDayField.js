e8fc6796f8ce6d8d16666a8ce5ed4de2a1e64524
Caleb Thompson <caleb.thompson@welltok.com>
2016-04-26 13:45:00 -0700

    Put TimeOfDay as UTC.

--- fields/types/timeofday/TimeOfDayField.js
+++ fields/types/timeofday/TimeOfDayField.js
@@ -1,8 +1,8 @@
-import DateInput from '../../components/DateInput';
-import Field from '../Field';
-import moment from 'moment';
-import React from 'react';
-import { Button, FormField, FormInput, FormNote, InputGroup } from 'elemental';
+import DateInput from '../../components/DateInput'
+import Field from '../Field'
+import moment from 'moment'
+import React from 'react'
+import { Button, FormField, FormInput, FormNote, InputGroup } from 'elemental'
 
 module.exports = Field.create({
 
@@ -19,62 +19,62 @@ module.exports = Field.create({
 	getInitialState () {
 		return {
 			timeValue: this.props.value && this.moment(this.props.value).format(this.timeInputFormat)
-		};
+		}
 	},
 
 	getDefaultProps () {
 		return {
 			formatString: 'Do h:mm a',
-		};
+		}
 	},
 
 	moment (value) {
-		var m = moment(value);
-		if (this.props.isUTC) m.utc();
-		return m;
+		var m = moment(value)
+		if (this.props.isUTC) m.utc()
+		return m
 	},
 
 	// TODO: Move isValid() so we can share with server-side code
 	isValid (value) {
-		return moment(value, this.parseFormats).isValid();
+		return moment(value, this.parseFormats).isValid()
 	},
 
 	// TODO: Move format() so we can share with server-side code
 	format (value, format) {
-		format = format || this.timeInputFormat;
-		return value ? this.moment(value).format(format) : '';
+		format = format || this.timeInputFormat
+		return value ? this.moment(value).format(format) : ''
 	},
 
 	handleChange () {
-		var value = this.state.timeValue;
-		var timeFormat = this.timeInputFormat;
+		var value = this.state.timeValue
+		var timeFormat = this.timeInputFormat
 		this.props.onChange({
 			path: this.props.path,
 			value: this.isValid(value) ? moment(value, timeFormat).toISOString() : null,
-		});
+		})
 	},
 
 	timeChanged (evt) {
-		this.setState({ timeValue: evt.target.value });
-		this.handleChange();
+		this.setState({ timeValue: evt.target.value })
+		this.handleChange()
 	},
 
 	setNow () {
-		var timeValue = moment().format(this.timeInputFormat);
+		var timeValue = moment().format(this.timeInputFormat)
 		this.setState({
 			timeValue: timeValue,
-		});
-		this.handleChange(timeValue);
+		})
+		this.handleChange(timeValue)
 	},
 
 	renderNote () {
-		if (!this.props.note) return null;
-		return <FormNote note={this.props.note} />;
+		if (!this.props.note) return null
+		return <FormNote note={this.props.note} />
 	},
 
 	renderUI () {
-		var input;
-		var fieldClassName = 'field-ui';
+		var input
+		var fieldClassName = 'field-ui'
 		if (this.shouldRenderField()) {
 			input = (
 				<InputGroup>
@@ -85,15 +85,15 @@ module.exports = Field.create({
 						<Button onClick={this.setNow}>Now</Button>
 					</InputGroup.Section>
 				</InputGroup>
-			);
+			)
 		} else {
-			input = <FormInput noedit>{this.format(this.props.value, this.props.formatString)}</FormInput>;
+			input = <FormInput noedit>{this.format(this.props.value, this.props.formatString)}</FormInput>
 		}
 		return (
 			<FormField label={this.props.label} className="field-type-datetime">
 				{input}
 				{this.renderNote()}
 			</FormField>
-		);
+		)
 	},
-});
+})
