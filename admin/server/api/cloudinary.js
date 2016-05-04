eb2a038d57d3a7e20616ea76a00b4a0e4a216d63
Caleb Thompson <caleb.thompson@welltok.com>
2016-05-04 15:38:10 -0700

    lint, yo

--- admin/server/api/cloudinary.js
+++ admin/server/api/cloudinary.js
@@ -35,49 +35,47 @@ module.exports = {
 		var max = req.query.max || 50;
 		var prefix = req.query.prefix || '';
 
-		var collection = []
+		var collection = [];
 		iterateCloudinary(collection, type, max, prefix, null,
-			function(error, callbackResult) {
+			function (error, callbackResult) {
 				if (error) {
-					res.json(error)
+					res.json(error);
 				} else {
 					res.json({
-						items: callbackResult.sort(sortById)
+						items: callbackResult.sort(sortById),
 					});
 				}
-		})
+			});
 	},
 	autocompletemedia: function (req, res) {
-		var cloudinary = require('cloudinary');
 		var async = require('async');
 
-		var type = req.params.type || 'image';
 		var max = req.query.max || 50;
 		var prefix = req.query.prefix || '';
-		var next = req.query.next || null;
 
-		var collection = []
+		var collection = [];
 
 		async.parallel([
-			function(callback) { iterateCloudinary(collection, 'image', max, prefix, null, callback) },
-			function(callback) { iterateCloudinary(collection, 'video', max, prefix, null, callback) }
+			function (callback) { iterateCloudinary(collection, 'image', max, prefix, null, callback); },
+			function (callback) { iterateCloudinary(collection, 'video', max, prefix, null, callback); },
 		],
-		function(error, results){
+		function (error, results) {
 			if (error) {
-				res.json(error)
+				res.json(error);
 			} else {
-				results[0].sort(sortById)
-				results[1].sort(sortById)
+				results[0].sort(sortById);
+				results[1].sort(sortById);
 
 				res.json({
-					items: results[0].concat(results[1])
+					items: results[0].concat(results[1]),
 				});
 			}
-		})
+		});
 	},
 
 	get: function (req, res) {
 		var cloudinary = require('cloudinary');
+
 		cloudinary.api.resource(req.query.id, function (result) {
 			if (result.error) {
 				res.json({ error: { message: result.error.message } });
@@ -88,21 +86,22 @@ module.exports = {
 	},
 };
 
-function iterateCloudinary(collection, type, max, prefix, next, callback) {
-	var cloudinary = require('cloudinary')
+function iterateCloudinary (collection, type, max, prefix, next, callback) {
+	var cloudinary = require('cloudinary');
+
 	cloudinary.api.resources(function (result) {
 		if (result.error) {
-			callback({ error: { message: result.error.message } }, null)
+			callback({ error: { message: result.error.message } }, null);
 		} else {
 			if (collection.length === 0) {
-				collection = result.resources
+				collection = result.resources;
 			} else {
-				collection = collection.concat(result.resources)
+				collection = collection.concat(result.resources);
 			}
 			if (result.next_cursor) {
-				iterateCloudinary(collection, type, max, prefix, result.next_cursor, callback)
+				iterateCloudinary(collection, type, max, prefix, result.next_cursor, callback);
 			} else {
-				callback(null, collection)
+				callback(null, collection);
 			}
 		};
 	}, {
@@ -114,12 +113,15 @@ function iterateCloudinary(collection, type, max, prefix, next, callback) {
 	});
 }
 
-function sortById(a,b) {
-  if (a.public_id < b.public_id)
-    return -1;
-  else if (a.public_id > b.public_id)
-    return 1;
-  else 
-    return 0;
+function sortById (a, b) {
+	if (a.public_id < b.public_id) {
+		return -1;
+	}
+	else if (a.public_id > b.public_id) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
 
