b48d5c7ba473ef909057a5774be7de2f0b3b0cb4
Justin Donnaruma <jdr0dn3y@users.noreply.github.com>
2016-05-09 11:38:35 -0400

    Update signin route to properly pull 'signin redirect'

--- admin/server/routes/signin.js
+++ admin/server/routes/signin.js
@@ -1,9 +1,12 @@
 var keystone = require('../../../');
-
-module.exports = function (req, res) {
-	keystone.render(req, res, 'signin', {
-		submitted: req.body,
-		from: req.query.from,
-		logo: keystone.get('signin logo'),
-	});
+	var signin = keystone.get('signin redirect') || 'signin';
+	if (signin != 'signin') {
+		res.redirect(signin);
+	} else {
+		keystone.render(req, res, signin, {
+			submitted: req.body,
+			from: req.query.from,
+			logo: keystone.get('signin logo'),
+		});
+	}
 };
