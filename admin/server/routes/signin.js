0617a9fe9f7411446f4b978039e7c687f342792d
Justin Raphael Donnaruma <justin@donnaruma.us>
2016-05-09 14:15:33 -0400

    fix accidental removal of module.exports

--- admin/server/routes/signin.js
+++ admin/server/routes/signin.js
@@ -1,12 +1,14 @@
 var keystone = require('../../../');
 var signin = keystone.get('signin redirect') || 'signin';
-if (signin !== 'signin') {
-	res.redirect(signin);
-} else {
-	keystone.render(req, res, signin, {
-		submitted: req.body,
-		from: req.query.from,
-		logo: keystone.get('signin logo'),
-	});
 
+module.exports = function (req, res) {
+	if (signin !== 'signin') {
+		res.redirect(signin);
+	} else {
+		keystone.render(req, res, signin, {
+			submitted: req.body,
+			from: req.query.from,
+			logo: keystone.get('signin logo'),
+		});
+	};
 };
