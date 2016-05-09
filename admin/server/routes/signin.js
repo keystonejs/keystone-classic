7b68f91c32d8b47574b66807106fda5163d7d407
Justin Donnaruma <jdr0dn3y@users.noreply.github.com>
2016-05-09 13:56:10 -0400

    Fix strict quality check and extra }

--- admin/server/routes/signin.js
+++ admin/server/routes/signin.js
@@ -1,12 +1,12 @@
 var keystone = require('../../../');
-	var signin = keystone.get('signin redirect') || 'signin';
-	if (signin != 'signin') {
-		res.redirect(signin);
-	} else {
-		keystone.render(req, res, signin, {
-			submitted: req.body,
-			from: req.query.from,
-			logo: keystone.get('signin logo'),
-		});
-	}
+var signin = keystone.get('signin redirect') || 'signin';
+if (signin !== 'signin') {
+	res.redirect(signin);
+} else {
+	keystone.render(req, res, signin, {
+		submitted: req.body,
+		from: req.query.from,
+		logo: keystone.get('signin logo'),
+	});
+
 };
