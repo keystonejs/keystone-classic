ae40112287b444e3be92ca51281a6c9f498b410d
Joss Mackison <joss@thinkmill.com.au>
2016-05-18 13:14:33 +1000

    Toolbar: component cleanup
    - remove cruft
    - prefer stateless functions
    - prefer const
    - only export a single react class per file

--- admin/client/App/screens/Item/components/Toolbar/ToolbarSection.js
+++ admin/client/App/screens/Item/components/Toolbar/ToolbarSection.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import blacklist from 'blacklist';
+import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
-module.exports = React.createClass({
-	displayName: 'ToolbarSection',
-	propTypes: {
-		className: React.PropTypes.string,
-		left: React.PropTypes.bool,
-		right: React.PropTypes.bool,
-	},
-	render () {
-		var className = classNames('Toolbar__section', {
-			'Toolbar__section--left': this.props.left,
-			'Toolbar__section--right': this.props.right,
-		}, this.props.className);
+const ToolbarSection = (props) => {
+	const className = classNames('Toolbar__section', {
+		'Toolbar__section--left': props.left,
+		'Toolbar__section--right': props.right,
+	}, props.className);
 
-		var props = blacklist(this.props, 'left', 'right', 'className');
+	return <div {...props} className={className} />;
+};
 
-		return <div {...props} className={className}>{this.props.children}</div>;
-	},
-});
+ToolbarSection.displayName = 'ToolbarSection';
+ToolbarSection.propTypes = {
+	children: PropTypes.node.isRequired,
+	className: PropTypes.string,
+	left: PropTypes.bool,
+	right: PropTypes.bool,
+};
+
+module.exports = ToolbarSection;
