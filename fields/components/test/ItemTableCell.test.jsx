b890751165e3d2240c89ea56dc00840e73159135
surya panikkal <surya.com@gmail.com>
2016-05-18 11:21:47 -0400

    react test: change test-utils to enzyme
    changed the mocha `--reporter` to `spec` since there are only few specs at this point

--- fields/components/test/ItemTableCell.test.jsx
+++ fields/components/test/ItemTableCell.test.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-import ItemsTableCell from '../ItemsTableCell';
+import { shallow } from 'enzyme';
 import demand from 'must';
+import ItemsTableCell from '../ItemsTableCell';
 
-describe('ItemsTableCell tests', () => {
-  it('should render the ItemsTableCell', () => {
-    const renderer = TestUtils.createRenderer();
-    renderer.render(<ItemsTableCell p1='v1' p2='v2' className='mock-class'/>);
-    const result = renderer.getRenderOutput();
-
-    demand(result.type).eql('td');
-    demand(result.props.p1).eql('v1');
-    demand(result.props.p2).eql('v2');
-    demand(result.props.className).eql('ItemList__col mock-class');
+describe('<ItemsTableCell/> tests', () => {
+  it('should render <td> with default class and properties', () => {
+    const cell = shallow(<ItemsTableCell/>);
+    demand(cell.find('td').type()).eql('td');
+    demand(cell.find('td').length).eql(1);
+    demand(cell.find('.ItemList__col').length).eql(1);
+    demand(cell.prop('className')).eql('ItemList__col ');
+  });
+  
+  it('should render <td> with properties p1, p2', () => {
+    const cell = shallow(<ItemsTableCell p1='v1' p2='v2'/>);
+    demand(cell.prop('p1')).eql('v1');
+    demand(cell.prop('p2')).eql('v2');
+  });
+  
+  it('should render <td> with css class mock-style', () => {
+    const cell = shallow(<ItemsTableCell className='mock-style'/>);
+    demand(cell.find('.mock-style').length).eql(1);
+    demand(cell.find('.ItemList__col').length).eql(1);
   });
 });
