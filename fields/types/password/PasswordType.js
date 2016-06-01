0f8dbcf156bb7a5e84a6272cb7926e34f9aa7a05
Jed Watson <jed.watson@me.com>
2016-06-01 23:44:56 +1000

    Preventing double password hashing w/ inherited lists, ref #2972

--- fields/types/password/PasswordType.js
+++ fields/types/password/PasswordType.js
@@ -55,6 +55,9 @@ password.prototype.addToSchema = function () {
 		if (!this.isModified(field.path) || !this[needs_hashing]) {
 			return next();
 		}
+		// reset the [needs_hashing] flag so that new values can't be hashed more than once
+		// (inherited models double up on pre save handlers for password fields)
+		this[needs_hashing] = false;
 		if (!this.get(field.path)) {
 			this.set(field.path, undefined);
 			return next();
