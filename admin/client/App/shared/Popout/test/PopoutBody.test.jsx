dfd2fec3ceb1a4a579f797c4120c0ca1529a66df
Max <contact@mstoiber.com>
2016-07-01 17:02:19 +0200

    Test PopoutBody component

---
+++ admin/client/App/shared/Popout/test/PopoutBody.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import demand from 'must';
+import PopoutBody from '../PopoutBody';
+
+describe('<PopoutBody />', () => {
+	it('should render a div', () => {
+		const component = shallow(<PopoutBody />);
+		demand(component.find('div').length).eql(1);
+	});
+
+	it('should have a class of Popout__body', () => {
+		const component = shallow(<PopoutBody />);
+		demand(component.find('div').at(0).prop('className')).eql('Popout__body');
+	});
+
+	it('should add a class name when scrollable', () => {
+		const component = shallow(<PopoutBody scrollable />);
+		demand(component.find('div').at(0).prop('className'))
+			.eql('Popout__body Popout__scrollable-area');
+	});
+
+	it('should render its children', () => {
+		const children = (<h1>Hello World!</h1>);
+		const component = shallow(
+			<PopoutBody>{children}</PopoutBody>
+		);
+
+		demand(component.contains(children)).true();
+	});
+});
