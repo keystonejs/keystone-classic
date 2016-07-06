8d307f1e63cc079d8ff216c98a2d5717af82be8b
Jed Watson <jed.watson@me.com>
2016-07-07 00:08:09 +1000

    Implementing FieldType value watching

--- fields/explorer/index.js
+++ fields/explorer/index.js
@@ -9,10 +9,29 @@ const Types = {
 			label: 'Text Field',
 			path: 'textField',
 		},
+		value: '',
 	},
 };
 
-const Field = React.createClass({
+const FieldType = React.createClass({
+	getInitialState () {
+		return {
+			fieldValue: Types[this.props.type].value,
+			filterValue: {},
+		};
+	},
+	onFieldChange (e) {
+		console.log(`${this.props.type} field value changed:`, e.value);
+		this.setState({
+			value: e.value,
+		});
+	},
+	onFilterChange (value) {
+		console.log(`${this.props.type} filter value changed:`, value);
+		this.setState({
+			filter: value,
+		});
+	},
 	render () {
 		const FieldComponent = Types[this.props.type].Field;
 		const FilterComponent = Types[this.props.type].Filter;
@@ -21,9 +40,9 @@ const Field = React.createClass({
 			<div className="ExplorerField">
 				<h2>{this.props.type}</h2>
 				<h3>Field</h3>
-				<FieldComponent path={spec.path} onChange={(e) => console.log(e)} />
+				<FieldComponent path={spec.path} onChange={this.onFieldChange} value={this.state.value} />
 				<h3>Filter</h3>
-				<FilterComponent field={spec} onChange={(e) => console.log(e)} />
+				<FilterComponent field={spec} onChange={this.onFilterChange} filter={this.state.filter} />
 			</div>
 		);
 	},
@@ -32,7 +51,7 @@ const Field = React.createClass({
 ReactDOM.render(
 	<div>
 		<h1>Fields Explorer</h1>
-		<Field type="Text" />
+		<FieldType type="Text" />
 	</div>,
 	document.getElementById('explorer')
 );
