d896c1b8742e7ba5a5a4c2a68a29f686db538a66
Joss Mackison <joss@thinkmill.com.au>
2016-07-08 13:08:03 +1000

    add nav sections + readme wip

--- fields/explorer/field.js
+++ fields/explorer/field.js
@@ -1,8 +1,6 @@
 import Domify from 'react-domify';
 import React from 'react';
-import { Link, Router, Route, browserHistory, IndexRoute } from 'react-router';
-
-import { Col, Row } from 'elemental';
+import Markdown from 'react-markdown';
 
 const Types = {
 	Boolean: require('../types/boolean/test/explorer'),
@@ -19,12 +17,50 @@ const Types = {
 	Url: require('../types/url/test/explorer'),
 };
 
+const Row = (props) => {
+	const styles = {
+		display: 'flex',
+		flexWrap: 'wrap',
+		marginLeft: -10,
+		marginRight: -10,
+		...props.style,
+	};
+
+	return (
+		<div
+			{...props}
+			className={props.className || 'Row'}
+			style={styles}
+			/>
+	);
+};
+
+const Col = (props) => {
+	const styles = {
+		flex: props.width ? null : '1 1 0',
+		minHeight: 1,
+		paddingLeft: 10,
+		paddingRight: 10,
+		width: props.width || '100%',
+		...props.style,
+	};
+
+	return (
+		<div
+			{...props}
+			className={props.className || 'Col'}
+			style={styles}
+			/>
+	);
+};
+
 const ExplorerFieldType = React.createClass({
 	getInitialState () {
 		const FilterComponent = Types[this.props.params.type].Filter;
 		return {
-			value: Types[this.props.params.type].value,
+			readmeIsVisible: true,
 			filter: FilterComponent.getDefaultValue(),
+			value: Types[this.props.params.type].value,
 		};
 	},
 	componentWillReceiveProps (newProps) {
@@ -50,34 +86,105 @@ const ExplorerFieldType = React.createClass({
 	},
 	render () {
 		const { type } = this.props.params;
+		const { readmeIsVisible } = this.state;
+		// const Readme = Types[type].Readme;
 		const FieldComponent = Types[type].Field;
 		const FilterComponent = Types[type].Filter;
 		const spec = Types[type].spec;
 		return (
 			<div className="fx-page">
-				<div className="fx-page__header">{type}</div>
+				<div className="fx-page__header">
+					<div className="fx-page__header__title">{type}</div>
+					<button
+						className="fx-page__header__button mega-octicon octicon-file-text"
+						onClick={() => this.setState({ readmeIsVisible: !readmeIsVisible })}
+						title={readmeIsVisible ? 'Hide Readme' : 'Show Readme'}
+						type="button"
+					/>
+				</div>
 				<div className="fx-page__content">
 					<Row>
-						<Col sm={300}>
-							<FieldComponent {...spec} onChange={this.onFieldChange} value={this.state.value} />
+						<Col>
+							<div className="fx-page__content__inner">
+								<Row>
+									<Col width={300}>
+										<FieldComponent
+											{...spec}
+											onChange={this.onFieldChange}
+											value={this.state.value}
+										/>
+									</Col>
+									<Col>
+										<Domify
+											className="Domify"
+											value={{ value: this.state.value }}
+										/>
+									</Col>
+								</Row>
+								<h3>Filter</h3>
+								<Row>
+									<Col width={300}>
+										<FilterComponent
+											field={spec}
+											filter={this.state.filter}
+											onChange={this.onFilterChange}
+										/>
+									</Col>
+									<Col>
+										<Domify
+											className="Domify"
+											value={this.state.filter}
+										/>
+									</Col>
+								</Row>
+							</div>
 						</Col>
-						<Col style={{ paddingLeft: 50, width: 'auto' }}>
-							<Domify className="Domify" value={{ value: this.state.value }} />
-						</Col>
-					</Row>
-					<h3>Filter</h3>
-					<Row>
-						<Col sm={300}>
-							<FilterComponent field={spec} onChange={this.onFilterChange} filter={this.state.filter} />
-						</Col>
-						<Col style={{ paddingLeft: 50, width: 'auto' }}>
-							<Domify className="Domify" value={this.state.filter} />
-						</Col>
+						{!!readmeIsVisible && (
+							<Col width={380}>
+								<Markdown
+									className="Markdown"
+									source={STUB_README}
+								/>
+							</Col>
+						)}
 					</Row>
 				</div>
 			</div>
 		);
 	},
 });
+
+const STUB_README = `
+# Boolean Field
+
+Stores a \`Boolean\` in the model.
+
+## Methods
+
+### \`format\`
+
+Returns the string \`"true"\` or \`"false"\`.
+
+### \`updateItem\`
+
+Because \`FormData\` can only submit \`String\` values, and HTML Forms won't submit fields at all when checkbox inputs are not checked, there are a few unusual behaviours when updating Boolean fields:
+
+* The stored value is always updated, even when \`undefined\` is provided
+* Any falsy value, including \`undefined\`, \`null\`, \`false\`, \`""\` and \`0\` will store \`false\`
+* The string \`"false"\` will store \`false\`
+* Any other truthy string will store \`true\`
+* Any truthy number will store \`true\`
+* The boolean \`true\` will store \`true\`
+
+There is no way to use \`updateItem\` to remove Boolean field values from the item.
+
+### \`validateInput\`
+
+Ensures the value can be interpreted using the rules above. Other complex values (objects that aren't null, dates, arrays, etc) are not valid input.
+
+### \`validateRequiredInput\`
+
+Ensures the value matches one of the rules above that will store \`true\`
+`;
 
 module.exports = ExplorerFieldType;
--- fields/explorer/index.js
+++ fields/explorer/index.js
@@ -2,37 +2,60 @@ import React from 'react';
 import { Link, Router, Route, browserHistory, IndexRoute } from 'react-router';
 import ReactDOM from 'react-dom';
 import FieldType from './field';
-const TypeKeys = [
-	'Boolean',
-	'Color',
-	'Date',
-	'Datetime',
-	'Email',
-	'Key',
-	'Name',
-	'Password',
-	'Text',
-	'Textarea',
-	'Textarray',
-	'Url',
-];
+
+const TypeKeys = {
+	Text: [
+		'Color',
+		'Email',
+		'Key',
+		'Name',
+		'Text',
+		'Textarea',
+		'Textarray',
+		'Url',
+	],
+	Date: [
+		'Date',
+		'Datetime',
+	],
+	Number: [],
+	Miscellaneous: [
+		'Boolean',
+		'Password',
+	],
+};
 
 const App = (props) => {
 	return (
 		<div className="fx-wrapper">
 			<div className="fx-sidebar">
 				<div className="fx-sidebar__header">{props.params.type
-					? 'Exploring'
+					? <Link to="/" className="fx-sidebar__header__link">Exploring</Link>
 					: 'Ready'}
 				</div>
-				{TypeKeys.map(type => {
-					const klass = `fx-sidebar__item${props.params.type === type
-						? ' fx-sidebar__item--active'
-						: ''}`;
+				{Object.keys(TypeKeys).map(section => {
+					const types = TypeKeys[section].map(type => {
+
+						const itemClassName = props.params.type === type
+							? 'fx-sidebar__item fx-sidebar__item--active'
+							: 'fx-sidebar__item';
+
+						return (
+							<Link key={type} to={`/${section}/${type}`} className={itemClassName}>
+								{upcase(type)}
+							</Link>
+						);
+					});
+
+					const sectionClassName = props.params.section === section
+						? 'fx-sidebar__section fx-sidebar__section--active'
+						: 'fx-sidebar__section';
+
 					return (
-						<Link key={type} to={type} className={klass}>
-							{upcase(type)}
-						</Link>
+						<div key={section} className={sectionClassName}>
+							<div key={section} className="fx-sidebar__section__title">{section}</div>
+							{types}
+						</div>
 					);
 				})}
 			</div>
@@ -56,7 +79,7 @@ ReactDOM.render(
 	<Router history={browserHistory}>
 		<Route path="/" component={App}>
 			<IndexRoute component={Home} />
-			<Route path=":type" component={FieldType} />
+			<Route path=":section/:type" component={FieldType} />
 		</Route>
 	</Router>,
 	document.getElementById('explorer')
