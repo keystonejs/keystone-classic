c35d229442a98a789194460eea20bcccc94ff864
Jed Watson <jed.watson@me.com>
2016-07-13 20:57:32 +1000

    Working through keystone singleton references in List, see #1777

--- index.js
+++ index.js
@@ -133,6 +133,14 @@ Keystone.prototype.wrapHTMLError = require('./lib/core/wrapHTMLError');
  */
 var keystone = module.exports = new Keystone();
 
+/*
+	Note: until #1777 is complete, the order of execution here with the requires
+	(specifically, they happen _after_ the module.exports above) is really
+	important. As soon as the circular dependencies are sorted out to get their
+	keystone instance from a closure or reference on {this} we can move these
+	bindings into the Keystone constructor.
+*/
+
 // Expose modules and Classes
 keystone.Admin = {
 	Server: require('./admin/server'),
@@ -141,7 +149,7 @@ keystone.Email = require('./lib/email');
 keystone.Field = require('./fields/types/Type');
 keystone.Field.Types = require('./lib/fieldTypes');
 keystone.Keystone = Keystone;
-keystone.List = require('./lib/list');
+keystone.List = require('./lib/list')(keystone);
 keystone.View = require('./lib/view');
 
 keystone.content = require('./lib/content');
