571906c1e49a667bb09c3cb7f381c467ba995c09
Joss Mackison <joss@thinkmill.com.au>
2016-07-25 13:02:02 +1000

    field explorer Col + Row: cleanup incidental properties

--- fields/explorer/components/Col.js
+++ fields/explorer/components/Col.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 const Col = (props) => {
+	const { className, gutter, style, width, ...incidentalProps } = props;
 	const styles = {
-		flex: props.width ? null : '1 1 0',
+		flex: width ? null : '1 1 0',
 		minHeight: 1,
-		paddingLeft: 10,
-		paddingRight: 10,
-		width: props.width || '100%',
-		...props.style,
+		paddingLeft: gutter,
+		paddingRight: gutter,
+		width: width || '100%',
+		...style,
 	};
+	const klass = 'Col' + (className
+		? ' ' + className
+		: '');
 
 	return (
 		<div
-			{...props}
-			className={props.className || 'Col'}
+			{...incidentalProps}
+			className={klass}
 			style={styles}
 		/>
 	);
+};
+Col.propTypes = {
+	className: PropTypes.string,
+	gutter: PropTypes.number,
+	style: PropTypes.string,
+	width: PropTypes.number,
+};
+Col.defaultProps = {
+	gutter: 10,
 };
 
 module.exports = Col;
--- fields/explorer/components/Row.js
+++ fields/explorer/components/Row.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 const Row = (props) => {
+	const { className, gutter, style, ...incidentalProps } = props;
 	const styles = {
 		display: 'flex',
 		flexWrap: 'wrap',
-		marginLeft: -10,
-		marginRight: -10,
-		...props.style,
+		marginLeft: gutter * -1,
+		marginRight: gutter * -1,
+		...style,
 	};
+	const klass = 'Row' + (className
+		? ' ' + className
+		: '');
 
 	return (
 		<div
-			{...props}
-			className={props.className || 'Row'}
+			{...incidentalProps}
+			className={klass}
 			style={styles}
 		/>
 	);
+};
+Row.propTypes = {
+	className: PropTypes.string,
+	gutter: PropTypes.number,
+	style: PropTypes.string,
+};
+Row.defaultProps = {
+	gutter: 10,
 };
 
 module.exports = Row;
