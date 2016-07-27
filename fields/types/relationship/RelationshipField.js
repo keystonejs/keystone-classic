658f93a0ce598a00b9b26e878c678e4fda2fa92f
Pavel Zagoskin <d4rklit3@gmail.com>
2016-07-27 16:28:51 -0700

    implement fix to determine whether input param is string or object and act accordingly to set q  value

--- fields/types/relationship/RelationshipField.js
+++ fields/types/relationship/RelationshipField.js
@@ -117,7 +117,12 @@ module.exports = Field.create({
 	},
 
 	buildOptionQuery: function (input) {
-		var value = input && input[0] && input[0].value || '';
+		var value = '';
+		if(typeof input === 'string' && input.length) {
+			value = input;
+		}else if(typeof input === 'object' && input[0] && input[0].value) {
+			value = input[0].value;
+		}
 		var filters = this.buildFilters();
 		return 'context=relationship&q=' + value +
 				'&list=' + Keystone.list.path +
