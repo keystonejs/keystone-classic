8bbdeef686cbc4c1931136ecb9d9a06fa12017c1
webteckie <avp4carlos@gmail.com>
2016-07-31 21:26:29 -0400

    test(e2e): restore e2e to passing state.
    Figure out all issues with getting e2e to run green.
    
    Figure out all issues with getting e2e to run green.
    
    test(e2e): try to fix issue running in travis ci.

--- test/e2e/adminUI/tests/group999FixMe/testDependsOnField_issue3068.js
+++ test/e2e/adminUI/tests/group999FixMe/testDependsOnField_issue3068.js
@@ -24,10 +24,12 @@ module.exports = {
 
 
 		// The dependency condition is met by default, so the dependent field should show.
-		browser.initialFormPage.assertUI({
+		browser.initialFormPage.assertUIVisible({
 			listName: 'DependsOn',
-			fields: ['dependent'],
-			args: {'editForm': false}
+			fields: ['dependency', 'dependent'],
+			args: {
+				'editForm': false,
+			}
 		});
 
 		browser.initialFormPage.fillInputs({
@@ -37,8 +39,21 @@ module.exports = {
 			}
 		});
 
-		// The dependency condition is no longer met, field should not be visible.
-		browser.initialFormPage.section.form.section.dependsonList.section.dependent.expect.element('@label').to.not.be.present;
+		browser.initialFormPage.assertUIVisible({
+			listName: 'DependsOn',
+			fields: ['dependency'],
+			args: {
+				'editForm': false,
+			}
+		});
+
+		browser.initialFormPage.assertUINotPresent({
+			listName: 'DependsOn',
+			fields: ['dependent'],
+			args: {
+				'editForm': false,
+			}
+		});
 
 		browser.initialFormPage.fillInputs({
 			listName: 'DependsOn',
@@ -47,22 +62,23 @@ module.exports = {
 			}
 		});
 
-		// The dependency condition now met again, so the dependent field should show.
-		browser.initialFormPage.assertUI({
+		browser.initialFormPage.assertUIVisible({
 			listName: 'DependsOn',
-			fields: ['dependent'],
-			args: {'editForm': false}
+			fields: ['dependency', 'dependent'],
+			args: {
+				'editForm': false,
+			}
 		});
-
 
 		browser.initialFormPage.save();
 	},
+
 	'Depends On field should work in the edit form': function(browser) {
 
 		// The dependency condition is met, so the dependent field should show.
-		browser.itemPage.assertUI({
+		browser.itemPage.assertUIVisible({
 			listName: 'DependsOn',
-			fields: ['dependent'],
+			fields: ['dependency', 'dependent'],
 			args: {'editForm': false}
 		});
 
@@ -74,7 +90,13 @@ module.exports = {
 		});
 
 		// The dependency condition is no longer met, field should not be visible.
-		browser.itemPage.section.form.section.dependsonList.section.dependent.expect.element('@label').to.not.be.present;
+		browser.itemPage.assertUINotPresent({
+			listName: 'DependsOn',
+			fields: ['dependent'],
+			args: {
+				'editForm': false,
+			}
+		});
 
 		browser.itemPage.fillInputs({
 			listName: 'DependsOn',
@@ -83,5 +105,14 @@ module.exports = {
 			}
 		});
 
+		browser.itemPage.assertUIVisible({
+			listName: 'DependsOn',
+			fields: ['dependency', 'dependent'],
+			args: {
+				'editForm': false,
+			}
+		});
+
+		browser.itemPage.save();
 	}
 };
