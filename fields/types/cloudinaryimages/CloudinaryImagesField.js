74f5c7a4e1cdfc01d6d48954278a831c5dd13728
jstockwin <jstockwin@gmail.com>
2016-08-16 10:45:28 +0100

    [e2e] Fix cloudinaryImageMultiple Field test

--- fields/types/cloudinaryimages/CloudinaryImagesField.js
+++ fields/types/cloudinaryimages/CloudinaryImagesField.js
@@ -272,7 +272,7 @@ module.exports = Field.create({
 
 		return (
 			<div style={toolbarStyles}>
-				<Button onClick={this.triggerFileBrowser} style={uploadButtonStyles}>
+				<Button onClick={this.triggerFileBrowser} style={uploadButtonStyles} data-e2e-upload-button="true">
 					Upload Images
 				</Button>
 				{this.hasFiles() && (
