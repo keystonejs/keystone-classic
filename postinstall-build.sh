f0122298b24c124215269d9963bcf4adce9e7ad9
Wout Mertens <Wout.Mertens@gmail.com>
2016-08-22 10:42:39 +0200

    Build: only install build deps
    perhaps this script should read package.json and exclude deps from a blacklist.

--- postinstall-build.sh
+++ postinstall-build.sh
@@ -4,7 +4,7 @@
 set -e
 canary=admin/public/js/common.js
 devFile=node_modules/.bin/webpack
-if [ ! -d admin ]; then
+if [ ! -d admin/public ]; then
 	echo "Run me from the git repo root" >&2
 	exit 1
 fi
@@ -14,10 +14,48 @@ if [ ! -e $canary ] || \
 	# we assume that if webpack is not installed, we don't have the dev deps
 	if [ ! -e $devFile ]; then
 		shouldRemoveDev=true
-		npm install --only=dev
+		# carefully curated collection of build dependencies, update with package.json
+		npm install \
+			aphrodite \
+			babel-core \
+			babel-loader \
+			babel-plugin-transform-object-assign \
+			babel-polyfill \
+			babel-preset-es2015 \
+			babel-preset-react \
+			babel-preset-stage-2 \
+			codemirror \
+			core-assert \
+			css-loader \
+			json-loader \
+			raw-loader \
+			react \
+			react-addons-test-utils \
+			react-addons-css-transition-group \
+			react-alt-text \
+			react-color \
+			react-day-picker \
+			react-dnd \
+			react-dnd-html5-backend \
+			react-dom \
+			react-domify \
+			react-engine \
+			react-images \
+			react-markdown \
+			react-proxy-loader \
+			react-redux \
+			react-router \
+			react-router-redux \
+			redux \
+			redux-saga \
+			redux-thunk \
+			style-loader \
+			superagent \
+			webpack
 	fi
 	npm run build
-	if [ -n "$shouldRemoveDev" ]; then
-		npm prune --production
-	fi
+	# Not removing the dev deps - they might be needed next build time
+	# if [ -n "$shouldRemoveDev" ]; then
+	# 	npm prune --production
+	# fi
 fi
