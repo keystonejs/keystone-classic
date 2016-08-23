0c467864aa4bac65bae58116b150020a074d091f
Max <contact@mstoiber.com>
2016-08-23 13:37:39 +1000

    Fix list not loading on second open
    Closes #3369

--- admin/client/App/sagas/index.js
+++ admin/client/App/sagas/index.js
@@ -24,11 +24,15 @@ function * debouncedSearch () {
 }
 
 function * rootSaga () {
+	// Block loading on all items until the first load comes in
 	yield take(actions.INITIAL_LIST_LOAD);
 	yield put(loadItems());
+	// Search debounced
 	yield fork(takeLatest, actions.SET_ACTIVE_SEARCH, debouncedSearch);
+	// If one of the other active properties changes, update the query params and load the new items
 	yield fork(takeLatest, [actions.SET_ACTIVE_SORT, actions.SET_ACTIVE_COLUMNS, actions.SET_CURRENT_PAGE], updateParams);
-	yield fork(takeLatest, [actions.ADD_FILTER, actions.CLEAR_FILTER, actions.CLEAR_ALL_FILTERS], loadTheItems);
+	// Whenever the filters change or another list is loaded, load the items
+	yield fork(takeLatest, [actions.INITIAL_LIST_LOAD, actions.ADD_FILTER, actions.CLEAR_FILTER, actions.CLEAR_ALL_FILTERS], loadTheItems);
 }
 
 export default rootSaga;
