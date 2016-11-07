27fa2aab5b666f6f14daec862837c8f39dcc470c
jstockwin <jstockwin@gmail.com>
2016-11-07 13:43:14 +0000

    Update expected "no items found" test string

--- test/e2e/adminUI/tests/group004List/uxTestListView.js
+++ test/e2e/adminUI/tests/group004List/uxTestListView.js
@@ -175,7 +175,7 @@ module.exports = {
 
 		browser.adminUIApp.waitForListScreen();
 
-		browser.adminUIListScreen.assertElementTextEquals({ element: '@noItemsFoundText', text: 'No names found…' });
+		browser.adminUIListScreen.assertElementTextEquals({ element: '@noItemsFoundText', text: 'No names found...' });
 	},
 
 	// UNDO ANY STATE CHANGES -- THIS TEST SHOULD RUN LAST
