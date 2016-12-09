6e2898823182a1ae9f3775dd4c32962c4c033133
Jeremy Zahner <me@jeremyzahner.com>
2016-12-09 11:54:46 +0100

    Adds className and visual offset for list field for better UX.

--- fields/types/list/ListField.js
+++ fields/types/list/ListField.js
@@ -118,7 +118,7 @@ module.exports = Field.create({
 	renderUI () {
 		const { label, value } = this.props;
 		return (
-			<div>
+			<div className="FormFieldList">
 				<h3 data-things="whatever">{label}</h3>
 				{this.shouldRenderField() ? (
 					this.renderItems()
