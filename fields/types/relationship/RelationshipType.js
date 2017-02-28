8d64773e7686bfce834b1e4cf1b228b81634d024
noviny <novinyarts@gmail.com>
2017-02-28 17:29:08 +1100

    Add clarity to and comments to relationship.updateItem
    * cleared up single item logic flow
    * Updated the description
    * Added comments to the logic flow

--- fields/types/relationship/RelationshipType.js
+++ fields/types/relationship/RelationshipType.js
@@ -213,7 +213,8 @@ relationship.prototype.inputIsValid = function (data, required, item) {
  * Updates the value for this field in the item from a data object.
  * Only updates the value if it has changed.
  * Treats an empty string as a null value.
- * If data object does not contain the path field, then delete the field.
+ * If data object does not contain the path field, then leave the field untouched.
+ * falsey values such as `null` or an empty string will reset the field
  */
 relationship.prototype.updateItem = function (item, data, callback) {
 	if (item.populated(this.path)) {
@@ -224,6 +225,8 @@ relationship.prototype.updateItem = function (item, data, callback) {
 	if (value === undefined) {
 		return process.nextTick(callback);
 	}
+
+	// Are we handling a many relationship or just one value?
 	if (this.many) {
 		var arr = item.get(this.path);
 		var _old = arr.map(function (i) { return String(i); });
@@ -237,13 +240,15 @@ relationship.prototype.updateItem = function (item, data, callback) {
 			item.set(this.path, _new);
 		}
 	} else {
-		if (value) {
-			if (value !== item.get(this.path)) {
-				item.set(this.path, value);
-			}
-		} else if (item.get(this.path)) {
+		// Ok, it's one value, should I do anything with it?
+		if (value && value !== item.get(this.path)) {
+			// If it's set and has changed, I do.
+			item.set(this.path, value);
+		} else if (!value && item.get(this.path)) {
+			// If it's not set and it was set previously, I need to clear.
 			item.set(this.path, null);
 		}
+		// Otherwise, ignore.
 	}
 	process.nextTick(callback);
 };
