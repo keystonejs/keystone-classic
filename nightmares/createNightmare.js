2c3b2ba3d6500087bf83402b9906f57010ae4fa0
Mike James <m@mikejam.es>
2017-03-16 17:46:38 +1100

    attempt to test out 2382 in nightmare

--- nightmares/createNightmare.js
+++ nightmares/createNightmare.js
@@ -1,24 +1,24 @@
 var Nightmare = require('nightmare');
 
 // Add further actions here
-Nightmare.action('sendKeyDown', function (name, options, parent, win, renderer, done) {
-	parent.respondTo('sendKeyDown', function (unicodeValue, done) {
-		win.focus();
-		win.webContents.sendInputEvent({
-			type: 'keyDown',
-			keyCode: unicodeValue,
-		});
-		win.webContents.sendInputEvent({
-			type: 'keyUp',
-			keyCode: unicodeValue,
-		});
-		done();
-	});
-	done();
-},
-function (unicodeValue, done) {
-	this.child.call('sendKeyDown', unicodeValue, done);
-});
+// Nightmare.action('sendKeyDown', function (name, options, parent, win, renderer, done) {
+// 	parent.respondTo('sendKeyDown', function (unicodeValue, done) {
+// 		win.focus();
+// 		win.webContents.sendInputEvent({
+// 			type: 'keyDown',
+// 			keyCode: unicodeValue,
+// 		});
+// 		win.webContents.sendInputEvent({
+// 			type: 'keyUp',
+// 			keyCode: unicodeValue,
+// 		});
+// 		done();
+// 	});
+// 	done();
+// },
+// function (unicodeValue, done) {
+// 	this.child.call('sendKeyDown', unicodeValue, done);
+// });
 
 module.exports = function createNightmare (opts) {
 	return new Nightmare(Object.assign({}, { show: process.env.SHOW_ELECTRON || false }, opts));
--- nightmares/issues/2382.js
+++ nightmares/issues/2382.js
@@ -6,7 +6,7 @@ var createUser = require('../plugins/createUser');
 
 // Closed, ensure its not reproducible https://github.com/keystonejs/keystone/issues/2382
 describe('Issue 2382', function () {
-	this.timeout(20000);
+	this.timeout(400000);
 
 	it.only('Describe the relationship issue', function () {
 		return createNightmare()
@@ -23,6 +23,63 @@ describe('Issue 2382', function () {
 		.click('button[data-e2e-list-create-button]')
 		.type('[name="name"]', 'Issue2382')
 		.type('.Select-control input', 'Bob Dylan\u000d')
+		.wait(500)
 		.click('button[type="submit"]')
+		.wait(500)
+		.use(navigationClick('users'))
+		.wait(500)
+		// DELETE USER
+		.click('div#react-root > div.css-awku5i:nth-child(1) > main.css-1gs0ko2:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div.css-1xkojxp:nth-child(3) > div:nth-child(1) > div.ItemList-wrapper:nth-child(1) > table.Table.ItemList:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td.ItemList__col--control.ItemList__col--delete:nth-child(1) > button.ItemList__control.ItemList__control--delete:nth-child(1) > span.octicon.octicon-trashcan:nth-child(1)')
+		// CONFIRM DELETE USER
+		.click('html > body > div:nth-child(14) > div:nth-child(1) > div:nth-child(2) > div.css-z5zujf:nth-child(1) > div.css-1mzpobk:nth-child(1) > div.css-2dhvf4:nth-child(2) > button.css-t4884:nth-child(1)')
+		.use(navigationClick('booleans'))
+		.use(navigationClick('relationships'))
+		.wait('.ItemList__value--truncate')
+		.click('.ItemList__value--truncate')
+		// .click('tr .ItemList__col:nth-child(1) .ItemList__value')
+		// .click('div#react-root > div.css-awku5i:nth-child(1) > main.css-1gs0ko2:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div.css-1xkojxp:nth-child(3) > div:nth-child(1) > div.ItemList-wrapper:nth-child(1) > table.Table.ItemList:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td.ItemList__col:nth-child(2) > a.ItemList__value.ItemList__value--text.ItemList__link--interior.ItemList__link--padded.ItemList__value--truncate:nth-child(1)')
+		// .wait(500)
+		.evaluate(function () {
+			return document.querySelector('.Select-value').innerHTML;
+		})
+		.end()
+		.then(function (html) {
+			demand(html).to.equal(html);
+		});
+	});
+	it.only('Describe the relationship issue', function () {
+		return createNightmare()
+		.use(login())
+		.use(createUser({
+			firstName: 'Bob',
+			lastName: 'Dylan',
+			email: 'knockknock@onheavens.door.com',
+			password: 'knockknock',
+		}))
+		.use(navigationClick('booleans'))
+		.use(navigationClick('relationships'))
+		.wait(500)
+		.click('button[data-e2e-list-create-button]')
+		.type('[name="name"]', 'Issue2382')
+		.type('.Select-control input', 'Bob Dylan\u000d')
+		.wait(500)
+		.click('button[type="submit"]')
+		.wait(500)
+		.use(navigationClick('booleans'))
+		.use(navigationClick('relationships'))
+		.wait(1000)
+		.click('.ItemList__value--truncate')
+		.evaluate(function () {
+			return document.location.href;
+		})
+		.end()
+		.then(function (url) {
+			console.log('final url', url);
+		});
+		// .click('.ItemList__value--truncate')
+		// .end()
+		// .then(function (html) {
+			// demand(html).to.equal(html);
+		// });
 	});
 });
