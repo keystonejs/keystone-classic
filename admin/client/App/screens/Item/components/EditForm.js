8bf9b1afa78cdf2eefd933d9be10e61a0cf58e26
Anton Kuzniatsou <akuzniatsou@akuzniatsou-m1.local>
2018-05-16 10:29:40 -0500

    smooth scroll spped now based on current scroll position

--- admin/client/App/screens/Item/components/EditForm.js
+++ admin/client/App/screens/Item/components/EditForm.js
@@ -38,11 +38,16 @@ function getNameFromData (data) {
 }
 
 function smoothScrollTop () {
-	if (document.body.scrollTop || document.documentElement.scrollTop) {
-		window.scrollBy(0, -50);
-		var timeOut = setTimeout(smoothScrollTop, 20);
-	}	else {
-		clearTimeout(timeOut);
+	var position = window.scrollY || window.pageYOffset;
+	var speed = position / 10;
+
+	if (position > 1) {
+		var newPosition = position - speed;
+
+		window.scrollTo(0, newPosition);
+		window.requestAnimationFrame(smoothScrollTop);
+	} else {
+		window.scrollTo(0, 0);
 	}
 }
 
