3b04476dac5d4c82e145cc93c15a630ecfb48fdf
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-05-31 15:13:35 +1000

    fixing admin ui tests (thanks to Vito)
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- admin/client/App/sagas/test/queryParamsSagas.test.js
+++ admin/client/App/sagas/test/queryParamsSagas.test.js
@@ -176,6 +176,12 @@ describe('<List /> query param sagas', function () {
 		});
 
 		describe('If the query and the cached query are the same', function () {
+			before(() => {
+				global.Keystone = {
+					adminPath: '/keystone',
+				};
+			});
+
 			it('puts a QUERY HAS NOT CHANGED action to the store', function () {
 				const generator = evalQueryParams();
 				const pathname = '/keystone/Dictators';
