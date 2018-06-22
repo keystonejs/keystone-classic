3953813a0f2396c6fc3691a6528059d2023bb6c4
Jed Watson <jed.watson@me.com>
2018-06-22 19:16:18 +1000

    Fix for updating single relationship values

--- fields/types/relationship/RelationshipField.js
+++ fields/types/relationship/RelationshipField.js
@@ -191,7 +191,8 @@ module.exports = Field.create({
 
 	renderSelect (noedit) {
 		const inputName = this.getInputName(this.props.path);
-		const emptyValueInput = (this.state.value && this.state.value.length) ? null : <input type="hidden" name={inputName} value="" />;
+		const emptyValueInput = (this.props.many && (!this.state.value || !this.state.value.length))
+			? <input type="hidden" name={inputName} value="" /> : null;
 		return (
 			<div>
 				{/* This input ensures that an empty value is submitted when no related items are selected */}
