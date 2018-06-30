1e458db1708d12ed6f0ecdd290c8dece7f1e0737
Erik van der Plas <erikvdplas@users.noreply.github.com>
2018-06-30 11:30:59 +0200

    Fix bug in Safari where XHR form submission fails with empty file input (#4673)
    * Provides a fix for a bug in Safari where XHR form submission fails when input[type=file] is empty

--- admin/client/App/screens/Item/components/EditForm.js
+++ admin/client/App/screens/Item/components/EditForm.js
@@ -124,7 +124,18 @@ var EditForm = React.createClass({
 	updateItem () {
 		const { data, list } = this.props;
 		const editForm = this.refs.editForm;
+
+		// Fix for Safari where XHR form submission fails when input[type=file] is empty
+		// https://stackoverflow.com/questions/49614091/safari-11-1-ajax-xhr-form-submission-fails-when-inputtype-file-is-empty
+		$(editForm).find("input[type='file']").each(function () {
+			if ($(this).get(0).files.length === 0) { $(this).prop('disabled', true); }
+		});
+
 		const formData = new FormData(editForm);
+
+		$(editForm).find("input[type='file']").each(function () {
+			if ($(this).get(0).files.length === 0) { $(this).prop('disabled', false); }
+		});
 
 		// Show loading indicator
 		this.setState({
