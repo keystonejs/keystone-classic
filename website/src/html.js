a6c0c28a89f1c3248b3dec55b7d29650933ea7bf
Nathan Simpson <hello@nathansimpson.design>
2018-07-26 17:24:49 +1000

    Adding Favicon to Keystone 4 website
    Tested with Chrome and Firefox.

--- website/src/html.js
+++ website/src/html.js
@@ -29,6 +29,9 @@ module.exports = class HTML extends React.Component {
 						name="viewport"
 						content="width=device-width, initial-scale=1, shrink-to-fit=no"
 					/>
+					<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 					{this.props.headComponents}
 					{css}
 				</head>
