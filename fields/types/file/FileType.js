0e6e0d99f644f70f65ac41401f173f466b4c5b7f
myadzel <myadzel@gmail.com>
2019-07-25 14:00:18 +0300

    fix file deletion (change operation) #4946

--- fields/types/file/FileType.js
+++ fields/types/file/FileType.js
@@ -47,12 +47,22 @@ file.prototype.upload = function (item, file, callback) {
 	var field = this;
 	// TODO; Validate there is actuall a file to upload
 	debug('[%s.%s] Uploading file for item %s:', this.list.key, this.path, item.id, file);
-	this.storage.uploadFile(file, function (err, result) {
-		if (err) return callback(err);
-		debug('[%s.%s] Uploaded file for item %s with result:', field.list.key, field.path, item.id, result);
-		item.set(field.path, result);
-		callback(null, result);
-	});
+	var upload = function (item, file, callback) {
+		field.storage.uploadFile(file, function (err, result) {
+			if (err) return callback(err);
+			debug('[%s.%s] Uploaded file for item %s with result:', field.list.key, field.path, item.id, result);
+			item.set(field.path, result);
+			callback(null, result);
+		});
+	};
+	if (item[field.path] && item[field.path].filename) {
+		field.remove(item, function (err, result) {
+			if (err) return callback(err);
+			upload(item, file, callback);
+		});
+	} else {
+		upload(item, file, callback);
+	}
 };
 
 /**
